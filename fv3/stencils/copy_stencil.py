3458d7e7f10d4fffaedd9d00e693ed1c32652141
Jeremy McGibbon <jeremym@vulcan.com>
2020-03-03 15:04:48 -0800

    fix linting errors, ignore variable-unused errors

--- fv3/stencils/copy_stencil.py
+++ fv3/stencils/copy_stencil.py
@@ -1,5 +1,6 @@
 import fv3.utils.gt4py_utils as utils
 import gt4py.gtscript as gtscript
+from gt4py.gtscript import computation, interval, PARALLEL
 
 
 @gtscript.stencil(backend=utils.exec_backend)
--- fv3/stencils/vorticity_volumemean.py
+++ fv3/stencils/vorticity_volumemean.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-from fv3.utils.gt4py_utils import sd, halo, exec_backend, make_storage_from_shape
-import numpy as np
-import gt4py as gt
+from fv3.utils.gt4py_utils import sd, exec_backend
 import gt4py.gtscript as gtscript
 import fv3._config as spec
+from gt4py.gtscript import computation, interval, PARALLEL
 
 
 @gtscript.stencil(backend=exec_backend)
@@ -19,6 +18,25 @@ def volume_mean_relative_vorticity(ut: sd, vt: sd, rarea: sd, wk: sd):
 
 
 def compute(u, v, ut, vt, wk):
-    vorticity(u, spec.grid.dx, vt, origin=spec.grid.default_origin(), domain=spec.grid.domain_shape_y())
-    vorticity(v, spec.grid.dy, ut, origin=spec.grid.default_origin(), domain=spec.grid.domain_shape_x())
-    volume_mean_relative_vorticity(ut, vt, spec.grid.rarea, wk, origin=spec.grid.default_origin(), domain=spec.grid.domain_shape_standard())
+    vorticity(
+        u,
+        spec.grid.dx,
+        vt,
+        origin=spec.grid.default_origin(),
+        domain=spec.grid.domain_shape_y(),
+    )
+    vorticity(
+        v,
+        spec.grid.dy,
+        ut,
+        origin=spec.grid.default_origin(),
+        domain=spec.grid.domain_shape_x(),
+    )
+    volume_mean_relative_vorticity(
+        ut,
+        vt,
+        spec.grid.rarea,
+        wk,
+        origin=spec.grid.default_origin(),
+        domain=spec.grid.domain_shape_standard(),
+    )
