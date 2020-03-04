5c65248d98da348be0ce27fd8d45788e8812e3fb
Rhea George <RheaG@Rheas-MacBook-Pro.local>
2020-03-03 21:33:22 -0800

    sim1_solver validates

--- fv3/stencils/sim1_solver.py
+++ fv3/stencils/sim1_solver.py
@@ -16,92 +16,85 @@ def main_pe(gm2: sd, pe:sd):
         pe = gm2 * 2.0
         
 # TODO: implement MOIST_CAPPA=false
-def solve(dt, gama, gm2, cp2, akap, pe2, dm, pm2, pem, w2, dz2, ptr, wsr, p_fac):
-    is_origin = (spec.grid.is_, 0, 0)
-    islice = slice(spec.grid.is_, spec.grid.ie + 1)
-    islicet= slice(0,spec.grid.nic)
-    tmpshape = (spec.grid.nic, spec.grid.npz, 1)
-    tmpshape_p1 = (spec.grid.nic, spec.grid.npz+1, 1)
- 
-    t1g = 2.0 * dt * dt
-    npz = spec.grid.npz
-    rdt = 1.0 / dt
-    cappa1 = akap - 1.0
-    print(pe2.shape, gm2.shape)
-    slice_m = (islice, slice(0, npz - 1), 0)
-    slice_m2 = (islice, slice(1, npz+1), 0)
-    slice_n = (islice, slice(0, npz), 0)
-    slice_mt = (islicet, slice(0, npz - 1), 0)
-    slice_mt2 = (islicet, slice(1, npz+1), 0)
-    slice_nt = (islicet, slice(0, npz), 0)
+def solve(is_, ie, dt, gm2, cp2, pe2, dm, pm2, pem, w2, dz2, ptr, wsr, p_fac):
     
-    pe2[slice_m] = np.exp(gm2[slice_m] * np.log(-dm[slice_m] / dz2[slice_m] * constants.RDGAS * ptr[slice_m])) - pm2[slice_m]
-  
-    g_rat =  utils.make_storage_from_shape(tmpshape, (0,0,0), backend=utils.data_backend)
-    bb =  utils.make_storage_from_shape(tmpshape, (0,0,0), backend=utils.data_backend)
-    aa =  utils.make_storage_from_shape(tmpshape, (0,0,0), backend=utils.data_backend)
-    dd =  utils.make_storage_from_shape(tmpshape, (0,0,0), backend=utils.data_backend)
-    gam =  utils.make_storage_from_shape(tmpshape, (0,0,0), backend=utils.data_backend)
-    w1 =  utils.make_storage_from_shape(tmpshape, (0,0,0), backend=utils.data_backend)
-    pp =  utils.make_storage_from_shape(tmpshape_p1, (0,0,0), backend=utils.data_backend)
-    w1a = cp.copy(w2, is_origin)
-    w1[:,:, 0] = w1a[islice, 0:spec.grid.npz, 0]
-    g_rat[:,:,0] = dm[slice_n] / dm[slice_m2]
- 
-    bb[slice_nt] = 2.0 * (1.0 + g_rat[slice_nt])
-    dd[slice_nt] = 3.0 * (pe2[slice_n] + g_rat[slice_nt] * pe2[slice_m2])
-    bet = np.squeeze(np.copy(bb)[:,0])
-    p1 = np.squeeze(np.copy(bb)[:,0])
-    ppa = cp.copy(pem, is_origin)
-    pp[:, :, 0] = ppa[islice, :,0]
-    pp[:, 0, 0] = 0.0
-    pp[:, 1, 0] = dd[:, 0, 0] / bet
-    bb[:, npz - 1, 0] = 2.0
-    dd[:, npz - 1, 0] = 3.0* pe2[islice, npz - 1, 0]
-   
+    nic = ie - is_ + 1
+    km = spec.grid.npz - 1
+    npz = spec.grid.npz
+    tmpshape = (nic, km+1)
+    tmpshape_p1 = (nic, km+2)
+    t1g = 2.0 * dt * dt
+    
+    slice_m = slice(0, km + 1)
+    slice_n2 = slice(1, km + 2)
+    slice_m2 = slice(1, km + 1)
+    slice_n = slice(0, km)
+    g_rat = np.zeros(tmpshape)
+    bb = np.zeros(tmpshape)
+    aa = np.zeros(tmpshape)
+    dd = np.zeros(tmpshape)
+    gam = np.zeros(tmpshape)
+    w1 = np.zeros(tmpshape)
+    pp = np.zeros(tmpshape_p1)
+    bet = np.zeros(nic)
+    p1 = np.zeros(nic)
+    pp = np.zeros(pem.shape)
+
+    # pe2[:, slice_m] = np.exp(gm2[:, slice_m] * np.log(-dm[:, slice_m] / dz2[:, slice_m] * constants.RDGAS * ptr[:, slice_m])) - pm2[:, slice_m]
+    pe2[:, slice_m] = np.exp(gm2 * np.log(-dm / dz2 * constants.RDGAS * ptr)) - pm2
+    w1 = np.copy(w2)
+
+    g_rat[:, slice_n] = dm[:, slice_n] / dm[:, slice_m2]
+    bb[:, slice_n] = 2.0 * (1.0 + g_rat[:, slice_n])
+    dd[:, slice_n] = 3.0 * (pe2[:, slice_n] + g_rat[:, slice_n] * pe2[:, slice_m2])
+
+    bet[:] = bb[:, 0]
+    pp[:, 0] = 0.0
+    pp[:, 1] = dd[:, 0] / bet
+    bb[:, km] = 2.0
+    dd[:, km] = 3.0 * pe2[:, km]
+
     for k in range(1, npz):
-        for i in range(spec.grid.nic):
-            gam[i, k] = g_rat[i,k - 1] / bet[i]
+        for i in range(nic):
+            gam[i, k] = g_rat[i, k - 1] / bet[i]
             bet[i] = bb[i, k] - gam[i, k]
             pp[i, k+1] = (dd[i, k] - pp[i, k]) / bet[i]
-   
-    for k in range(npz - 1, 0, -1):
-        pp[:, k + 1, 0] = (dd[:, k, 0] - pp[:, k, 0]) / bet
+
+    for k in range(km, 0, -1):
+        pp[:, k] = pp[:, k] - gam[:, k] * pp[:, k+1]
+
     # w solver
-    zslice = slice(0, npz - 1)
-    zslice2 = slice(1, npz)
-    aa[:, zslice2, 0] = t1g * 0.5 * (gm2[islice, zslice, 0] + gm2[islice, zslice2, 0])\
-        / (dz2[islice, zslice, 0] + dz2[islice, zslice2, 0]) * (pem[islice, zslice2, 0] + pp[:, zslice2, 0])
-    bet[:] = dm[islice, 0, 0] - aa[:, 1, 0]
-    w2[islice, 0, 0] = (dm[islice, 0, 0] * w1[:, 0, 0] + dt * pp[:, 1, 0]) / bet
-    for k in range(1, npz - 1):
-        for i in range(spec.grid.nic):
-            gi = i + spec.grid.is_
-            gam[i, k, 0] = aa[i, k, 0] / bet[i]
-            bet[i] = dm[gi, k, 0] - (aa[i, k, 0] + aa[i, k + 1, 0] + aa[i, k, 0] * gam[i, k, 0])
-            w2[i, k, 0] = (dm[gi, k, 0] * w1[i, k, 0] +
-                           dt * (pp[i, k+1, 0] - pp[i, k, 0]) - aa[i, k, 0] * w2[gi, k-1, 0]) / bet[i]
-    for i in range(spec.grid.nic):
-        gi = i + spec.grid.is_
-        p1[i] = t1g * gm2[gi,npz - 1, 0]/dz2[gi,npz - 1, 0]*(pem[gi,npz - 1+1, 0] + pp[i,npz - 1+1, 0])
-        gam[i,npz - 1, 0] = aa[i,npz - 1, 0] / bet[i]
-        bet[i] =  dm[gi, npz - 1, 0] - (aa[i, npz - 1, 0] + p1[i] + aa[i, npz - 1, 0] * gam[i, npz - 1, 0])
-        w2[gi, npz - 1, 0] = (dm[gi, npz - 1, 0] * w1[i,npz - 1, 0] + dt*(pp[i, npz - 1+1, 0] - pp[i, npz - 1, 0]) - p1[i] * wsr[gi, 0, 0]  -aa[i, npz - 1, 0] * w2[gi,npz - 1 -1, 0])/bet[i]
+    aa[:, slice_m2] = t1g * 0.5 * (gm2[:, slice_n] + gm2[:, slice_m2])\
+                                   / (dz2[:, slice_n] + dz2[:, slice_m2]) * (pem[:, slice_m2] + pp[:, slice_m2])
 
-    for k in range(npz - 2, 0, -1):
-        w2[islice, k, 0] = w2[islice, k, 0 ] - gam[i, k+1, 0] * w2[islice, k+1, 0]
-
-    pe2[islice, 0, 0] = 0.0
-    for k in range(npz):
-        pe2[islice, k+1, 0] = pe2[islice, k, 0] + dm[islice, k, 0] * (w2[islice, k, 0] - w1[:, k, 0]) / dt
-
-    p1[:] = (pe2[islice, npz - 1, 0] + 2.0 * pe2[islice, npz, 0]) * 1.0 / 3.0
+    bet[:] = dm[:, 0] - aa[:, 1]
+    w2[:, 0] = (dm[:, 0] * w1[:, 0] + dt * pp[:, 1]) / bet
+    for k in range(1, km):
+        for i in range(nic):
+            gam[i, k] = aa[i, k] / bet[i]
+            bet[i] = dm[i, k] - (aa[i, k] + aa[i, k + 1] + aa[i, k] * gam[i, k])
+            w2[i, k] = (dm[i, k] * w1[i, k] +
+                        dt * (pp[i, k+1] - pp[i, k]) - aa[i, k] * w2[i, k-1]) / bet[i]
   
-    for i in range(spec.grid.is_, spec.grid.ie + 1):
-        dz2[i, npz - 1, 0] = -dm[i, npz - 1, 0] * constants.RDGAS * ptr[i, npz - 1, 0] * np.exp((cp2[i, npz - 1, 0] - 1.0) * np.log(max(p_fac * pm2[i, npz - 1, 0], p1[i - spec.grid.is_] + pm2[i, npz - 1, 0])))
-                
-    for k in range(npz - 2, 0, -1):
-        for i in range(spec.grid.is_, spec.grid.ie + 1):
-            li = i - spec.grid.is_
-            p1[li] = (pe2[i, k, 0] + bb[li, k, 0] * pe2[i, k+1, 0] + g_rat[li,k, 0] * pe2[i,k+2, 0]) * 1.0 / 3.0 - g_rat[li,k, 0] * p1[li]
-            dz2[i, k, 0] = -dm[i, k, 0] * constants.RDGAS * ptr[i, k, 0] * np.exp((cp2[i, k, 0] - 1.0) * np.log(max(p_fac * pm2[i, k, 0], p1[li] + pm2[i, k, 0])))
+    for i in range(nic):
+        p1[i] = t1g * gm2[i, km] / dz2[i, km] * (pem[i, km+1] + pp[i, km+1])
+        gam[i, km] = aa[i, km] / bet[i]
+        bet[i] = dm[i, km] - (aa[i, km] + p1[i] + aa[i, km] * gam[i, km])
+        w2[i, km] = (dm[i, km] * w1[i, km] + dt * (pp[i, km+1] - pp[i, km]) - p1[i] * wsr[i, 0] - aa[i, km] * w2[i, km - 1])/ bet[i]
+  
+    for k in range(km - 1, -1, -1):
+        w2[:, k] = w2[:, k] - gam[:, k+1] * w2[:, k+1]
+
+    pe2[:, 0] = 0.0
+    for k in range(npz):
+        pe2[:, k+1] = pe2[:, k] + dm[:, k] * (w2[:, k] - w1[:, k]) / dt
+
+    p1[:] = (pe2[:, km] + 2.0 * pe2[:, km + 1]) * 1.0 / 3.0
+  
+    for i in range(nic):
+        dz2[i, km] = -dm[i, km] * constants.RDGAS * ptr[i, km] * np.exp((cp2[i, km] - 1.0) * np.log(max(p_fac * pm2[i, km], p1[i] + pm2[i, km])))
+               
+    for k in range(npz - 2, -1, -1):
+        for i in range(nic):
+            p1[i] = (pe2[i, k] + bb[i, k] * pe2[i, k+1] + g_rat[i, k] * pe2[i, k+2]) * 1.0 / 3.0 - g_rat[i, k] * p1[i]
+            dz2[i, k] = -dm[i, k] * constants.RDGAS * ptr[i, k] * np.exp((cp2[i, k] - 1.0) * np.log(max(p_fac * pm2[i, k], p1[i] + pm2[i, k])))
--- fv3/translate/translate_sim1_solver.py
+++ fv3/translate/translate_sim1_solver.py
@@ -2,7 +2,7 @@ from .translate import TranslateFortranData2Py
 import fv3.stencils.sim1_solver as sim1_solver
 import fv3.utils.gt4py_utils as utils
 import numpy as np
-
+import gt4py as gt
 
 class TranslateSIM1_solver(TranslateFortranData2Py):
     def __init__(self, grid):
@@ -12,31 +12,33 @@ class TranslateSIM1_solver(TranslateFortranData2Py):
         self.in_vars['data_vars'] = {}
         for f in in_fields:
             self.in_vars['data_vars'][f] = {'astart': grid.is_ - 1}
-        self.in_vars['parameters'] = ['dt', 'gama', 'p_fac', 'akap']
-        self.out_vars = {'pe2':  {'istart': grid.is_ - 1, 'iend': grid.ie + 1, 'kstart': 0, 'kend': grid.npz},
-                         'dz2':  {'istart': grid.is_ - 1, 'iend': grid.ie + 1, 'kstart': 0, 'kend': grid.npz - 1},
-                         'w2':   {'istart': grid.is_ - 1, 'iend': grid.ie + 1, 'kstart': 0, 'kend': grid.npz - 1},
+        self.in_vars['parameters'] = ['dt', 'p_fac']
+        #self.out_vars = {'pe2':  {'istart': grid.is_ - 1, 'iend': grid.ie + 1, 'kstart': 0, 'kend': grid.npz},
+        #                 'dz2':  {'istart': grid.is_ - 1, 'iend': grid.ie + 1, 'kstart': 0, 'kend': grid.npz - 1},
+        #                 'w2':   {'istart': grid.is_ - 1, 'iend': grid.ie + 1, 'kstart': 0, 'kend': grid.npz - 1},
+        self.out_vars = {'pe2':  {'kstart': 0, 'kend': grid.npz},
+                         'dz2':  {'kstart': 0, 'kend': grid.npz - 1},
+                         'w2':   {'kstart': 0, 'kend': grid.npz - 1},
         }
 
     def make_2d_storage_data(self, array2d, shape2d, astart=0, bstart=0):
-        return utils.make_2d_storage_data(array2d, shape2d, istart=astart, jstart=bstart, origin=(astart, bstart, 0), backend=utils.data_backend)
-        
-     
+        storage = utils.make_2d_storage_data(array2d, shape2d, istart=astart, jstart=bstart, origin=(astart, bstart, 0), backend=utils.data_backend)
+        return storage.data[self.grid.is_ - 1:self.grid.ie + 2, 0:array2d.shape[1], 0]
+       
     def compute(self, inputs):
-        print('here')
         shape2d=self.grid.domain2d_ik_buffer_1cell()
         for d, info in self.in_vars['data_vars'].items():
-            print(d, info, inputs[d].shape)
             if d == 'ptr':
                 inputs[d] = inputs[d][:,0,:]
             arr = np.squeeze(inputs[d])
             if len(arr.shape) == 1:
                 arr = np.repeat(arr[:, np.newaxis], shape2d[1], axis=1)
-            inputs[d] = self.make_2d_storage_data(arr,
-                                                  shape2d, astart=info['astart'])
-        print(inputs.keys())
+                inputs[d] = arr
+            #inputs[d] = self.make_2d_storage_data(arr,
+            #                                      shape2d, astart=info['astart'])
+        inputs['is_'] = self.grid.is_ - 1
+        inputs['ie'] = self.grid.ie + 1
         #self.make_storage_data_input_vars(inputs)
-        
         self.compute_func(**inputs)
         out = {}
         for var in self.out_vars.keys():
@@ -44,5 +46,6 @@ class TranslateSIM1_solver(TranslateFortranData2Py):
             self.update_info(info, inputs)   
             ds = self.grid.default_domain_dict()
             ds.update(info)
-            out[var] = np.squeeze(inputs[var].data[(slice(ds['istart'], ds['iend'] + 1), slice(ds['kstart'], ds['kend'] + 1))])
+            #out[var] = np.squeeze(inputs[var][:, ds['kstart']:ds['kend'] + 1])
+            out[var] = np.squeeze(inputs[var])
         return out
