6b4e36d5cc716bf4aaf8ba22c2692a1191b10488
Rhea George <RheaG@Rheas-MacBook-Pro.local>
2020-03-16 21:16:16 -0700

    d2a2c stencil first draft, passing 40-80 percent

---
+++ fv3/translate/translate_d2a2c_vect.py
@@ -0,0 +1,13 @@
+from .translate import TranslateFortranData2Py
+import fv3.stencils.d2a2c_vect as d2a2c_vect
+
+
+class TranslateD2A2C_Vect(TranslateFortranData2Py):
+    def __init__(self, grid):
+        super().__init__(grid)
+        self.compute_func = d2a2c_vect.compute
+        self.in_vars['data_vars'] = {'uc': {}, 'vc': {}, 'u': {}, 'v': {}, 'ua': {}, 'va': {}, 'utc': {}, 'vtc': {}}
+        self.in_vars['parameters'] = ['dord4']
+        self.out_vars = {'uc': grid.x3d_domain_dict(), 'vc': grid.y3d_domain_dict(),
+                         'ua': {}, 'va': {}, 'utc': {}, 'vtc': {}}
+        self.max_error = 1e-10
