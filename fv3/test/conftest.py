973f82722f0963e1b3ee81aa87bad43971cc27bb
Jeremy McGibbon <jeremym@vulcan.com>
2020-03-19 08:50:43 -0700

    make reformat

--- fv3/test/conftest.py
+++ fv3/test/conftest.py
@@ -91,9 +91,7 @@ def process_grid_savepoint(serializer, grid_savepoint):
 def get_test_class_instance(test_name, grid):
     translate_class_name = f"Translate{test_name.replace('-', '_')}"
     try:
-        instance = getattr(fv3.translate, translate_class_name)(
-            grid
-        )
+        instance = getattr(fv3.translate, translate_class_name)(grid)
     except AttributeError as err:
         if translate_class_name in err.args[0]:
             instance = None
