dbe7cdae62a0438a870849dace5a8ef1cca6a965
Rhea George <RheaG@Rheas-MacBook-Pro.local>
2020-03-18 22:02:12 -0700

    d2a2c_vect working to relative error 1e-11 -- the edge_interpolat4 method causes issues at higher precision

--- fv3/stencils/d2a2c_vect.py
+++ fv3/stencils/d2a2c_vect.py
@@ -80,29 +80,33 @@ def ut_main(utmp: sd, uc: sd, v: sd, cosa_u: sd, rsin_u: sd,  ut: sd):
         uc = lagrange_x_func(utmp)
         ut = contravariant(uc, v, cosa_u, rsin_u)
 
+
 @gtscript.stencil(backend=utils.exec_backend)
 def vt_main(vtmp: sd, vc: sd, u: sd, cosa_v: sd, rsin_v: sd,  vt: sd):
     with computation(PARALLEL), interval(...):
         vc = lagrange_y_func(vtmp)
         vt = contravariant(vc, u, cosa_v, rsin_v)
-        
 
-        
+
 @gtscript.function
 def vol_conserv_cubic_interp_func_x(u):
     return c1 * u[-2, 0, 0] + c2 * u[-1, 0, 0] + c3 * u
+
 
 @gtscript.function
 def vol_conserv_cubic_interp_func_x_rev(u):
     return c1 * u[1, 0, 0] + c2 * u + c3 * u[-1, 0, 0]
 
+
 @gtscript.function
 def vol_conserv_cubic_interp_func_y(v):
     return c1 * v[0, -2, 0] + c2 * v[0, -1, 0] + c3 * v
 
+
 @gtscript.function
 def vol_conserv_cubic_interp_func_y_rev(v):
     return c1 * v[0, 1, 0] + c2 * v + c3 * v[0, -1, 0]
+
 
 @gtscript.stencil(backend=utils.exec_backend)
 def vol_conserv_cubic_interp_x(utmp: sd, uc: sd):
@@ -135,22 +139,8 @@ def vc_y_edge1(vt: sd, sin_sg4: sd, sin_sg2: sd, vc: sd):
     with computation(PARALLEL), interval(...):
         vc = vt * sin_sg4[0, -1, 0] if vt > 0 else vt * sin_sg2
 
-#def x_edge_ut_uc_1(uc: sd, ut: sd, sin_sg3:Sd, sin_sg1:sd, ua:sd, dxa:sd):
-#    with computation(PARALLEL), interval(...):
-#        ut = edge_interpolate4(ua, dxa)
-'''
-@gtscript.stencil(backend=utils.exec_backend)
-def edge_interpolate4(ua:sd, dxa:sd, out:sd):
-    with computation(PARALLEL), interval(...):
-        t1 = dxa + dxa[1, 0, 0]
-        t2 = dxa[2, 0, 0] + dxa[3, 0, 0]
-        n1 = (t1 + dxa[1, 0, 0]) * ua[1, 0, 0] - dxa[1, 0, 0] * ua[0, 0, 0]
-        n2 = (t1 + dxa[2, 0, 0]) * ua[2, 0, 0] - dxa[2, 0, 0] * ua[3, 0, 0]
-        out = 0.5 * (n1 / t1 + n2 / t2)
-'''
 
-
-# TODO make this a stencil
+# TODO make this a stencil?
 def edge_interpolate4_x(ua, dxa):
     t1 = dxa[0, :, :] + dxa[1, :, :]
     t2 = dxa[2, :, :] + dxa[3, :, :]
@@ -158,12 +148,14 @@ def edge_interpolate4_x(ua, dxa):
     n2 = (t1 + dxa[2, :, :]) * ua[2, :, :] - dxa[2, :, :] * ua[3, :, :]
     return 0.5 * (n1 / t1 + n2 / t2)
 
-def edge_interpolate4_y(ua, dxa):
+
+def edge_interpolate4_y(va, dxa):
     t1 = dxa[:, 0, :] + dxa[:, 1, :]
     t2 = dxa[:, 2, :] + dxa[:, 3, :]
-    n1 = (t1 + dxa[:, 1, :]) * ua[:, 1, :] - dxa[:, 1, :] * ua[:, 0, :]
-    n2 = (t1 + dxa[:, 2, :]) * ua[:, 2, :] - dxa[:, 2, :] * ua[:, 3, :]
+    n1 = (t1 + dxa[:, 1, :]) * va[:, 1, :] - dxa[:, 1, :] * va[:, 0, :]
+    n2 = (t1 + dxa[:, 2, :]) * va[:, 2, :] - dxa[:, 2, :] * va[:, 3, :]
     return 0.5 * (n1 / t1 + n2 / t2)
+
 
 def compute(dord4, uc, vc, u, v, ua, va, utc, vtc):
     grid = spec.grid
@@ -172,62 +164,55 @@ def compute(dord4, uc, vc, u, v, ua, va, utc, vtc):
     ny = grid.npy + 2
     i1 = grid.is_ - 1
     j1 = grid.js - 1
-   
-    if dord4:
-        id_ = 1
-    else:
-        id_ = 0
-    if spec.namelist['grid_type'] < 3 and not grid.nested:
-        npt = grid.halo + 3
-    else:
-        npt = 0
+    id_ = 1 if dord4 else 0
+    npt = 4 if spec.namelist['grid_type'] < 3 and not grid.nested else 0
+
     utmp = utils.make_storage_from_shape(ua.shape, grid.default_origin())
     vtmp = utils.make_storage_from_shape(va.shape, grid.default_origin())
     utmp[:] = big_number
     vtmp[:] = big_number
-    js1 = npt if grid.south_edge else grid.js - 1
+    js1 = npt + grid.js - 1 if grid.south_edge else grid.js - 1
     je1 = ny - npt if grid.north_edge else grid.je + 1
-    is1 = npt if grid.west_edge else grid.isd
+    is1 = npt + grid.is_ - 1 if grid.west_edge else grid.isd
     ie1 = nx - npt if grid.east_edge else grid.ied
-    print('u', (is1, js1, 0),(ie1 - is1 + 1, je1 - js1 + 1, grid.npz))
     lagrange_interpolation_y_p1(u, utmp, origin=(is1, js1, 0),
                                 domain=(ie1 - is1 + 1, je1 - js1 + 1, grid.npz))
-   
-    is1 = npt if grid.west_edge else grid.is_ - 1
+
+    is1 = npt + grid.is_ - 1 if grid.west_edge else grid.is_ - 1
     ie1 = nx - npt if grid.east_edge else grid.ie + 1
-    js1 = npt if grid.south_edge else grid.jsd
+    js1 = npt + grid.js - 1 if grid.south_edge else grid.jsd
     je1 = ny - npt if grid.north_edge else grid.jed
-    print('v', (ie1 - is1 + 1, je1 - js1 + 1, grid.npz))
+
     lagrange_interpolation_x_p1(v, vtmp, origin=(is1, js1, 0),
                                 domain=(ie1 - is1 + 1, je1 - js1 + 1, grid.npz))
     # tmp edges
     if grid.south_edge or grid.jsd < npt:
         avg_box(u, v, utmp, vtmp, origin=(grid.isd, grid.jsd, 0),
-                domain=(grid.nid, npt - grid.jsd, grid.npz))
+                domain=(grid.nid, npt + grid.js - 1 - grid.jsd, grid.npz))
     if grid.north_edge or grid.jed >= (ny - npt):
         je2 = ny - npt + 1
         avg_box(u, v, utmp, vtmp, origin=(grid.isd, je2, 0),
                 domain=(grid.nid, grid.jed - je2 + 1, grid.npz))
-   
-    js2 = max(npt, grid.jsd)
-    je2 = min(ny - npt, grid.jed)
+
+    js2 = npt + grid.js - 1 if grid.south_edge else grid.jsd
+    je2 = ny - npt if grid.north_edge else grid.jed
     jdiff = je2 - js2 + 1
     if grid.west_edge or grid.isd < npt:
         avg_box(u, v, utmp, vtmp, origin=(grid.isd, js2, 0),
-                domain=(npt - grid.isd, jdiff, grid.npz))
+                domain=(npt + grid.is_ - 1 - grid.isd, jdiff, grid.npz))
     if grid.east_edge or grid.ied >= (nx - npt):
         avg_box(u, v, utmp, vtmp, origin=(nx + 1 - npt, js2, 0),
                 domain=(grid.ied - nx + npt, jdiff, grid.npz))
 
     # contra-variant components at cell center
+    pad = 2 + 2 * id_
     contravariant_components(utmp, vtmp, grid.cosa_s, grid.rsin2, ua, va,
                              origin=(grid.is_ - 1 - id_, grid.js - 1 - id_, 0),
-                             domain=(grid.nic + 2 + 2 * id_, grid.njc + 2 + 2 * id_, grid.npz))
-   
+                             domain=(grid.nic + pad, grid.njc + pad, grid.npz))
     # Fix the edges
     # Xdir:
     # TODO, make stencils? need variable offsets
-    
+
     if grid.sw_corner:
         for i in range(-2, 1):
             utmp[i + 2, grid.js - 1, :] = -vtmp[grid.is_ - 1, grid.js - i, :]
@@ -245,15 +230,15 @@ def compute(dord4, uc, vc, u, v, ua, va, utc, vtc):
             utmp[nx + i, ny, :] = -vtmp[nx, grid.je - i, :]
         if spec.namelist['grid_type'] < 3:
             ua[nx, ny, :] = -va[nx,  ny - 1, :]
-            ua[nx + 1, ny, :] = -va[nx, ny, :]
+            ua[nx + 1, ny, :] = -va[nx, ny - 2, :]
     if grid.nw_corner:
         for i in range(-2, 1):
             utmp[i + 2, ny, :] = vtmp[grid.is_ - 1, grid.je + i, :]
         if spec.namelist['grid_type'] < 3:
             ua[i1 - 1, ny, :] = va[i1, ny - 2, :]
             ua[i1, ny, :] = va[i1, ny - 1, :]
-    
-    ifirst = grid.is_ + 1 if grid.west_edge else grid.is_ - 1
+
+    ifirst = grid.is_ + 2 if grid.west_edge else grid.is_ - 1
     ilast = grid.npx if grid.east_edge else grid.ie + 2
     idiff = ilast - ifirst + 1
 
@@ -277,14 +262,14 @@ def compute(dord4, uc, vc, u, v, ua, va, utc, vtc):
                                   domain=domain_edge_x)
 
         if grid.east_edge and not grid.nested:
-            vol_conserv_cubic_interp_x(utmp, uc, origin=(nx - 1, grid.js - 1, 0), domain=domain_edge_x)
+            vol_conserv_cubic_interp_x(utmp, uc, origin=(nx - 1, j1, 0), domain=domain_edge_x)
             islice = slice(nx - 2, nx + 2)
             utc[nx, jslice, :] = edge_interpolate4_x(ua[islice, jslice, :], grid.dxa[islice, jslice, :])
-            uc_x_edge1(utc, grid.sin_sg3, grid.sin_sg1, uc, origin=(nx, grid.js - 1, 0), domain=domain_edge_x)
-            vol_conserv_cubic_interp_x(utmp, uc, origin=(nx + 1, grid.js - 1, 0), domain=domain_edge_x)
-            contravariant_stencil(uc, v, grid.cosa_u, grid.rsin_u, utc,  origin=(grid.npx + 1, grid.js - 1, 0),
+            uc_x_edge1(utc, grid.sin_sg3, grid.sin_sg1, uc, origin=(nx, j1, 0), domain=domain_edge_x)
+            vol_conserv_cubic_interp_x_rev(utmp, uc, origin=(nx + 1, j1, 0), domain=domain_edge_x)
+            contravariant_stencil(uc, v, grid.cosa_u, grid.rsin_u, utc,  origin=(grid.npx + 1, j1, 0),
                                   domain=domain_edge_x)
-            contravariant_stencil(uc, v, grid.cosa_u, grid.rsin_u, utc, origin=(nx + 1, grid.js - 1, 0),
+            contravariant_stencil(uc, v, grid.cosa_u, grid.rsin_u, utc, origin=(nx + 1, j1, 0),
                                   domain=domain_edge_x)
     # Ydir:
     if grid.sw_corner:
@@ -306,34 +291,26 @@ def compute(dord4, uc, vc, u, v, ua, va, utc, vtc):
         for j in range(0, 3):
             vtmp[nx, ny + j, :] = -utmp[grid.ie - j, ny, :]
         va[nx, ny, :] = -ua[nx - 1,  ny, :]
-        va[nx + 1, ny, :] = -ua[nx - 2, ny, :]
-    
+        va[nx, ny + 1, :] = -ua[nx - 2, ny, :]
+
     if spec.namelist['grid_type'] < 3:
-        domain_edge_y = (grid.nic + 2, 1,  grid.npz) # TODO SHOULD BE + 2
-        domain_edge_y_hack = (grid.nic + 1, 1,  grid.npz)
-        islice = slice(grid.is_ - 1, grid.ie + 1)
-        print('before edges',vc[53, 7, 0])
+        domain_edge_y = (grid.nic + 2, 1,  grid.npz)
+        islice = slice(grid.is_ - 1, grid.ie + 2)
         if grid.south_edge:
-            print(vtmp.shape, vc.shape, i1, j1, grid.cosa_v.shape, grid.rsin_v.shape, vtc.shape, domain_edge_y)
             vt_edge(vtmp, vc, u, grid.cosa_v, grid.rsin_v,  vtc,  0, origin=(i1, j1, 0), domain=domain_edge_y)
-            print(vc[53, 7, 0])
             jslice = slice(grid.js - 2, grid.js + 2)
             vtc[islice, grid.js, :] = edge_interpolate4_y(va[islice, jslice, :], grid.dya[islice, jslice, :])
-            print(vc[53, 7, 0])
             vc_y_edge1(vtc, grid.sin_sg4, grid.sin_sg2, vc, origin=(i1, grid.js, 0), domain=domain_edge_y)
-            print(vc[53, 7, 0], is1, js1 + 1, domain_edge_y, grid.nic + 2)
             vt_edge(vtmp, vc, u, grid.cosa_v, grid.rsin_v,  vtc,  1, origin=(i1, grid.js + 1, 0), domain=domain_edge_y)
-        print(vc[53, 7, 0])
         if grid.north_edge:
             vt_edge(vtmp, vc, u, grid.cosa_v, grid.rsin_v,  vtc,  0, origin=(i1, ny - 1, 0), domain=domain_edge_y)
             jslice = slice(ny - 2, ny + 2)
             vtc[islice, ny, :] = edge_interpolate4_y(va[islice, jslice, :], grid.dya[islice, jslice, :])
             vc_y_edge1(vtc, grid.sin_sg4, grid.sin_sg2, vc, origin=(grid.is_ - 1, ny, 0), domain=domain_edge_y)
             vt_edge(vtmp, vc, u, grid.cosa_v, grid.rsin_v,  vtc,  1, origin=(i1, ny + 1, 0), domain=domain_edge_y)
-        print(vc[53, 7, 0])
         vt_main(vtmp, vc, u, grid.cosa_v, grid.rsin_v,  vtc,
-                origin=(i1, j1, 0),
-                domain=(grid.nic + 2, grid.njc + 3, grid.npz))
-        print(vc[53, 7, 0])
+                origin=(i1, grid.js + 2, 0),
+                domain=(grid.nic + 2, ny - grid.js - 3, grid.npz))
+
     else:
         raise Exception('unimplemented grid_type >= 3')
--- fv3/translate/translate_d2a2c_vect.py
+++ fv3/translate/translate_d2a2c_vect.py
@@ -10,4 +10,5 @@ class TranslateD2A2C_Vect(TranslateFortranData2Py):
         self.in_vars['parameters'] = ['dord4']
         self.out_vars = {'uc': grid.x3d_domain_dict(), 'vc': grid.y3d_domain_dict(),
                          'ua': {}, 'va': {}, 'utc': {}, 'vtc': {}}
-        self.max_error = 1e-10
+        # TODO -- this seems to be needed primarily for the edge_interpolate_4 methods, can we rejigger the order of operations to make it match to more precision?
+        self.max_error = 1e-11
