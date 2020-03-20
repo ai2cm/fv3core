e9fd768fb46183837bc8e39d0520b6eff435228a
oelbert <olivere@vulcan.com>
2020-03-20 17:52:44 -0400

    first go at transportdelp, need to do translate file still. Also added fill3

---
+++ fv3/translate/translate_fill3_4corners.py
@@ -0,0 +1,25 @@
+from .translate import TranslateFortranData2Py
+from ..utils.corners import fill3_4corners
+
+class TranslateFill3_4Corners(TranslateFortranData2Py):
+    def __init__(self, grid):
+        super().__init__(grid)
+        self.in_vars["data_vars"] = {"q1c": {},
+                                     "q2c": {},
+                                     "q3c": {}}
+        self.in_vars["parameters"] = ["dir"]
+        self.out_vars = {"q1c": {},
+                         "q2c": {},
+                         "q3c": {}}
+
+    def compute(self, inputs):
+
+        if inputs["dir"] == 1:
+            direction = "x"
+        if inputs["dir"] == 2:
+            direction = "y"
+        fill3_4corners(inputs["q1c"], inputs["q2c"], inputs["q3c"], direction, self.grid)
+
+        return {"q1c": inputs["q1c"],
+                "q2c": inputs["q2c"],
+                "q3c": inputs["q3c"]}
