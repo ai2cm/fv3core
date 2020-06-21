c81b19a06e54fb9db1d563ed0f107c53aab5bf47
Rhea George <RheaG@Rheas-MBP.PK5001Z>
2020-06-20 21:44:55 -0700

    remove deprecated imports

--- fv3/translate/translate_map1_ppm_2d.py
+++ fv3/translate/translate_map1_ppm_2d.py
@@ -1,6 +1,4 @@
 from .translate import TranslateFortranData2Py
-import fv3.stencils.map_ppm_2d as Map1_PPM_2d
-import fv3.stencils.map_scalar as Map_Scalar
 import fv3.stencils.map_single as Map_Single
 import numpy as np
 
