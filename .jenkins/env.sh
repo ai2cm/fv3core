2a8ea9fe75ad96eb78b722f4dfbee77304d6b980
Rhea George <RheaG@Rheas-MacBook-Pro.local>
2020-08-07 12:32:59 -0700

    remove module load git from env.sh

--- .jenkins/env.sh
+++ .jenkins/env.sh
@@ -1,13 +1,14 @@
 envdir="${envloc}/env"
+if [ -z `which git`
 if [ -d "${envdir}" ] ; then
     pushd "${envdir}" > /dev/null
-        `module load git; git pull &>/dev/null`
+        `git pull &>/dev/null`
         if [ $? -ne 0 ] ; then
             echo "WARNING: Problem pulling the buildenv. Defaulting to offline mode."
         fi
     popd
 else
-    `module load git; git clone https://github.com/VulcanClimateModeling/buildenv ${envdir} &>/dev/null`
+    `git clone https://github.com/VulcanClimateModeling/buildenv ${envdir} &>/dev/null`
     if [ $? -ne 0 ] ; then
         echo "Error: Could not download the buildenv (https://github.com/VulcanClimateModeling/buildenv) into ${envdir}. Aborting."
         exit 1
