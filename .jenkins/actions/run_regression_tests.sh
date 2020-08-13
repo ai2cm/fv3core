dec3d45b2670335714365cc95acf7fb1efce24a1
Rhea George <RheaG@Rheas-MacBook-Pro.local>
2020-08-12 21:04:53 -0700

    more fussing

--- .jenkins/actions/run_regression_tests.sh
+++ .jenkins/actions/run_regression_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 envloc=$1
 BACKEND=$2
-EXPERIMENT_NAME=$3
+EXPERIMENT=$3
 ARGS="-v -s -rsx --backend=${BACKEND}"
 maxsleep=9000
 if [ "`hostname | grep daint`" != "" ] ; then
@@ -22,8 +22,8 @@ module add /project/d107/install/modulefiles/
 module load gcloud
 # get the test data version from the Makefile
 FORTRAN_VERSION=`grep "FORTRAN_SERIALIZED_DATA_VERSION=" Makefile  | cut -d '=' -f 2`
-DATA_DIR="/scratch/snx3000/rgeorge/fv3core_serialized_test_data/${FORTRAN_VERSION}/${EXPERIMENT_NAME}"
-PROJECT_DATA_DIR="/project/d107/fv3core_serialized_test_data/${FORTRAN_VERSION}/${EXPERIMENT_NAME}"
+DATA_DIR="/scratch/snx3000/rgeorge/fv3core_serialized_test_data/${FORTRAN_VERSION}/${EXPERIMENT}"
+PROJECT_DATA_DIR="/project/d107/fv3core_serialized_test_data/${FORTRAN_VERSION}/${EXPERIMENT}"
 # sync the test data if it does not live in /scratch
 if [ ! -d ${DATA_DIR} ] ; then
     TEST_DATA_HOST=${PROJECT_DATA_DIR} make sync_test_data
@@ -55,6 +55,6 @@ cat ${out}
 rm ${out}
 
 else
-    EXPERIMENT=${EXPERIMENT_NAME} make tests TEST_ARGS="${ARGS}"
-    EXPERIMENT=${EXPERIMENT_NAME} make tests_mpi TEST_ARGS="${ARGS}"
+    EXPERIMENT=${EXPERIMENT} make tests TEST_ARGS="${ARGS}"
+    EXPERIMENT=${EXPERIMENT} make tests_mpi TEST_ARGS="${ARGS}"
 fi
