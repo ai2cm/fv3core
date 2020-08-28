d3124a20bce1ce00de3d52fed6b2171edeadeab5
oelbert <olivere@vulcan.com>
2020-08-28 14:22:41 -0400

    working version

--- fv3core/decorators.py
+++ fv3core/decorators.py
@@ -6,6 +6,8 @@ import gt4py as gt
 import fv3core
 import numpy
 import fv3core._config as spec
+from fv3util import Quantity
+import xarray as xr
 
 ArgSpec = collections.namedtuple(
     "ArgSpec", ["arg_name", "standard_name", "units", "intent"]
@@ -38,8 +40,10 @@ def state_inputs(*arg_specs):
                 if isinstance(state[standard_name].storage, numpy.ndarray):
                     dat = state[standard_name].storage
                     # print(len(state[standard_name].storage.shape))
+                    dims = state[standard_name].dims
+                    origin = namespace_kwargs[arg_name + "_quantity"].origin
+                    extent = namespace_kwargs[arg_name + "_quantity"].extent
                     if len(state[standard_name].storage.shape) == 3:#gotta check the dimension orders
-                        dims = state[standard_name].dims
                         zcheck = ["z" in dim for dim in dims]
                         ycheck = ["y" in dim for dim in dims]
                         xcheck = ["x" in dim for dim in dims]
@@ -48,15 +52,26 @@ def state_inputs(*arg_specs):
                         # print(indices[zcheck])
                         ind=[indices[zcheck][0],indices[ycheck][0],indices[xcheck][0]]
                         dat = dat.transpose(ind)
+                        # print(numpy.asarray(dims))
+                        # print(ind)
+                        dims = tuple(numpy.asarray(dims)[ind]) #lol
+                        origin = tuple(numpy.asarray(origin)[ind])
+                        extent = tuple(numpy.asarray(extent)[ind])
                         # print(dat.shape)
                     elif len(state[standard_name].storage.shape) == 2: #TODO maybe add handling for non (y,x) arrays
                         dat = numpy.tile(dat, (spec.namelist["npz"]+1,1,1))
+                        dims = ("z", dims[0], dims[1])
+                        origin = (0, origin[0], origin[1])
+                        extent = (spec.namelist["npz"], extent[0], extent[1])
                         # print(dat.shape)
                     elif len(state[standard_name].storage.shape) == 1: #fml
                         xdim = spec.namelist["npx"]+6# 3 halo on each side
                         ydim = spec.namelist["npy"]+6# ditto
                         zdim = spec.namelist["npz"]+1 #no halos in z-direction!
-                        dat = numpy.repeat(dat,xdim*ydim,axis=0).reshape(zdim,ydim,xdim) #TODO maybe add handling for non z arrays
+                        dat = numpy.repeat(dat,xdim*ydim,axis=0).reshape(zdim,ydim,xdim)
+                        dims = (dims[0], "y", "x")#TODO maybe add handling for non z arrays
+                        origin = (origin[0], 3, 3)
+                        extent = (extent[0], spec.namelist["npy"]-1, spec.namelist["npx"]-1)
                         # print(dat.shape)
                     else:
                         raise IndexError(
@@ -64,6 +79,7 @@ def state_inputs(*arg_specs):
                     )
                     dat = dat.transpose(2,1,0)
                     namespace_kwargs[arg_name] = gt.storage.from_array(data=dat, backend=fv3core.utils.gt4py_utils.backend, default_origin=(0,0,0), shape=(spec.namelist["npx"]+6,spec.namelist["npy"]+6,spec.namelist["npz"]+1))
+                    namespace_kwargs[arg_name + "_quantity"] = Quantity(data=namespace_kwargs[arg_name], units=namespace_kwargs[arg_name + "_quantity"].units, origin=origin, extent=extent, dims=dims)#?
             func(types.SimpleNamespace(**namespace_kwargs), *args, **kwargs)
 
         return wrapped
