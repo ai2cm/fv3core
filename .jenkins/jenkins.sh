787e5ac7e213ebf506b878a8503b68022b1b6983
Rhea George <rheag@vulcan.com>
2020-09-12 17:00:00 -0700

    omg

--- .jenkins/jenkins.sh
+++ .jenkins/jenkins.sh
@@ -69,10 +69,12 @@ script="${root}/actions/${action}.sh"
 test -f "${script}" || exitError 1301 ${LINENO} "cannot find script ${script}"
 . ${envloc}/env/schedulerTools.sh
 scheduler_script="`dirname $0`/env/submit.${host}.${scheduler}"
-if [ grep -q "parallel" <<< "${script}" ] && [ grep -q "ranks" <<< "${optarg2}" ] ; then
-    NUM_RANKS=`echo ${optarg2} | grep -o -E '[0-9]+ranks' | grep -o -E '[0-9]+'`
-    if [ -f ${envloc}/env/env.${host}.sh ] ; then
-	sed -i 's|<NTASKS>|"'${NUM_RANKS}'"|g' ${scheduler_script}
+if grep -q "parallel" <<< "${script}"; then
+    if grep -q "ranks" <<< "${optarg2}"; then
+	NUM_RANKS=`echo ${optarg2} | grep -o -E '[0-9]+ranks' | grep -o -E '[0-9]+'`
+	if [ -f ${envloc}/env/env.${host}.sh ] ; then
+	    sed -i 's|<NTASKS>|"'${NUM_RANKS}'"|g' ${scheduler_script}
+	fi
     fi
 fi
 
