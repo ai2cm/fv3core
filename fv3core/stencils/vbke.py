d194fc0fb9bb8384b5569f0528bb604f6620f4e5
Jeremy McGibbon <jeremym@vulcan.com>
2020-09-15 16:30:14 -0700

    refactor vbke to use regions concept

--- fv3core/stencils/vbke.py
+++ fv3core/stencils/vbke.py
@@ -2,59 +2,44 @@
 import fv3core.utils.gt4py_utils as utils
 import gt4py.gtscript as gtscript
 import fv3core._config as spec
-from gt4py.gtscript import computation, interval, PARALLEL
+from gt4py.gtscript import computation, interval, PARALLEL, parallel, region
 
 sd = utils.sd
 
 
 @utils.stencil()
-def main_vb(vc: sd, uc: sd, cosa: sd, rsina: sd, vb: sd, dt5: float):
+def main_vb(vc: sd, uc: sd, cosa: sd, rsina: sd, vt: sd, vb: sd, dt4: float, dt5: float):
+    from __splitters__ import i_start, i_end, j_start, j_end
     with computation(PARALLEL), interval(...):
         vb[0, 0, 0] = dt5 * (vc[-1, 0, 0] + vc - (uc[0, -1, 0] + uc) * cosa) * rsina
-
-
-@utils.stencil()
-def y_edge(vt: sd, vb: sd, dt5: float):
-    with computation(PARALLEL), interval(...):
-        vb[0, 0, 0] = dt5 * (vt[-1, 0, 0] + vt)
-
-
-@utils.stencil()
-def x_edge(vt: sd, vb: sd, dt4: float):
-    with computation(PARALLEL), interval(...):
-        vb[0, 0, 0] = dt4 * (-vt[-2, 0, 0] + 3.0 * (vt[-1, 0, 0] + vt) - vt[1, 0, 0])
+        with parallel(region[i_start:i_start+1, j_start:j_end], region[i_end:i_end+1, j_start:j_end]):
+            vb[0, 0, 0] = dt4 * (-vt[-2, 0, 0] + 3.0 * (vt[-1, 0, 0] + vt) - vt[1, 0, 0])
+        with parallel(region[i_start:i_end+1, j_start:j_start+1], region[i_start:i_end+1, j_end:j_end+1]):
+            vb[0, 0, 0] = dt5 * (vt[-1, 0, 0] + vt)
 
 
 def compute(uc, vc, vt, vb, dt5, dt4):
     grid = spec.grid
-    # avoid running center-domain computation on tile edges, since they'll be overwritten.
-    js2 = grid.js + 1 if grid.south_edge else grid.js
-    is2 = grid.is_ + 1 if grid.west_edge else grid.is_
-    je1 = grid.je if grid.north_edge else grid.je + 1
-    ie1 = grid.ie if grid.east_edge else grid.ie + 1
-    jdiff = je1 - js2 + 1
-    idiff = ie1 - is2 + 1
+    splitters = {
+        "i_start": 0,
+        "i_end": grid.ie + 1 - grid.is_,
+        "j_start": 0,
+        "j_end": grid.je + 1 - grid.js,
+    }
     if spec.namelist.grid_type < 3 and not grid.nested:
-        domain_y = (grid.nic + 1, 1, grid.npz)
-        domain_x = (1, jdiff, grid.npz)
-        if grid.south_edge:
-            y_edge(vt, vb, dt5=dt5, origin=grid.compute_origin(), domain=domain_y)
         main_vb(
             vc,
             uc,
             grid.cosa,
             grid.rsina,
+            vt,
             vb,
+            dt4=dt4,
             dt5=dt5,
-            origin=(is2, js2, 0),
-            domain=(idiff, jdiff, grid.npz),
+            origin=(grid.is_, grid.js, 0),
+            domain=(grid.ie - grid.is_ + 2, grid.je - grid.js + 2, grid.npz),
+            splitters=splitters
         )
-        if grid.west_edge:
-            x_edge(vt, vb, dt4=dt4, origin=(grid.is_, js2, 0), domain=domain_x)
-        if grid.east_edge:
-            x_edge(vt, vb, dt4=dt4, origin=(grid.ie + 1, js2, 0), domain=domain_x)
-        if grid.north_edge:
-            y_edge(vt, vb, dt5=dt5, origin=(grid.is_, grid.je + 1, 0), domain=domain_y)
     else:
         # should be a stencil like vb = dt5 * (vc[-1, 0,0] + vc)
         raise Exception("unimplemented grid_type >= 3 or nested")
