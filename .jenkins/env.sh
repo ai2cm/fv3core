256f9e241a2727b0b9ddb0012d8ce653076b252b
Rhea George <rheag@vulcan.com>
2020-09-15 19:32:31 -0700

    env pointed back at master

--- .jenkins/env.sh
+++ .jenkins/env.sh
@@ -7,8 +7,7 @@ if [ -d "${envdir}" ] ; then
         fi
     popd
 else
-    `git clone https://github.com/VulcanClimateModeling/buildenv ${envdir} &>/dev/null`
-    `cd ${envdir} && git checkout container-cmd &>/dev/null`
+    git clone https://github.com/VulcanClimateModeling/buildenv ${envdir} &>/dev/null
     if [ $? -ne 0 ] ; then
         echo "Error: Could not download the buildenv (https://github.com/VulcanClimateModeling/buildenv) into ${envdir}. Aborting."
         exit 1
