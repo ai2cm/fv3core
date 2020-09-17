bc453ee27a85c8f6fd748e00e54b96ecbd4b97ae
Mark Cheeseman <MarkC@Vulcan.com>
2020-09-17 10:05:24 -0700

    stencil now validates on 54 MPI tasks

--- fv3core/stencils/vorticitytransport_cgrid.py
+++ fv3core/stencils/vorticitytransport_cgrid.py
@@ -10,7 +10,7 @@ def update_zonal_velocity( vorticity: sd, ke: sd, velocity: sd, velocity_c: sd, 
 
     with computation(PARALLEL), interval(...):
         tmp_flux = dt2 * (velocity - velocity_c * cosa) / sina
-        with parallel(region[i_start:i_start+1,:],region[i_end+1:i_end+2,:]):
+        with parallel(region[i_start:i_start+1,:],region[i_end:i_end+1,:]):
             tmp_flux = dt2 * velocity
         flux = vorticity if tmp_flux > 0.0 else vorticity[0, 1, 0]
         velocity_c = velocity_c + tmp_flux * flux + rdxc * (ke[-1, 0, 0] - ke)
@@ -29,12 +29,17 @@ def update_meridional_velocity( vorticity: sd, ke: sd, velocity: sd, velocity_c:
 # Update the C-Grid zonal and meridional velocity fields
 def compute(uc, vc, vort_c, ke_c, v, u, fxv, fyv, dt2):
     grid = spec.grid
+    j_start = grid.js-grid.global_js if grid.south_edge else 10000
+    j_end = grid.je-grid.js if grid.north_edge else -10000
+    
     update_meridional_velocity( vort_c, ke_c, u, vc, grid.cosa_v, grid.sina_v, grid.rdyc, dt2,
                                 origin=grid.compute_origin(), 
                                 domain=(grid.nic, grid.njc + 1, grid.npz),
-                                splitters={"j_start": grid.js-grid.js, "j_end": grid.je-grid.js} )
+                                splitters={"j_start": j_start, "j_end": j_end} )
 
+    i_start = grid.global_is-grid.is_ if grid.west_edge else 10000
+    i_end = grid.ie-grid.is_+1 if grid.east_edge else -10000
     update_zonal_velocity( vort_c, ke_c, v, uc, grid.cosa_u, grid.sina_u, grid.rdxc, dt2, 
                            origin=grid.compute_origin(), 
                            domain=(grid.nic + 1, grid.njc, grid.npz),
-                           splitters={"i_start": grid.is_-grid.is_, "i_end": grid.ie-grid.is_} )
+                           splitters={"i_start": i_start, "i_end": i_end} )
