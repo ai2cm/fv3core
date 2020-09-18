9fa18dae1f918fbdfc0a655dae0aab8d29aea234
Rhea George <rheag@vulcan.com>
2020-09-18 16:04:06 -0700

    get the test data for parallel tests as when run as an axis on gce they have their own workspace

--- .jenkins/actions/run_parallel_regression_tests.sh
+++ .jenkins/actions/run_parallel_regression_tests.sh
@@ -8,5 +8,8 @@ export EXPERIMENT=${EXPNAME}
 # Set the host data location
 export TEST_DATA_HOST="${TEST_DATA_DIR}/${EXPNAME}/"
 
+# sync the test data 
+make get_test_data
+
 # The default of this set to 1 causes a segfault
 make run_tests_parallel TEST_ARGS="${ARGS}"
