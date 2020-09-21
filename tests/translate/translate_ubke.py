cca55684ddb0d864ed982c3e60f1dbba11be3f57
Johann Dahm <johannd@vulcan.com>
2020-09-21 16:03:14 -0700

    Update ubke and vbke translate classes

--- tests/translate/translate_ubke.py
+++ tests/translate/translate_ubke.py
@@ -1,11 +1,26 @@
 from .translate import TranslateFortranData2Py
-import fv3core.stencils.ubke as ubke
+from fv3core.stencils.ubke import ubke
 
 
 class TranslateUbKE(TranslateFortranData2Py):
+    def _call(self, *args, **kwargs):
+        ubke(
+            *args,
+            **kwargs,
+            cosa=self.grid.cosa,
+            rsina=self.grid.rsina,
+            origin=(self.grid.is_, self.grid.js, 0),
+            domain=(
+                self.grid.ie - self.grid.is_ + 2,
+                self.grid.je - self.grid.js + 2,
+                self.grid.npz,
+            ),
+            splitters=self.grid.splitters,
+        )
+
     def __init__(self, grid):
         super().__init__(grid)
-        self.compute_func = ubke.compute
+        self.compute_func = self._call
         self.in_vars["data_vars"] = {
             "uc": {},
             "vc": {},
--- tests/translate/translate_vbke.py
+++ tests/translate/translate_vbke.py
@@ -1,14 +1,29 @@
 from .translate import TranslateFortranData2Py
-import fv3core.stencils.vbke as vbke
+from fv3core.stencils.vbke import vbke
 
 
 class TranslateVbKE(TranslateFortranData2Py):
+    def _call(self, *args, **kwargs):
+        vbke(
+            *args,
+            **kwargs,
+            cosa=self.grid.cosa,
+            rsina=self.grid.rsina,
+            origin=(self.grid.is_, self.grid.js, 0),
+            domain=(
+                self.grid.ie - self.grid.is_ + 2,
+                self.grid.je - self.grid.js + 2,
+                self.grid.npz,
+            ),
+            splitters=self.grid.splitters,
+        )
+
     def __init__(self, grid):
         super().__init__(grid)
-        self.compute_func = vbke.compute
+        self.compute_func = self._call
         self.in_vars["data_vars"] = {
-            "uc": {},
             "vc": {},
+            "uc": {},
             "vt": {},
             "vb": grid.compute_dict_buffer_2d(),
         }
