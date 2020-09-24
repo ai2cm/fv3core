edb587e452907c210ee752f8554a04676d4b4d51
Johann Dahm <johannd@vulcan.com>
2020-09-24 12:26:35 -0700

    Remove extraneous slash

--- .jenkins/actions/lint.sh
+++ .jenkins/actions/lint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e -x
 pip3 install -r requirements.txt
-PIP_BIN_DIR=/home/jenkins/.local/bin/
+PIP_BIN_DIR=/home/jenkins/.local/bin
 ${PIP_BIN_DIR}/pre-commit run --all-files
 echo $(date) > aggregate
