ai2cm/pace

commit 66616ed1128a5cc6d420527a3a09f600ebb2f071

Enforce that rebuild and backend are only set as module attributes (#107)
Raises an exception if rebuild or backend are passed to the stencil function/decorator.

fv3core/utils/gt4py_utils.py
@@ -48,6 +48,11 @@ def quantity_name(name):
     return name + "_quantity"
 
 
+def module_level_var_errmsg(var: str, func: str):
+    loc = f"fv3core.utils.gt4py_utils.{var}"
+    return f"The {var} flag should be set in {loc} instead of as an argument to {func}"
+
+
 def stencil(**stencil_kwargs):
     def decorator(func):
         stencils = {}
@@ -56,9 +61,13 @@ def stencil(**stencil_kwargs):
         def wrapped(*args, **kwargs):
             key = (backend, rebuild)
             if key not in stencils:
-                stencils[key] = gtscript.stencil(
-                    backend=backend, rebuild=rebuild, **stencil_kwargs
-                )(func)
+                if "rebuild" in stencil_kwargs:
+                    raise ValueError(module_level_var_errmsg("rebuild", __module__))
+                if "backend" in stencil_kwargs:
+                    raise ValueError(module_level_var_errmsg("backend", __module__))
+                stencil_kwargs["rebuild"] = rebuild
+                stencil_kwargs["backend"] = backend
+                stencils[key] = gtscript.stencil(**stencil_kwargs)(func)
             return stencils[key](*args, **kwargs)
 
         return wrapped

ai2cm/fv3core

commit 592cc9a95e6f5b88e5dc54ebedeefe884f5a1576

Fix bug introduced in #107

fv3core/utils/gt4py_utils.py
@@ -61,13 +61,14 @@ def stencil(**stencil_kwargs):
         def wrapped(*args, **kwargs):
             key = (backend, rebuild)
             if key not in stencils:
-                if "rebuild" in stencil_kwargs:
+                build_kwargs = stencil_kwargs.copy()
+                if "rebuild" in build_kwargs:
                     raise ValueError(module_level_var_errmsg("rebuild", __module__))
-                if "backend" in stencil_kwargs:
+                if "backend" in build_kwargs:
                     raise ValueError(module_level_var_errmsg("backend", __module__))
-                stencil_kwargs["rebuild"] = rebuild
-                stencil_kwargs["backend"] = backend
-                stencils[key] = gtscript.stencil(**stencil_kwargs)(func)
+                build_kwargs["rebuild"] = rebuild
+                build_kwargs["backend"] = backend
+                stencils[key] = gtscript.stencil(**build_kwargs)(func)
             return stencils[key](*args, **kwargs)
 
         return wrapped