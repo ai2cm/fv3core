ba025f43c6ad288ebc0a9c101eea97be3b4e1c4f
Johann Dahm <johannd@vulcan.com>
2020-09-30 08:07:46 -0700

    Format

--- fv3core/utils/types.py
+++ fv3core/utils/types.py
@@ -2,12 +2,15 @@ from typing import Tuple
 
 from gtscript import Field
 
+
 Int3 = Tuple[int, int, int]
 """Common type: tuple of three ints."""
+
 
 class _FieldDescriptor:
     def __getitem__(self, dtype_and_axes):
         return gtscript.Field[dtype_and_axes]
+
 
 Field = _FieldDescriptor()
 """A gtscript field."""
