eced089367fa122336dba70df92e4285cc260157
Johann Dahm <johannd@vulcan.com>
2020-10-05 17:11:14 -0700

    Revert a few typing changes

--- fv3core/stencils/basic_operations.py
+++ fv3core/stencils/basic_operations.py
@@ -4,16 +4,12 @@ import gt4py.gtscript as gtscript
 from gt4py.gtscript import PARALLEL, computation, interval
 
 import fv3core.utils.gt4py_utils as utils
-from fv3core.utils.types import Field, Int3
 
-
-# The stencils here are compiled for this data type
-DATA_TYPE = float
-DataField = Field[DATA_TYPE]
-
+Int3 = Tuple[int, int, int]
+sd = utils.sd
 
 @utils.stencil()
-def copy_stencil(q_in: DataField, q_out: DataField):
+def copy_stencil(q_in: sd, q_out: sd):
     """Copy q_in to q_out.
 
     Args:
@@ -25,7 +21,7 @@ def copy_stencil(q_in: DataField, q_out: DataField):
 
 
 def copy(
-    q_in: DataField, *, origin: Int3 = (0, 0, 0), domain: Union[Int3, None] = None
+    q_in: sd, *, origin: Int3 = (0, 0, 0), domain: Union[Int3, None] = None
 ):
     """Copy q_in to a field returned.
 
@@ -43,68 +39,68 @@ def copy(
 
 
 @utils.stencil()
-def adjustmentfactor_stencil(adjustment: DataField, q_out: DataField):
+def adjustmentfactor_stencil(adjustment: sd, q_out: sd):
     with computation(PARALLEL), interval(...):
         q_out[0, 0, 0] = q_out * adjustment
 
 
 @utils.stencil()
-def adjust_divide_stencil(adjustment: DataField, q_out: DataField):
+def adjust_divide_stencil(adjustment: sd, q_out: sd):
     with computation(PARALLEL), interval(...):
         q_out[0, 0, 0] = q_out / adjustment
 
 
 @utils.stencil()
-def multiply_stencil(in1: DataField, in2: DataField, out: DataField):
+def multiply_stencil(in1: sd, in2: sd, out: sd):
     with computation(PARALLEL), interval(...):
         out[0, 0, 0] = in1 * in2
 
 
 @utils.stencil()
-def divide_stencil(in1: DataField, in2: DataField, out: DataField):
+def divide_stencil(in1: sd, in2: sd, out: sd):
     with computation(PARALLEL), interval(...):
         out[0, 0, 0] = in1 / in2
 
 
 @utils.stencil()
-def addition_stencil(in1: DataField, in2: DataField, out: DataField):
+def addition_stencil(in1: sd, in2: sd, out: sd):
     with computation(PARALLEL), interval(...):
         out[0, 0, 0] = in1 + in2
 
 
 @utils.stencil()
-def add_term_stencil(in1: DataField, out: DataField):
+def add_term_stencil(in1: sd, out: sd):
     with computation(PARALLEL), interval(...):
         out[0, 0, 0] = out + in1
 
 
 @utils.stencil()
-def add_term_two_vars(in1: DataField, out1: DataField, in2: DataField, out2: DataField):
+def add_term_two_vars(in1: sd, out1: sd, in2: sd, out2: sd):
     with computation(PARALLEL), interval(...):
         out1[0, 0, 0] = out1 + in1
         out2[0, 0, 0] = out2 + in2
 
 
 @utils.stencil()
-def subtract_term_stencil(in1: DataField, out: DataField):
+def subtract_term_stencil(in1: sd, out: sd):
     with computation(PARALLEL), interval(...):
         out[0, 0, 0] = out - in1
 
 
 @utils.stencil()
-def multiply_constant(in1: DataField, in2: float, out: DataField):
+def multiply_constant(in1: sd, in2: float, out: sd):
     with computation(PARALLEL), interval(...):
         out[0, 0, 0] = in1 * in2
 
 
 @utils.stencil()
-def multiply_constant_inout(inout: DataField, in_float: float):
+def multiply_constant_inout(inout: sd, in_float: float):
     with computation(PARALLEL), interval(...):
         inout[0, 0, 0] = in_float * inout
 
 
 @utils.stencil()
-def floor_cap(var: DataField, floor_value: float):
+def floor_cap(var: sd, floor_value: float):
     with computation(PARALLEL), interval(0, None):
         var[0, 0, 0] = var if var > floor_value else floor_value
 
