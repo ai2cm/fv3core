7c2f03685922ea874a84eec4c2f5ef84cd4a6bf1
Johann Dahm <johannd@vulcan.com>
2020-10-06 12:38:27 -0700

    Update translate class

--- tests/translate/translate_transportdelp.py
+++ tests/translate/translate_transportdelp.py
@@ -1,12 +1,20 @@
-import fv3core.stencils.transportdelp as TransportDelp
+from fv3core.stencils.transportdelp import transportdelp
 
 from .translate import TranslateFortranData2Py
 
 
 class TranslateTransportDelp(TranslateFortranData2Py):
+    def _call(self, *args, **kwargs):
+        transportdelp(
+            *args,
+            **kwargs,
+            origin=(self.grid.is_ - 1, self.grid.js - 1, 0),
+            domain=self.grid.domain_shape_compute_buffer_2d(add=(2, 2, 0)),
+            splitters=self.grid.splitters,
+        )
+
     def __init__(self, grid):
         super().__init__(grid)
-        self.compute_func = TransportDelp.compute
         self.in_vars["data_vars"] = {
             "delp": {},
             "pt": {},
@@ -14,10 +22,11 @@ class TranslateTransportDelp(TranslateFortranData2Py):
             "utc": {},
             "vtc": {},
             "wc": {},
+            "delpc": {}, "ptc": {},
         }
         self.out_vars = {"delpc": {}, "ptc": {}, "wc": {}}
 
-    def compute(self, inputs):
-        self.make_storage_data_input_vars(inputs)
-        delpc, ptc = self.compute_func(**inputs)
-        return self.slice_output(inputs, {"delpc": delpc, "ptc": ptc})
+    def compute(self, storages):
+        self.make_storage_data_input_vars(storages)
+        self._call(**storages)
+        return self.slice_output(storages, {"delpc": storages["delpc"], "ptc": storages["ptc"]})
