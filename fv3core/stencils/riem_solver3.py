9ecedfbf69c2b6af7e36ad405d1a65992503bb15
Johann Dahm <johannd@vulcan.com>
2020-10-05 19:22:52 -0700

    Fix test and specify origin

--- fv3core/stencils/riem_solver3.py
+++ fv3core/stencils/riem_solver3.py
@@ -5,10 +5,10 @@ import numpy as np
 
 import fv3core._config as spec
 import fv3core.decorators as decorators
-import fv3core.stencils.basic_operations as basic
 import fv3core.stencils.sim1_solver as sim1_solver
 import fv3core.utils.global_constants as constants
 import fv3core.utils.gt4py_utils as utils
+from fv3core.stencils.basic_operations import copy
 
 
 sd = utils.sd
