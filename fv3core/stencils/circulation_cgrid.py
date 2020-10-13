316122092aa6def38ba3ed2aa9ed04f4822bf11d
Johann Dahm <johannd@vulcan.com>
2020-10-12 18:17:14 -0700

    Regions port: circulation_cgrid

--- fv3core/stencils/circulation_cgrid.py
+++ fv3core/stencils/circulation_cgrid.py
@@ -1,5 +1,5 @@
 import gt4py.gtscript as gtscript
-from gt4py.gtscript import PARALLEL, computation, interval
+from gt4py.gtscript import PARALLEL, computation, interval, parallel, region
 
 import fv3core._config as spec
 import fv3core.utils.gt4py_utils as utils
@@ -7,84 +7,29 @@ from fv3core.decorators import gtstencil
 
 
 sd = utils.sd
-origin = utils.origin
-
-# Flux field computation
-@gtstencil()
-def compute_flux(flux: sd, grid_spacing: sd, val_in: sd):
-    with computation(PARALLEL), interval(...):
-        flux[0, 0, 0] = val_in * grid_spacing
-
-
-# Vorticity field update (no corner value updates)
-@gtstencil()
-def update_vorticity(vorticity: sd, fx: sd, fy: sd):
-    with computation(PARALLEL), interval(...):
-        vorticity[0, 0, 0] = fx[0, -1, 0] - fx - fy[-1, 0, 0] + fy
-
-
-# Vorticity field update of corner values
-@gtstencil()
-def update_vorticity_western_corner(vorticity: sd, fy: sd):
-    with computation(PARALLEL), interval(...):
-        vorticity[0, 0, 0] = vorticity + fy[-1, 0, 0]
 
 
 @gtstencil()
-def update_vorticity_eastern_corner(vorticity: sd, fy: sd):
+def circulation_cgrid(uc: sd, vc: sd, dxc: sd, dyc: sd, vort_c: sd):
+    """Update vort_c.
+
+    Args:
+        uc: x-velocity on C-grid (input)
+        vc: y-velocity on C-grid (input)
+        dxc: grid spacing in x-dir (input)
+        dyc: grid spacing in y-dir (input)
+        vort_c: C-grid vorticity (output)
+    """
+    from __splitters__ import i_start, i_end, j_start, j_end
+
     with computation(PARALLEL), interval(...):
-        vorticity[0, 0, 0] = vorticity - fy
+        fx = dxc * uc
+        fy = dyc * vc
 
+        vort_c = fx[0, -1, 0] - fx - fy[-1, 0, 0] + fy
 
-def compute(uc, vc, vort_c):
-    grid = spec.grid
+        with parallel(region[i_start, j_start], region[i_start, j_end + 1]):
+            vort_c += fy[-1, 0, 0]
 
-    # Compute the zonal flux values
-    fx = utils.make_storage_from_shape(uc.shape, origin=(grid.is_ - 1, grid.js - 1, 0))
-    compute_flux(
-        fx,
-        uc,
-        grid.dxc,
-        origin=(grid.is_, grid.js - 1, 0),
-        domain=(grid.nic + 1, grid.njc + 2, grid.npz),
-    )
-
-    # Compute the meridional flux values
-    fy = utils.make_storage_from_shape(vc.shape, origin=(grid.is_ - 1, grid.js - 1, 0))
-    compute_flux(
-        fy,
-        vc,
-        grid.dyc,
-        origin=(grid.is_ - 1, grid.js, 0),
-        domain=(grid.nic + 2, grid.njc + 1, grid.npz),
-    )
-
-    # Update the vorticity (non-corner) values
-    update_vorticity(
-        vort_c,
-        fx,
-        fy,
-        origin=(grid.is_, grid.js, 0),
-        domain=(grid.nic + 1, grid.njc + 1, grid.npz),
-    )
-
-    # Update the vorticity corner values
-    if grid.sw_corner:
-        update_vorticity_western_corner(
-            vort_c, fy, origin=(grid.is_, grid.js, 0), domain=grid.corner_domain()
-        )
-    if grid.nw_corner:
-        update_vorticity_western_corner(
-            vort_c, fy, origin=(grid.is_, grid.je + 1, 0), domain=grid.corner_domain()
-        )
-    if grid.se_corner:
-        update_vorticity_eastern_corner(
-            vort_c, fy, origin=(grid.ie + 1, grid.js, 0), domain=grid.corner_domain()
-        )
-    if grid.ne_corner:
-        update_vorticity_eastern_corner(
-            vort_c,
-            fy,
-            origin=(grid.ie + 1, grid.je + 1, 0),
-            domain=grid.corner_domain(),
-        )
+        with parallel(region[i_end + 1, j_start], region[i_end + 1, j_end + 1]):
+            vort_c -= fy[0, 0, 0]
--- tests/translate/translate_circulation_cgrid.py
+++ tests/translate/translate_circulation_cgrid.py
@@ -1,4 +1,4 @@
-import fv3core.stencils.circulation_cgrid as Circulation_Cgrid
+from fv3core.stencils.circulation_cgrid import circulation_cgrid
 
 from .translate import TranslateFortranData2Py
 
@@ -6,7 +6,6 @@ from .translate import TranslateFortranData2Py
 class TranslateCirculation_Cgrid(TranslateFortranData2Py):
     def __init__(self, grid):
         super().__init__(grid)
-        self.compute_func = Circulation_Cgrid.compute
         self.in_vars["data_vars"] = {
             "uc": {},
             "vc": {},
@@ -25,3 +24,16 @@ class TranslateCirculation_Cgrid(TranslateFortranData2Py):
                 "jend": grid.je + 1,
             }
         }
+
+    def compute(self, inputs):
+        self.make_storage_data_input_vars(inputs)
+        circulation_cgrid(
+            inputs["uc"],
+            inputs["vc"],
+            self.grid.dxc,
+            self.grid.dyc,
+            inputs["vort_c"],
+            origin=self.grid.compute_origin(),
+            domain=self.grid.domain_shape_compute_buffer_2d(add=(1, 1, 0)),
+        )
+        return self.slice_output({"vort_c": inputs["vort_c"]})
