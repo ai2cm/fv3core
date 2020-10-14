59bfe846ae4a847987f7724308ac6a77ab934ceb
Eddie Davis <eddied@vulcan.com>
2020-10-14 23:42:06 +0000

    Fix Fill4Corners test for GT backends

--- tests/translate/translate_fill4corners.py
+++ tests/translate/translate_fill4corners.py
@@ -1,3 +1,4 @@
+import fv3core.utils.gt4py_utils as utils
 from fv3core.utils import corners
 
 from .translate import TranslateFortranData2Py
@@ -11,6 +12,7 @@ class TranslateFill4Corners(TranslateFortranData2Py):
         self.out_vars = {"q4c": {}}
 
     def compute(self, inputs):
+        inputs["q4c"] = utils.make_storage_data(inputs["q4c"], inputs["q4c"].shape)
         corners.fill_4corners(
             inputs["q4c"], "x" if inputs["dir"] == 1 else "y", self.grid
         )
