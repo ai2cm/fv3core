24068524bd6ff4c189ad4178596add1117af2f03
Jeremy McGibbon <jeremym@vulcan.com>
2020-10-15 16:36:17 -0700

    remove mentions of HISTORY.md

--- setup.py
+++ setup.py
@@ -9,9 +9,6 @@ from setuptools import find_packages, setup
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open("HISTORY.md") as history_file:
-    history = history_file.read()
-
 requirements = ["f90nml>=1.1.0", "gt4py", "numpy", "fv3gfs-util>=0.4.3"]
 
 setup_requirements = []
@@ -35,7 +32,7 @@ setup(
     install_requires=requirements,
     extras_require={},
     license="BSD license",
-    long_description=readme + "\n\n" + history,
+    long_description=readme,
     include_package_data=True,
     keywords="fv3core",
     name="fv3core",
