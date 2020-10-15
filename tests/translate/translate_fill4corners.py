5205f14d3fd0b8db167df5f28864ec9f9af2f414
Eddie Davis <eddied@vulcan.com>
2020-10-14 20:21:13 -0700

    Add calls to 'make_storage_data_input_vars' and 'slice_output'

--- tests/translate/translate_fill4corners.py
+++ tests/translate/translate_fill4corners.py
@@ -1,4 +1,3 @@
-import fv3core.utils.gt4py_utils as utils
 from fv3core.utils import corners
 
 from .translate import TranslateFortranData2Py
@@ -12,8 +11,8 @@ class TranslateFill4Corners(TranslateFortranData2Py):
         self.out_vars = {"q4c": {}}
 
     def compute(self, inputs):
-        inputs["q4c"] = utils.make_storage_data(inputs["q4c"], inputs["q4c"].shape)
+        self.make_storage_data_input_vars(inputs)
         corners.fill_4corners(
             inputs["q4c"], "x" if inputs["dir"] == 1 else "y", self.grid
         )
-        return {"q4c": inputs["q4c"]}
+        return self.slice_output(inputs, {"q4c": inputs["q4c"]})
