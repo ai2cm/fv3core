bfab809c724d0a305d58a35d7918ac7948565846
Jeremy McGibbon <jeremym@vulcan.com>
2020-10-15 10:21:24 -0700

    remove old dockerfiles, rename dependency dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,12 +3,15 @@
 set -e
 
 
-# install /external python packages including wrapper
-if [[ -d /external/fv3gfs-wrapper ]]
+if [[ -d /external/fv3gfs-fortran ]]
 then
     # Re-compile the fortran and wrapper sources
     make -C /external/fv3gfs-fortran/FV3
     PREFIX=/usr/local make -C /external/fv3gfs-fortran/FV3 install
+fi
+# install /external python packages including wrapper
+if [[ -d /external/fv3gfs-wrapper ]]
+then
     CC=mpicc MPI=mpich make -C /external/fv3gfs-wrapper build
 fi
 for package in /external/*
