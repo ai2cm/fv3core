2c3f972fa7d664ae0e4e495899d8299a18683612
Eddie Davis <eddied@vulcan.com>
2020-10-30 00:07:09 +0000

    Add GPU_NEAR_ZERO

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -18,7 +18,9 @@ np.set_printoptions(threshold=4096)
 
 OUTDIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "output")
 GPU_MAX_ERR = 1e-10
-GPU_SMALL_ERR = 1e-14
+GPU_NEAR_ZERO = 1e-15
+
+_near_zero = 1e-18
 
 
 def compare_arr(computed_data, ref_data):
@@ -35,11 +37,10 @@ def success_array(computed_data, ref_data, eps, ignore_near_zero_errors):
         # np.isclose(computed_data, ref_data, rtol=eps * 1e-2, atol=eps * 1e-2),
     )
     if ignore_near_zero_errors:
-        small_number = GPU_SMALL_ERR if "cuda" in gt_utils.backend else 1e-18
         success = np.logical_or(
             success,
             np.logical_and(
-                np.abs(computed_data) < small_number, np.abs(ref_data) < small_number
+                np.abs(computed_data) < _near_zero, np.abs(ref_data) < _near_zero
             ),
         )
     return success
@@ -131,6 +132,7 @@ def test_sequential_savepoint(
     # Reduce error threshold for GPU
     if backend.endswith("cuda") and testobj.max_error < GPU_MAX_ERR:
         testobj.max_error = GPU_MAX_ERR
+        _near_zero = GPU_NEAR_ZERO
     fv3core._config.set_grid(grid)
     input_data = testobj.collect_input_data(serializer, savepoint_in)
     # run python version of functionality
@@ -209,6 +211,7 @@ def test_mock_parallel_savepoint(
     # Reduce error threshold for GPU
     if backend.endswith("cuda") and testobj.max_error < GPU_MAX_ERR:
         testobj.max_error = GPU_MAX_ERR
+        _near_zero = GPU_NEAR_ZERO
     fv3core._config.set_grid(grid)
     inputs_list = []
     for savepoint_in, serializer in zip(savepoint_in_list, serializer_list):
