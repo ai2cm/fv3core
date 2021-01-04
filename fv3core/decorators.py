41750a5a47d7e9f7c0c619b855aa0f6e282d47ad
Johann Dahm <johannd@vulcan.com>
2021-01-04 12:06:02 -0800

    Update decorators.py

--- fv3core/decorators.py
+++ fv3core/decorators.py
@@ -222,7 +222,7 @@ class FV3StencilObject:
             }
 
             if stencil_kwargs["backend"].startswith("gt"):
-                stencil_kwargs["debug_mode"] = global_config.get_debug()
+                stencil_kwargs["debug_mode"] = global_config.get_cpp_debug()
 
             # gtscript.stencil always returns a new class instance even if it
             # used the cached module.
