65a9f1ce0e0fe44af91c02d14a867c52c01dd454
rheacangeo <rheag@vulcan.com>
2021-01-12 18:19:48 +0000

    update_gtpy action should build and run the gt4py tests on gpu

--- .jenkins/actions/update_gt4py.sh
+++ .jenkins/actions/update_gt4py.sh
@@ -2,9 +2,11 @@
 set -e
 set -x
 # Run when we change the gt4py source code
-
+export CUDA=y
 make pull_environment
 make -C docker build_gt4py
+make container_gt4py_tests
 make tests
 make tests_mpi
-make -C docker push_gt4py
+# TODO uncomment when verify the above works
+#make -C docker push_gt4py
