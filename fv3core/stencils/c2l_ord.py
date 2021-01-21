3177d24415993518fe5d6c6bf6a58b2d1dacbfc8
Johann Dahm <johannd@vulcan.com>
2021-01-21 11:43:03 -0800

    Remove compute_ord2

--- fv3core/stencils/c2l_ord.py
+++ fv3core/stencils/c2l_ord.py
@@ -1,8 +1,10 @@
-from gt4py.gtscript import PARALLEL, computation, interval, parallel, region
+from gt4py.gtscript import PARALLEL, computation, horizontal, interval, region
 
 import fv3core._config as spec
 import fv3core.utils.gt4py_utils as utils
 from fv3core.decorators import gtstencil
+from fv3core.utils.typing import FloatField
+from fv3gfs.util import CubedSphereCommunicator
 
 
 sd = utils.sd
@@ -13,7 +15,16 @@ C2 = -0.125
 
 @gtstencil()
 def c2l_ord2(
-    u: sd, v: sd, dx: sd, dy: sd, a11: sd, a12: sd, a21: sd, a22: sd, ua: sd, va: sd
+    u: FloatField,
+    v: FloatField,
+    dx: FloatField,
+    dy: FloatField,
+    a11: FloatField,
+    a12: FloatField,
+    a21: FloatField,
+    a22: FloatField,
+    ua: FloatField,
+    va: FloatField,
 ):
     with computation(PARALLEL), interval(...):
         wu = u * dx
@@ -26,38 +37,18 @@ def c2l_ord2(
         va = a21 * u1 + a22 * v1
 
 
-def compute_ord2(u, v, ua, va, do_halo=False):
-    grid = spec.grid
-    i1 = grid.is_
-    i2 = grid.ie
-    j1 = grid.js
-    j2 = grid.je
-    # Usually used for nesting
-    if do_halo:
-        i1 -= -1
-        i2 += 1
-        j1 -= 1
-        j2 += 1
-
-    c2l_ord2(
-        u,
-        v,
-        grid.dx,
-        grid.dy,
-        grid.a11,
-        grid.a12,
-        grid.a21,
-        grid.a22,
-        ua,
-        va,
-        origin=(i1, j1, 0),
-        domain=(i2 - i1 + 1, j2 - j1 + 1, grid.npz),
-    )
-
-
 @gtstencil()
 def ord4_transform(
-    u: sd, v: sd, a11: sd, a12: sd, a21: sd, a22: sd, dx: sd, dy: sd, ua: sd, va: sd
+    u: FloatField,
+    v: FloatField,
+    a11: FloatField,
+    a12: FloatField,
+    a21: FloatField,
+    a22: FloatField,
+    dx: FloatField,
+    dy: FloatField,
+    ua: FloatField,
+    va: FloatField,
 ):
     with computation(PARALLEL), interval(...):
         from __externals__ import i_end, i_start, j_end, j_start
@@ -66,12 +57,12 @@ def ord4_transform(
         vtmp = C2 * (v[-1, 0, 0] + v[2, 0, 0]) + C1 * (v + v[1, 0, 0])
 
         # south/north edge
-        with parallel(region[:, j_start], region[:, j_end]):
+        with horizontal(region[:, j_start], region[:, j_end]):
             vtmp = 2.0 * ((v * dy) + (v[1, 0, 0] * dy[1, 0, 0])) / (dy + dy[1, 0, 0])
             utmp = 2.0 * (u * dx + u[0, 1, 0] * dx[0, 1, 0]) / (dx + dx[0, 1, 0])
 
         # west/east edge
-        with parallel(region[i_start, :], region[i_end, :]):
+        with horizontal(region[i_start, :], region[i_end, :]):
             utmp = 2.0 * ((u * dx) + (u[0, 1, 0] * dx[0, 1, 0])) / (dx + dx[0, 1, 0])
             vtmp = 2.0 * ((v * dy) + (v[1, 0, 0] * dy[1, 0, 0])) / (dy + dy[1, 0, 0])
 
@@ -80,7 +71,14 @@ def ord4_transform(
         va = a21 * utmp + a22 * vtmp
 
 
-def compute_cubed_to_latlon(u, v, ua, va, comm, mode=True):
+def compute_cubed_to_latlon(
+    u: FloatField,
+    v: FloatField,
+    ua: FloatField,
+    va: FloatField,
+    comm: CubedSphereCommunicator,
+    halo_update_uv: bool = True,
+):
     """
     Interpolate D-grid winds to into a-grid winds at latitude-longitude.
     Args:
@@ -88,14 +86,32 @@ def compute_cubed_to_latlon(u, v, ua, va, comm, mode=True):
         v: y-wind on D-grid (in)
         ua: x-wind on A-grid (out)
         va: y-wind on A-grid (out)
-        comm: MPI communicator, in case of a halo update (in)
+        comm: Communicator in case of halo update (in)
         mode: If True, halo update before transforming to lat/lon
     """
-    if spec.namelist.c2l_ord == 2:
-        compute_ord2(u, v, ua, va, False)
+    namelist = spec.namelist
+    grid = spec.grid
+    if namelist.c2l_ord == 2:
+        c2l_ord2(
+            u,
+            v,
+            grid.dx,
+            grid.dy,
+            grid.a11,
+            grid.a12,
+            grid.a21,
+            grid.a22,
+            ua,
+            va,
+            origin=grid.compute_origin(
+                add=(-1, -1, 0) if halo_update_uv else (0, 0, 0)
+            ),
+            domain=grid.domain_shape_compute(
+                add=(2, 2, 0) if halo_update_uv else (0, 0, 0)
+            ),
+        )
     else:
-        grid = spec.grid
-        if mode:
+        if halo_update_uv:
             comm.vector_halo_update(u, v, n_points=utils.halo)
         ord4_transform(
             u.storage,
