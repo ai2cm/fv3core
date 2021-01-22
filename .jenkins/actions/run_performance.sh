cf76ff70afc4e74aa84bc52997ea38737ecd8f86
tobias <twicki12@gmail.com>
2021-01-22 13:26:41 +0100

    change target path

--- .jenkins/actions/run_performance.sh
+++ .jenkins/actions/run_performance.sh
@@ -13,4 +13,4 @@ if [ "$experiment" = "c12_6ranks_standard" ]; then
     data_path="/project/s1053/fv3core_serialized_test_data/7.0.0/c12_6ranks_standard"
 fi
 
-source $ROOT_DIR/examples/standalone/benchmarks/run_on_daint.sh 60 6 $backend /project/s1053/performance/python/$backend/ $data_path
+source $ROOT_DIR/examples/standalone/benchmarks/run_on_daint.sh 60 6 $backend /project/s1053/performance/fv3core_monitor/$backend/ $data_path
