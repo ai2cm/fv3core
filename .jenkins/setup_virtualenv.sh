a940c69b4718ec09e39252241180916b72bac969
Rhea George <rheag@vucan.com>
2021-01-24 08:02:42 +0100

    making setup_virtualenv a jenkins script rather than an action because we do not want it launched as a slurm job

--- .jenkins/setup_virtualenv.sh
+++ .jenkins/setup_virtualenv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e -x
+export JENKINS_TAG=${JOB_NAME}-${BUILD_NUMBER}
 if [ -z ${VIRTUALENV} ]; then
     echo "setting VIRTUALENV"
     export VIRTUALENV=${SCRATCH}/vcm_env_${JENKINS_TAG}
