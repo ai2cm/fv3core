90595255e67d20ced26eb4d0e08fe1e5d634d1d8
Rhea George <rheag@vulcan.com>
2021-01-25 15:41:22 +0000

    combine venv and docker plans

--- .jenkins/actions/run_parallel_regression_tests.sh
+++ .jenkins/actions/run_parallel_regression_tests.sh
@@ -2,10 +2,13 @@
 set -e -x
 BACKEND=$1
 EXPNAME=$2
-ARGS="-v -s -rsx --backend=${BACKEND} ${THRESH_ARGS}"
+export TEST_ARGS="-v -s -rsx --which_modules=CubedToLatLon --backend=${BACKEND} ${THRESH_ARGS}"
 
 # sync the test data
 make get_test_data
 
-# The default of this set to 1 causes a segfault
-make run_tests_parallel TEST_ARGS="${ARGS}"
+if [ ${IN_DOCKER} == "True" ]; then
+    make run_tests_parallel
+else
+    make test_venv_parallel
+fi
--- .jenkins/actions/run_regression_tests.sh
+++ .jenkins/actions/run_regression_tests.sh
@@ -2,9 +2,15 @@
 set -e -x
 BACKEND=$1
 EXPNAME=$2
-ARGS="-v -s -rsx --backend=${BACKEND} --junitxml=/.jenkins/sequential_test_results.xml ${THRESH_ARGS} --which_modules=XPPM"
+XML_REPORT="sequential_test_results.xml"
+export TEST_ARGS="-v -s -rsx --backend=${BACKEND} ${THRESH_ARGS} --which_modules=XPPM"
 
 # sync the test data
 make get_test_data
-
-make run_tests_sequential TEST_ARGS="${ARGS}"
+if [ ${IN_DOCKER} == "True" ]; then
+    export TEST_ARGS="${TEST_ARGS} --junitxml=/.jenkins/${XML_REPORT}"
+    make run_tests_sequential
+else
+    export TEST_ARGS="${TEST_ARGS} --junitxml=/${envloc}/${XML_REPORT}"
+    BASH_PREFIX="srun" make test_venv
+fi
