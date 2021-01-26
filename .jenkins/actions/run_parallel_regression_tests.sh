eff667c27ec134c63e6f33cb501b80167ef9a62a
Rhea George <rheag@vulcan.com>
2021-01-26 04:16:58 +0000

    run all the tests

--- .jenkins/actions/run_parallel_regression_tests.sh
+++ .jenkins/actions/run_parallel_regression_tests.sh
@@ -2,7 +2,7 @@
 set -e -x
 BACKEND=$1
 EXPNAME=$2
-export TEST_ARGS="-v -s -rsx --which_modules=CubedToLatLon --backend=${BACKEND} ${THRESH_ARGS}"
+export TEST_ARGS="-v -s -rsx --backend=${BACKEND} ${THRESH_ARGS}"
 
 # sync the test data
 make get_test_data
--- .jenkins/actions/run_regression_tests.sh
+++ .jenkins/actions/run_regression_tests.sh
@@ -3,12 +3,12 @@ set -e -x
 BACKEND=$1
 EXPNAME=$2
 XML_REPORT="sequential_test_results.xml"
-export TEST_ARGS="-v -s -rsx --backend=${BACKEND} ${THRESH_ARGS} --which_modules=XPPM"
+export TEST_ARGS="-v -s -rsx --backend=${BACKEND} ${THRESH_ARGS}"
 
 # sync the test data
 make get_test_data
 if [ ${host} == "daint" ]; then
-     export TEST_ARGS="${TEST_ARGS} --junitxml=${root}/.jenkins/${XML_REPORT}"
+     export TEST_ARGS="${TEST_ARGS} --junitxml=${root}/${XML_REPORT}"
      BASH_PREFIX="srun" make test_venv
 else
     export TEST_ARGS="${TEST_ARGS} --junitxml=/.jenkins/${XML_REPORT}"
