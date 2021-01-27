cd55b6475580e57c6a67a1e9ea2970223d7964e0
Rhea George <rheag@vulcan.com>
2021-01-27 03:51:50 +0000

    sequential pytest should not need srun

--- .jenkins/actions/run_regression_tests.sh
+++ .jenkins/actions/run_regression_tests.sh
@@ -9,7 +9,8 @@ export TEST_ARGS="-v -s -rsx --backend=${BACKEND} ${THRESH_ARGS} --which_modules
 make get_test_data
 if [ ${python_env} == "virtualenv" ]; then
      export TEST_ARGS="${TEST_ARGS} --junitxml=${root}/${XML_REPORT}"
-     BASH_PREFIX="srun" make tests_venv
+     #BASH_PREFIX="srun"
+     make tests_venv
 else
     export TEST_ARGS="${TEST_ARGS} --junitxml=/.jenkins/${XML_REPORT}"
     make tests
