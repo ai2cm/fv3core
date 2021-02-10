e35ad18d1df1e3f9898a34d3b6ae691d828dd417
Johann Dahm <johannd@vulcan.com>
2021-02-10 15:24:12 -0800

    Revert validation fields

--- tests/translate/translate_d2a2c_vect.py
+++ tests/translate/translate_d2a2c_vect.py
@@ -69,12 +69,12 @@ class TranslateD2A2C_Vect(TranslateFortranData2Py):
         }
         self.in_vars["parameters"] = ["dord4"]
         self.out_vars = {
-            # "uc": grid.x3d_domain_dict(),
-            # "vc": grid.y3d_domain_dict(),
+            "uc": grid.x3d_domain_dict(),
+            "vc": grid.y3d_domain_dict(),
             "ua": {},
             "va": {},
-            # "utc": {},
-            # "vtc": {},
+            "utc": {},
+            "vtc": {},
         }
         # TODO: This seems to be needed primarily for the edge_interpolate_4
         # methods, can we rejigger the order of operations to make it match to
@@ -97,18 +97,18 @@ class TranslateD2A2C_Vect(TranslateFortranData2Py):
         assert bool(inputs["dord4"]) is True
         del inputs["dord4"]
         stencil(
-            self.grid.cosa_s,
-            self.grid.cosa_u,
-            self.grid.cosa_v,
-            self.grid.dxa,
-            self.grid.dya,
-            self.grid.rsin2,
-            self.grid.rsin_u,
-            self.grid.rsin_v,
-            self.grid.sin_sg1,
-            self.grid.sin_sg2,
-            self.grid.sin_sg3,
-            self.grid.sin_sg4,
+            cosa_s=self.grid.cosa_s,
+            cosa_u=self.grid.cosa_u,
+            cosa_v=self.grid.cosa_v,
+            dxa=self.grid.dxa,
+            dya=self.grid.dya,
+            rsin2=self.grid.rsin2,
+            rsin_u=self.grid.rsin_u,
+            rsin_v=self.grid.rsin_v,
+            sin_sg1=self.grid.sin_sg1,
+            sin_sg2=self.grid.sin_sg2,
+            sin_sg3=self.grid.sin_sg3,
+            sin_sg4=self.grid.sin_sg4,
             **inputs,
             origin=self.grid.compute_origin(add=(-2, -2, 0)),
             domain=self.grid.domain_shape_compute(add=(4, 4, 0)),
