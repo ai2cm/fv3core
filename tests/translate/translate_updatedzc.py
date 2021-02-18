b4da4ff71e0803a1c1a8bc8f547e392a072bc629
Rhea George <rheag@vulcan.com>
2021-02-18 21:22:36 +0000

    updatedzc consolidated to a single stencil, called in a compute function, validating

--- tests/translate/translate_updatedzc.py
+++ tests/translate/translate_updatedzc.py
@@ -5,22 +5,32 @@ from fv3core.testing import TranslateFortranData2Py
 class TranslateUpdateDzC(TranslateFortranData2Py):
     def __init__(self, grid):
         super().__init__(grid)
-        self.compute_func = updatedzc.compute
+        self.compute_func=updatedzc.compute
         self.in_vars["data_vars"] = {
             "dp_ref": {"serialname": "dp0"},
             "zs": {},
             "ut": {"serialname": "utc"},
             "vt": {"serialname": "vtc"},
-            "gz_in": {"serialname": "gz"},
-            "ws3": {"serialname": "ws"},
+            "gz": {},
+            "ws": {}
         }
         self.in_vars["parameters"] = ["dt2"]
         self.out_vars = {
             "gz": grid.default_buffer_k_dict(),
             "ws": {"kstart": -1, "kend": None},
         }
-
+    '''
     def compute(self, inputs):
         self.make_storage_data_input_vars(inputs)
-        gz, ws = self.compute_func(**inputs)
-        return self.slice_output(inputs, {"gz": gz, "ws": ws})
+        updatedzc.update_dz_c_stencil(
+            area=self.grid.area,
+            **inputs,
+            origin=self.grid.compute_origin(add=(-2, -2, 0)),
+            domain = self.grid.domain_shape_compute(add=(3, 3, 1))
+        )
+        return self.slice_output(inputs)
+    '''
+
+
+    
+    
