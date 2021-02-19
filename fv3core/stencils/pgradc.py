b28820a153cc26dbdd6c3d27fa2f8d64f4bc5ea5
Rhea George <rheag@vulcan.com>
2021-02-19 00:11:42 +0000

    intermediate step

--- fv3core/stencils/pgradc.py
+++ fv3core/stencils/pgradc.py
@@ -9,31 +9,45 @@ from fv3core.utils.typing import FloatField
 
 @gtstencil()
 def p_grad_c_stencil(
-        uc: FloatField, vc: FloatField, delpc: FloatField, pkc: FloatField, gz: FloatField, rdxc: FloatField, rdyc:FloatField, dt2: float
+    uc: FloatField,
+    vc: FloatField,
+    delpc: FloatField,
+    pkc: FloatField,
+    gz: FloatField,
+    rdxc: FloatField,
+    rdyc: FloatField,
+    dt2: float,
 ):
-    from __externals__ import local_is, local_ie, local_js, local_je, namelist
+    from __externals__ import local_ie, local_is, local_je, local_js, namelist
+
     with computation(PARALLEL), interval(...):
         if __INLINED(namelist.hydrostatic):
             wk = pkc[0, 0, 1] - pkc
         else:
             wk = delpc
-        with horizontal(region[local_is:local_ie+2,local_js:local_je+1]):
+        with horizontal(region[local_is : local_ie + 2, local_js : local_je + 1]):
             uc = uc + dt2 * rdxc / (wk[-1, 0, 0] + wk) * (
-                (gz[-1, 0, 1] - gz) * (pkc[0, 0, 1] - pkc[-1, 0, 0]) + (gz[-1, 0, 0] - gz[0, 0, 1]) * (pkc[-1, 0, 1] - pkc))
-        with horizontal(region[local_is:local_ie+1,local_js:local_je+2]):
+                (gz[-1, 0, 1] - gz) * (pkc[0, 0, 1] - pkc[-1, 0, 0])
+                + (gz[-1, 0, 0] - gz[0, 0, 1]) * (pkc[-1, 0, 1] - pkc)
+            )
+        with horizontal(region[local_is : local_ie + 1, local_js : local_je + 2]):
             vc = vc + dt2 * rdyc / (wk[0, -1, 0] + wk) * (
                 (gz[0, -1, 1] - gz) * (pkc[0, 0, 1] - pkc[0, -1, 0])
                 + (gz[0, -1, 0] - gz[0, 0, 1]) * (pkc[0, -1, 1] - pkc)
             )
 
+
 def compute(uc, vc, delpc, pkc, gz, dt2):
     grid = spec.grid
     p_grad_c_stencil(
-        uc,vc,
+        uc,
+        vc,
         delpc,
         pkc,
         gz,
-        grid.rdxc, grid.rdyc,
+        grid.rdxc,
+        grid.rdyc,
         dt2=dt2,
         origin=grid.compute_origin(),
-        domain=grid.domain_shape_compute(add=(1, 1, 0)))
+        domain=grid.domain_shape_compute(add=(1, 1, 0)),
+    )
