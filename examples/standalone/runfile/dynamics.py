30710833eba39677a8faaf9f28ecdea2c541d84e
tobias <twicki12@gmail.com>
2021-02-23 22:25:11 +0100

    disable halo exchange as argument to dynamics

--- examples/standalone/runfile/dynamics.py
+++ examples/standalone/runfile/dynamics.py
@@ -12,10 +12,13 @@ import fv3core._config as spec
 import fv3core.stencils.fv_dynamics as fv_dynamics
 import fv3core.testing
 import fv3gfs.util as util
+from fv3core.utils import gt4py_utils
 
 
 def parse_args():
-    usage = "usage: python %(prog)s <data_dir> <timesteps> <backend> <hash>"
+    usage = (
+        "usage: python %(prog)s <data_dir> <timesteps> <backend> <hash> <halo_exchange>"
+    )
     parser = ArgumentParser(usage=usage)
 
     parser.add_argument(
@@ -41,6 +44,12 @@ def parse_args():
         type=str,
         action="store",
         help="git hash to store",
+    )
+    parser.add_argument(
+        "halo_exchange",
+        type=bool,
+        action="store",
+        help="enable or disable the halo exchange",
     )
 
     return parser.parse_args()
@@ -99,6 +108,8 @@ if __name__ == "__main__":
 
         fv3core.set_backend(args.backend)
         fv3core.set_rebuild(False)
+
+        gt4py_utils._do_halo_exchange = args.halo_exchange
 
         spec.set_namelist(args.data_dir + "/input.nml")
 
