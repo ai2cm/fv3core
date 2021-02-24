713c38b2d6795cdd1511f759986c340891cb601f
Eddie Davis <eddied@vulcan.com>
2021-02-24 23:11:37 +0000

    Update PressureAdjustedTemperature_NonHydrostatic

--- fv3core/stencils/temperature_adjust.py
+++ fv3core/stencils/temperature_adjust.py
@@ -1,20 +1,29 @@
-import numpy as np
 from gt4py.gtscript import PARALLEL, computation, exp, interval, log
 
 import fv3core._config as spec
 import fv3core.utils.global_constants as constants
-import fv3core.utils.gt4py_utils as utils
 from fv3core.decorators import gtstencil
 from fv3core.stencils.basic_operations import sign
-
-
-sd = utils.sd
+from fv3core.utils.typing import FloatField
 
 
 @gtstencil()
 def compute_pkz_tempadjust(
-    delp: sd, delz: sd, cappa: sd, heat_source: sd, delt: sd, pt: sd, pkz: sd
+    delp: FloatField,
+    delz: FloatField,
+    cappa: FloatField,
+    heat_source: FloatField,
+    pt: FloatField,
+    pkz: FloatField,
+    delt_factor: float,
 ):
+    with computation(PARALLEL):
+        with interval(0, 1):
+            delt = delt_factor * 0.1
+        with interval(1, 2):
+            delt = delt_factor * 0.5
+        with interval(2, None):
+            delt = delt_factor
     with computation(PARALLEL), interval(...):
         pkz = exp(cappa / (1.0 - cappa) * log(constants.RDG * delp / delz * pt))
         pkz = (constants.RDG * delp / delz * pt) ** (cappa / (1.0 - cappa))
@@ -24,20 +33,25 @@ def compute_pkz_tempadjust(
 
 
 # TODO use stencils. limited by functions exp, log and variable that depends on k
-def compute(pt, pkz, heat_source, delz, delp, cappa, n_con, bdt):
+def compute(
+    pt: FloatField,
+    pkz: FloatField,
+    heat_source: FloatField,
+    delz: FloatField,
+    delp: FloatField,
+    cappa: FloatField,
+    n_con: int,
+    bdt: float,
+):
     grid = spec.grid
-    delt_column = np.ones(delz.shape[2]) * abs(bdt * spec.namelist.delt_max)
-    delt_column[0] *= 0.1
-    delt_column[1] *= 0.5
-    delt = utils.make_storage_data(delt_column, delz.shape, origin=grid.full_origin())
     compute_pkz_tempadjust(
         delp,
         delz,
         cappa,
         heat_source,
-        delt,
         pt,
         pkz,
+        abs(bdt * spec.namelist.delt_max),
         origin=grid.compute_origin(),
         domain=(grid.nic, grid.njc, n_con),
     )
