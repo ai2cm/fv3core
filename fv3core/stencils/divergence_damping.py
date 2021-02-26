0b9fffe70ec5415b8cb8c30a1437c7e0a3fd4391
Rhea George <rheag@vulcan.com>
2021-02-26 06:12:25 +0000

    less stencils and less

--- fv3core/stencils/divergence_damping.py
+++ fv3core/stencils/divergence_damping.py
@@ -14,7 +14,7 @@ sd = utils.sd
 
 
 @gtstencil()
-def ptc_main(u: sd, v: sd, ua: sd, va: sd, uc: sd, vc: sd, cosa_u: sd, cosa_v: sd, sina_u: sd, sina_v: sd, dxc: sd, dyc: sd, sin_sg1: sd, sin_sg2:sd, sin_sg3: sd, sin_sg4: sd, ptc: sd, vort: sd):
+def ptc_main(u: sd, v: sd, ua: sd, va: sd, uc: sd, vc: sd, cosa_u: sd, cosa_v: sd, sina_u: sd, sina_v: sd, dxc: sd, dyc: sd, sin_sg1: sd, sin_sg2:sd, sin_sg3: sd, sin_sg4: sd, ptc: sd, vort: sd, delpc: sd):
     from __externals__ import i_start, i_end, j_start, j_end, local_is, local_ie, local_js, local_je
     with computation(PARALLEL), interval(...):
         ptc = (u - 0.5 * (va[0, -1, 0] + va) * cosa_v) * dyc * sina_v
@@ -27,47 +27,30 @@ def ptc_main(u: sd, v: sd, ua: sd, va: sd, uc: sd, vc: sd, cosa_u: sd, cosa_v: s
             vort = vort_copy
         with horizontal(region[i_start, :], region[i_end + 1, :]):
             vort = v * dxc * sin_sg3[-1, 0, 0] if uc > 0 else v * dxc * sin_sg1
-#@gtstencil()
-#def ptc_y_edge(u: sd, vc: sd, dyc: sd, sin_sg4: sd, sin_sg2: sd, ptc: sd):
-#    with computation(PARALLEL), interval(...):
-#        ptc[0, 0, 0] = u * dyc * sin_sg4[0, -1, 0] if vc > 0 else u * dyc * sin_sg2
-
-
-#@gtstencil()
-#def vorticity_main(v: sd, ua: sd, cosa_u: sd, sina_u: sd, dxc: sd, vort: sd):
-#    with computation(PARALLEL), interval(...):
-#        vort[0, 0, 0] = (v - 0.5 * (ua[-1, 0, 0] + ua) * cosa_u) * dxc * sina_u
-
-
-@gtstencil()
-def vorticity_x_edge(v: sd, uc: sd, dxc: sd, sin_sg3: sd, sin_sg1: sd, vort: sd):
-    with computation(PARALLEL), interval(...):
-        vort[0, 0, 0] = v * dxc * sin_sg3[-1, 0, 0] if uc > 0 else v * dxc * sin_sg1
-
-
-@gtstencil()
-def delpc_main(vort: sd, ptc: sd, delpc: sd):
-    with computation(PARALLEL), interval(...):
-        delpc[0, 0, 0] = vort[0, -1, 0] - vort + ptc[-1, 0, 0] - ptc
+        with horizontal(region[local_is: local_ie + 2, local_js: local_je + 2]):
+            delpc = vort[0, -1, 0] - vort + ptc[-1, 0, 0] - ptc
+        with horizontal(region[i_start, j_start], region[i_end + 1, j_start]):
+            delpc = remove_extra_term_south_corner(vort, delpc)
+        with horizontal(region[i_start, j_end + 1], region[i_end + 1, j_end + 1]):
+            delpc= remove_extra_term_north_corner(vort, delpc)
 
 @gtscript.function
 def remove_extra_term_south_corner(extra: sd, field: sd):
+    # from __externals__ import i_start, i_end,  j_start
+    # TODO: why does this not work?
+    # with horizontal(region[i_start, j_start], region[i_end + 1, j_start]):
+    #    field = field - extra[0, -1, 0]
+    # return field
     return field - extra[0, -1, 0]
-
-@gtstencil()
-def corner_south_remove_extra_term(vort: sd, delpc: sd):
-    with computation(PARALLEL), interval(...):
-        delpc = remove_extra_term_south_corner(vort, delpc)
 
 @gtscript.function
 def remove_extra_term_north_corner(extra: sd, field: sd):
+    # TODO: why does this not work?
+    # from __externals__ import i_start, i_end, j_end
+    # with horizontal(region[i_start, j_end + 1], region[i_end + 1, j_end + 1]):
+    #     field = field + extra
+    # return field
     return field + extra
-
-@gtstencil()
-def corner_north_remove_extra_term(vort: sd, delpc: sd):
-    with computation(PARALLEL), interval(...):
-        delpc = remove_extra_term_north_corner(vort, delpc)
-
 
 @gtscript.function
 def damp_tmp(q, da_min_c, d2_bg, dddmp):
@@ -204,7 +187,7 @@ def damping_nt0(rarea_c: sd, divg_u: sd, divg_v: sd, divg_d: sd, uc: sd, vc: sd)
     with horizontal(region[local_is:local_ie + 2 , local_js - 1:local_je + 2  ]):
         uc = uc_from_divg(divg_d, divg_v)
     
-    with horizontal(region[local_is - 0:local_ie + 2 , local_js:local_je + 2  ]):
+    with horizontal(region[local_is:local_ie + 2 , local_js:local_je + 2  ]):
         divg_d = redo_divg_d(uc, vc)
     with horizontal(region[i_start, j_start], region[i_end + 1, j_start]):
         divg_d = remove_extra_term_south_corner(uc, divg_d)
@@ -294,97 +277,37 @@ def damping_zero_order(
     compute_domain = (grid.nic + 1, grid.njc + 1, nk)
     is2 = grid.is_ + 1 if grid.west_edge else grid.is_
     ie1 = grid.ie if grid.east_edge else grid.ie + 1
-    if not grid.nested:
-        # TODO: ptc and vort are equivalent, but x vs y, consolidate if possible.
-        ptc_main(
-            u, v,
-            ua, va, uc, vc,
-            grid.cosa_u, grid.cosa_v,
-            grid.sina_u, grid.sina_v,
-            grid.dxc, grid.dyc, grid.sin_sg1, grid.sin_sg2,grid.sin_sg3, grid.sin_sg4,
-            ptc,vort,
-            origin=(grid.is_ - 1, grid.js - 1, kstart),
-            domain=(grid.nic + 2, grid.njc + 2, nk),
-        )
-        #y_edge_domain = (grid.nic + 2, 1, nk)
-        #if grid.south_edge:
-        #    ptc_y_edge(
-        #        u,
-        #        vc,
-        #        grid.dyc,
-        #        grid.sin_sg4,
-        #        grid.sin_sg2,
-        #        ptc,
-        #        origin=(grid.is_ - 1, grid.js, kstart),
-        #        domain=y_edge_domain,
-        #    )
-        #if grid.north_edge:
-        #    ptc_y_edge(
-        #        u,
-        #        vc,
-        #        grid.dyc,
-        #        grid.sin_sg4,
-        #        grid.sin_sg2,
-        #        ptc,
-        #        origin=(grid.is_ - 1, grid.je + 1, kstart),
-        #        domain=y_edge_domain,
-        #    )
-
-        #vorticity_main(
-        #    v,
-        #    ua,
-        #    grid.cosa_u,
-        #    grid.sina_u,
-        #    grid.dxc,
-        #    vort,
-        #    origin=(is2, grid.js - 1, kstart),
-        #    domain=(ie1 - is2 + 1, grid.njc + 2, nk),
-        #)
-        #x_edge_domain = (1, grid.njc + 2, nk)
-        #if grid.west_edge:
-        #    vorticity_x_edge(
-        #        v,
-        #        uc,
-        #        grid.dxc,
-        #        grid.sin_sg3,
-        #        grid.sin_sg1,
-        #        vort,
-        #        origin=(grid.is_, grid.js - 1, kstart),
-        #        domain=x_edge_domain,
-        #    )
-        #if grid.east_edge:
-        #    vorticity_x_edge(
-        #        v,
-        #        uc,
-        #        grid.dxc,
-        #        grid.sin_sg3,
-        #        grid.sin_sg1,
-        #        vort,
-        #        origin=(grid.ie + 1, grid.js - 1, kstart),
-        #        domain=x_edge_domain,
-        #    )
-    else:
+    if grid.nested:
         raise Exception("nested not implemented")
-    #compute_origin = (grid.is_, grid.js, kstart)
-    #compute_domain = (grid.nic + 1, grid.njc + 1, nk)
-    delpc_main(vort, ptc, delpc, origin=compute_origin, domain=compute_domain)
-    corner_domain = (1, 1, nk)
-    if grid.sw_corner:
-        corner_south_remove_extra_term(
-            vort, delpc, origin=(grid.is_, grid.js, kstart), domain=corner_domain
-        )
-    if grid.se_corner:
-        corner_south_remove_extra_term(
-            vort, delpc, origin=(grid.ie + 1, grid.js, kstart), domain=corner_domain
-        )
-    if grid.ne_corner:
-        corner_north_remove_extra_term(
-            vort, delpc, origin=(grid.ie + 1, grid.je + 1, kstart), domain=corner_domain
-        )
-    if grid.nw_corner:
-        corner_north_remove_extra_term(
-            vort, delpc, origin=(grid.is_, grid.je + 1, kstart), domain=corner_domain
-        )
+
+    ptc_main(
+        u, v,
+        ua, va, uc, vc,
+        grid.cosa_u, grid.cosa_v,
+        grid.sina_u, grid.sina_v,
+        grid.dxc, grid.dyc, grid.sin_sg1, grid.sin_sg2,grid.sin_sg3, grid.sin_sg4,
+        ptc,vort, delpc,
+        origin=(grid.is_ - 1, grid.js - 1, kstart),
+        domain=(grid.nic + 2, grid.njc + 2, nk),
+    )
+    #delpc_main(vort, ptc, delpc, origin=compute_origin, domain=compute_domain)
+    #corner_domain = (1, 1, nk)
+    #if grid.sw_corner:
+    #    corner_south_remove_extra_term(
+    #        vort, delpc, origin=(grid.is_, grid.js, kstart), domain=corner_domain
+    #    )
+    #if grid.se_corner:
+    #    corner_south_remove_extra_term(
+    #        vort, delpc, origin=(grid.ie + 1, grid.js, kstart), domain=corner_domain
+    #    )
+    #if grid.ne_corner:
+    #    corner_north_remove_extra_term(
+    #        vort, delpc, origin=(grid.ie + 1, grid.je + 1, kstart), domain=corner_domain
+    #    )
+    #if grid.nw_corner:
+    #    corner_north_remove_extra_term(
+    #        vort, delpc, origin=(grid.is_, grid.je + 1, kstart), domain=corner_domain
+    #    )
 
     damping_nord0_stencil(
         grid.rarea_c,
