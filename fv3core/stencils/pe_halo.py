06b836a13d18ffabd6c59cbe146afb604a9a1ad9
Chris Kung <christopher.w.kung@nasa.gov>
2021-03-02 13:24:53 -0500

    Applied horizontal regions to pe_halo.py to reduce number of stencil calls from 4 to 1.

--- fv3core/stencils/pe_halo.py
+++ fv3core/stencils/pe_halo.py
@@ -1,27 +1,39 @@
-from gt4py.gtscript import FORWARD, computation, interval
+from gt4py.gtscript import FORWARD, computation, horizontal, interval, region
 
 import fv3core._config as spec
-import fv3core.utils.gt4py_utils as utils
 from fv3core.decorators import gtstencil
-
-
-sd = utils.sd
+from fv3core.utils.typing import FloatField
 
 
 @gtstencil()
-def edge_pe(pe: sd, delp: sd, ptop: float):
+def edge_pe(pe: FloatField, delp: FloatField, ptop: float):
+    from __externals__ import i_end, i_start, j_end, j_start
+
     with computation(FORWARD):
         with interval(0, 1):
-            pe[0, 0, 0] = ptop
+            with horizontal(
+                region[i_start - 1 : i_start, j_start : j_end + 1],
+                region[i_end + 1 : i_end + 2, j_start : j_end + 1],
+                region[i_start - 1 : i_end + 1, j_start - 1 : j_start],
+                region[i_start - 1 : i_end + 1, j_end + 1 : j_end + 2],
+            ):
+                pe[0, 0, 0] = ptop
         with interval(1, None):
-            pe[0, 0, 0] = pe[0, 0, -1] + delp[0, 0, -1]
+            with horizontal(
+                region[i_start - 1 : i_start, j_start : j_end + 1],
+                region[i_end + 1 : i_end + 2, j_start : j_end + 1],
+                region[i_start - 1 : i_end + 2, j_start - 1 : j_start],
+                region[i_start - 1 : i_end + 2, j_end + 1 : j_end + 2],
+            ):
+                pe[0, 0, 0] = pe[0, 0, -1] + delp[0, 0, -1]
 
 
 def compute(pe, delp, ptop):
     grid = spec.grid
-    edge_domain_x = (1, grid.njc, grid.npz + 1)
-    edge_pe(pe, delp, ptop, origin=(grid.is_ - 1, grid.js, 0), domain=edge_domain_x)
-    edge_pe(pe, delp, ptop, origin=(grid.ie + 1, grid.js, 0), domain=edge_domain_x)
-    edge_domain_y = (grid.nic + 2, 1, grid.npz + 1)
-    edge_pe(pe, delp, ptop, origin=(grid.is_ - 1, grid.js - 1, 0), domain=edge_domain_y)
-    edge_pe(pe, delp, ptop, origin=(grid.is_ - 1, grid.je + 1, 0), domain=edge_domain_y)
+    edge_pe(
+        pe,
+        delp,
+        ptop,
+        origin=grid.full_origin(),
+        domain=grid.domain_shape_full(add=(0, 0, 1)),
+    )
