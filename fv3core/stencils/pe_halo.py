bfe114bd8f8723886202549742456d8b1192b693
Chris Kung <christopher.w.kung@nasa.gov>
2021-03-05 11:07:56 -0500

    Small tweak to regions bounds in 'i'

--- fv3core/stencils/pe_halo.py
+++ fv3core/stencils/pe_halo.py
@@ -22,8 +22,8 @@ def edge_pe(pe: FloatField, delp: FloatField, ptop: float):
             with horizontal(
                 region[i_start - 1 : i_start, j_start : j_end + 1],
                 region[i_end + 1 : i_end + 2, j_start : j_end + 1],
-                region[i_start - 1 : i_end + 2, j_start - 1 : j_start],
-                region[i_start - 1 : i_end + 2, j_end + 1 : j_end + 2],
+                region[i_start - 1 : i_end + 1, j_start - 1 : j_start],
+                region[i_start - 1 : i_end + 1, j_end + 1 : j_end + 2],
             ):
                 pe[0, 0, 0] = pe[0, 0, -1] + delp[0, 0, -1]
 
