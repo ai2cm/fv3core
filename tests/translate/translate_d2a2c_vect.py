d6d919b51bf0eab70eef41e46166f5b2acbc0810
Johann Dahm <johannd@vulcan.com>
2021-03-12 12:36:31 -0800

    Refactor d2a2c into single stencil

--- tests/translate/translate_d2a2c_vect.py
+++ tests/translate/translate_d2a2c_vect.py
@@ -5,7 +5,6 @@ from fv3core.testing import TranslateFortranData2Py
 class TranslateD2A2C_Vect(TranslateFortranData2Py):
     def __init__(self, grid):
         super().__init__(grid)
-        self.compute_func = d2a2c_vect.compute
         self.in_vars["data_vars"] = {
             "uc": {},
             "vc": {},
@@ -29,3 +28,14 @@ class TranslateD2A2C_Vect(TranslateFortranData2Py):
         # methods, can we rejigger the order of operations to make it match to
         # more precision?
         self.max_error = 2e-10
+
+    def compute(self, inputs):
+        self.make_storage_data_input_vars(inputs)
+
+        # d2a2c_vect is written assuming dord4 is True
+        assert bool(inputs["dord4"]) is True
+        del inputs["dord4"]
+
+        d2a2c_vect.compute(self.grid, **inputs)
+
+        return self.slice_output(inputs)
