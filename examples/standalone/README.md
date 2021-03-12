ec21cfd32a5147fe758cb6c75be042daac607474
Oliver Fuhrer <oli@ginko.ch>
2021-03-11 21:37:26 -0800

    fix EOF

---
+++ examples/standalone/README.md
@@ -0,0 +1,28 @@
+# How to get performance numbers
+
+The script `run_on_daint.sh` allows you to run a benchmark of the dynamical core standalone
+`dynamics.py` on bare metal Piz Daint.
+
+## Arguments
+
+-   timesteps: Number of timesteps to execute (this includes the first one as a warm up step)
+-   ranks: Number of ranks to run with
+-   backend: choice of gt4py backend
+-   data\_path: path to the directory containing the serialized test data
+-   py\_args: (optional) arguments to pass to python invocation
+-   run\_args: (optional) arguments to pass to the dynamics.py invocation
+
+## Constraints
+
+The data directory is expected to contain unpacked serialized data (serialized by serialbox).
+The serialized data is also expected to have both the `input.nml` as well as the `*.yml` namelists present.
+
+## Output
+
+A `timing.json` file containing statistics over the ranks for execution time.
+The first timestep is counted towards `init`, the rest of the timesteps are in `main loop`.
+Total is inclusive of the other categories
+
+## Example
+
+`./examples/standalone/run_on_daint.sh 60 6 gtx86 <data_path>`
--- examples/standalone/run_on_daint.sh
+++ examples/standalone/run_on_daint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+#############################################
+# Example syntax:
+# ./run_on_daint.sh 60 6 gtx86
+
 ## Arguments:
 # $1: number of timesteps to run
 # $2: number of ranks to execute with (ensure that this is compatible with fv3core)
 # $3: backend to use in gt4py
-# $4: target directory to store the output in
-# $5: path to the data directory that should be run
-#############################################
-# Example syntax:
-# ./run_on_daint.sh 60 6 gtx86
+# $4: path to the data directory that should be run
+# $5: (optional) arguments to pass to python invocation
+# $6: (optional) arguments to pass to dynamics.py invocation
+
 set -e
 exitError()
 {
@@ -21,32 +24,19 @@ exitError()
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 ROOT_DIR="$(dirname "$(dirname "$(dirname "$SCRIPTPATH")")")"
+NTHREADS=12
 
 # check sanity of environment
 test -n "$1" || exitError 1001 ${LINENO} "must pass a number of timesteps"
 timesteps="$1"
-
 test -n "$2" || exitError 1002 ${LINENO} "must pass a number of ranks"
 ranks="$2"
-
+test -n "$3" || exitError 1003 ${LINENO} "must pass a backend"
 backend="$3"
-if [ -z "$3" ] ; then
-    backend="numpy"
-fi
-
-target_dir="$4"
-if [ -z "$4" ] ; then
-    target_dir="$ROOT_DIR"
-fi
-
-data_path="$5"
-if [ -z "$5" ] ; then
-    data_path="/scratch/snx3000/olifu/jenkins/scratch/fv3core_fortran_data/7.2.5/c12_6ranks_standard/"
-fi
-
-py_args="$6"
-
-run_args="$7"
+test -n "$4" || exitError 1004 ${LINENO} "must pass a data path"
+data_path="$4"
+py_args="$5"
+run_args="$6"
 
 # get dependencies
 cd $ROOT_DIR
@@ -77,7 +67,6 @@ git clone https://github.com/VulcanClimateModeling/buildenv/
 cp ./buildenv/submit.daint.slurm compile.daint.slurm
 cp ./buildenv/submit.daint.slurm run.daint.slurm
 
-nthreads=12
 if git rev-parse --git-dir > /dev/null 2>&1 ; then
   githash=`git rev-parse HEAD`
 else
@@ -85,14 +74,13 @@ else
 fi
 
 echo "Configuration overview:"
+echo "    Root dir:         $ROOT_DIR"
 echo "    Timesteps:        $timesteps"
 echo "    Ranks:            $ranks"
 echo "    Backend:          $backend"
-echo "    Output dir:       $target_dir"
 echo "    Input data dir:   $data_path"
-echo "    Threads per rank: $nthreads"
+echo "    Threads per rank: $NTHREADS"
 echo "    GIT hash:         $githash"
-echo "    Slurm output dir: $ROOT_DIR"
 echo "    Python arguments: $py_args"
 echo "    Run arguments:    $run_args"
 
@@ -113,12 +101,12 @@ echo "submitting script to do compilation"
 sed -i s/\<NAME\>/standalone/g compile.daint.slurm
 sed -i s/\<NTASKS\>/$ranks/g compile.daint.slurm
 sed -i s/\<NTASKSPERNODE\>/1/g compile.daint.slurm
-sed -i s/\<CPUSPERTASK\>/$nthreads/g compile.daint.slurm
+sed -i s/\<CPUSPERTASK\>/$NTHREADS/g compile.daint.slurm
 sed -i s/--output=\<OUTFILE\>/--hint=nomultithread/g compile.daint.slurm
 sed -i s/00:45:00/03:30:00/g compile.daint.slurm
 sed -i s/cscsci/normal/g compile.daint.slurm
 sed -i s/\<G2G\>/export\ CRAY_CUDA_MPS=1/g compile.daint.slurm
-sed -i "s#<CMD>#export PYTHONPATH=/project/s1053/install/serialbox2_master/gnu/python:\$PYTHONPATH\nsrun python examples/standalone/runfile/dynamics.py $data_path 1 $backend $githash --disable_halo_exchange#g" compile.daint.slurm
+sed -i "s#<CMD>#export PYTHONPATH=/project/s1053/install/serialbox2_master/gnu/python:\$PYTHONPATH\nsrun python examples/standalone/dynamics.py $data_path 1 $backend $githash --disable_halo_exchange#g" compile.daint.slurm
 
 # execute on a gpu node
 sbatch -W -C gpu compile.daint.slurm
@@ -130,18 +118,15 @@ echo "submitting script to do performance run"
 sed -i s/\<NAME\>/standalone/g run.daint.slurm
 sed -i s/\<NTASKS\>/$ranks/g run.daint.slurm
 sed -i s/\<NTASKSPERNODE\>/1/g run.daint.slurm
-sed -i s/\<CPUSPERTASK\>/$nthreads/g run.daint.slurm
+sed -i s/\<CPUSPERTASK\>/$NTHREADS/g run.daint.slurm
 sed -i s/--output=\<OUTFILE\>/--hint=nomultithread/g run.daint.slurm
 sed -i s/00:45:00/00:40:00/g run.daint.slurm
 sed -i s/cscsci/normal/g run.daint.slurm
 sed -i s/\<G2G\>//g run.daint.slurm
-sed -i "s#<CMD>#export PYTHONPATH=/project/s1053/install/serialbox2_master/gnu/python:\$PYTHONPATH\nsrun python $py_args examples/standalone/runfile/dynamics.py $data_path $timesteps $backend $githash $run_args#g" run.daint.slurm
+sed -i "s#<CMD>#export PYTHONPATH=/project/s1053/install/serialbox2_master/gnu/python:\$PYTHONPATH\nsrun python $py_args examples/runfile/dynamics.py $data_path $timesteps $backend $githash $run_args#g" run.daint.slurm
 
 # execute on a gpu node
 sbatch -W -C gpu run.daint.slurm
 wait
-if [ -n "$target_dir" ] ; then
-    rsync *.json $target_dir
-fi
 
 echo "performance run sucessful"
