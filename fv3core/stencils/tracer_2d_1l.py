ced1e567a17a376a6ce994da81df5d0cd12a538c
Rhea George <rheag@vulcan.com>
2021-03-19 02:15:35 +0000

    some cleanup

--- fv3core/stencils/tracer_2d_1l.py
+++ fv3core/stencils/tracer_2d_1l.py
@@ -65,10 +65,11 @@ def cmax_multiply_by_frac(
     cyd: FloatField,
     yfx: FloatField,
     mfyd: FloatField,
-    frac: float,
+    nsplt: int,
 ):
     """multiply all other inputs in-place by frac."""
     with computation(PARALLEL), interval(...):
+        frac = 1.0 / nsplt
         cxd = cxd * frac
         xfx = xfx * frac
         mfxd = mfxd * frac
@@ -98,6 +99,17 @@ def dp_fluxadjustment(
 ):
     with computation(PARALLEL), interval(...):
         dp2 = dp1 + (mfx - mfx[1, 0, 0] + mfy - mfy[0, 1, 0]) * rarea
+
+
+def loop_temporaries_copy(
+    tmp_dp1_orig: FloatField,
+    q: FloatField,
+    dp1: FloatField,
+    tmp_qn2: FloatField,
+):
+    with computation(PARALLEL), interval(...):
+        dp1 = tmp_dp1_orig
+        tmp_qn2 = q
 
 
 @gtscript.function
@@ -139,45 +151,39 @@ class Tracer2D1L:
     def __init__(self, namelist):
         shape = spec.grid.domain_shape_full(add=(1, 1, 1))
         origin = spec.grid.compute_origin()
-        # do not really need these special origins, do they have any impact?
-        origin_x = spec.grid.compute_origin(add=(0, -spec.grid.halo, 0))
-        origin_y = spec.grid.compute_origin(add=(-spec.grid.halo, 0, 0))
-        self._tmp_xfx = utils.make_storage_from_shape(shape, origin_x)
-        self._tmp_yfx = utils.make_storage_from_shape(shape, origin_y)
-        self._tmp_ra_x = utils.make_storage_from_shape(shape, origin_x)
-        self._tmp_ra_y = utils.make_storage_from_shape(shape, origin_y)
+        self._tmp_xfx = utils.make_storage_from_shape(shape, origin)
+        self._tmp_yfx = utils.make_storage_from_shape(shape, origin)
+        self._tmp_ra_x = utils.make_storage_from_shape(shape, origin)
+        self._tmp_ra_y = utils.make_storage_from_shape(shape, origin)
         self._tmp_fx = utils.make_storage_from_shape(shape, origin)
         self._tmp_fy = utils.make_storage_from_shape(shape, origin)
         self._tmp_dp2 = utils.make_storage_from_shape(shape, origin)
         self._tmp_dp1_orig = utils.make_storage_from_shape(shape, origin)
         self._tmp_qn2 = utils.make_storage_from_shape(shape, origin)
-        axis_offsets = fv3core.utils.axis_offsets(
-            spec.grid, spec.grid.full_origin(), spec.grid.domain_shape_full()
-        )
-        print("\noffsts", axis_offsets)
         stencil_kwargs = {
             "backend": global_config.get_backend(),
             "rebuild": global_config.get_rebuild(),
-            "externals": axis_offsets,
+            "externals": fv3core.utils.axis_offsets(
+                spec.grid, spec.grid.full_origin(), spec.grid.domain_shape_full()
+            ),
         }
         stencil_wrapper = gtscript.stencil(**stencil_kwargs)
         self.stencil_flux_compute = stencil_wrapper(flux_compute)
         self.stencil_ra_update = stencil_wrapper(ra_stencil_update.func)
         self.stencil_cmax_multiply_by_frac = stencil_wrapper(cmax_multiply_by_frac)
         self.stencil_copy = stencil_wrapper(copy_stencil.func)
+        self.stencil_loop_temporaries_copy = stencil_wrapper(loop_temporaries_copy)
         self.stencil_dp_fluxadjustment = stencil_wrapper(dp_fluxadjustment)
         self.stencil_q_adjustments = stencil_wrapper(q_adjustments)
         self.stencil_q_adjust = stencil_wrapper(q_adjust)
         self.fvtp2d_obj = fvtp2d.FvTp2d(spec.namelist, spec.namelist.hord_tr)
         # If use AllReduce, will need something like this:
         # self._tmp_cmax = utils.make_storage_from_shape(shape, origin)
-        # self._tmp_nsplt3d = utils.make_storage_from_shape(shape, origin)
         # self.stencil_cmax_1 = stencil_wrapper(cmax_stencil1)
         # self.stencil_max_2 = stencil_wrapper(cmax_stencil2)
 
     def __call__(self, comm, tracers, dp1, mfxd, mfyd, cxd, cyd, mdt, nq):
         grid = spec.grid
-        shape = mfxd.data.shape
         # start HALO update on q (in dyn_core in fortran -- just has started when
         # this function is called...)
         self.stencil_flux_compute(
@@ -196,7 +202,7 @@ class Tracer2D1L:
             origin=grid.full_origin(),
             domain=grid.domain_shape_full(),
         )
-        # {
+
         # # TODO for if we end up using the Allreduce and compute cmax globally
         # (or locally). For now, hardcoded.
         # split = int(grid.npz / 6)
@@ -216,18 +222,15 @@ class Tracer2D1L:
         # # cmax_flat is a gt4py storage still, but of dimension [npz+1]...
 
         # cmax_max_all_ranks = cmax_flat.data
-        # # TODO mpi allreduce.... can we not?
+        # # TODO mpi allreduce...
         # # comm.Allreduce(cmax_flat, cmax_max_all_ranks, op=MPI.MAX)
-        # }
+
         cmax_max_all_ranks = 2.0
         nsplt = math.floor(1.0 + cmax_max_all_ranks)
         # NOTE: cmax is not usually a single value, it varies with k, if return to
-        # that, make nsplt a column as well and compute frac inside cmax_split_vars.
+        # that, make nsplt a column as well
 
-        # self._tmp_nsplt3d[:] = nsplt
-        frac = 1.0
         if nsplt > 1.0:
-            frac = 1.0 / nsplt
             self.stencil_cmax_multiply_by_frac(
                 cxd,
                 self._tmp_xfx,
@@ -235,7 +238,7 @@ class Tracer2D1L:
                 cyd,
                 self._tmp_yfx,
                 mfyd,
-                frac,
+                nsplt,
                 origin=grid.full_origin(),
                 domain=grid.domain_shape_full(add=(1, 1, 0)),
             )
@@ -266,15 +269,10 @@ class Tracer2D1L:
         )
         for qname in utils.tracer_variables[0:nq]:
             q = tracers[qname + "_quantity"]
-            # handling the q and it loop switching
-            self.stencil_copy(
+            self.stencil_loop_temporaries_copy(
                 self._tmp_dp1_orig,
-                dp1,
-                origin=grid.full_origin(),
-                domain=grid.domain_shape_full(),
-            )
-            self.stencil_copy(
                 q.storage,
+                dp1,
                 self._tmp_qn2,
                 origin=grid.full_origin(),
                 domain=grid.domain_shape_full(),
