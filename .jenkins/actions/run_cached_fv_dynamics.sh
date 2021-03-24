a62eb3725b405d89e234461e7eac4ce0861dad1d
tobias <twicki12@gmail.com>
2021-03-25 00:57:05 +0100

    bash doing bashy things

--- .jenkins/actions/run_cached_fv_dynamics.sh
+++ .jenkins/actions/run_cached_fv_dynamics.sh
@@ -9,7 +9,7 @@ make get_test_data
 
 if [ ! -d $(pwd)/.gt_cache_000000 ]; then
     version=`cat /scratch/snx3000/olifu/jenkins/scratch/store_gt_caches/$EXPNAME/$BACKEND/version.txt`
-    if [ "$version" = "$GT4PY_VERSION" ]; then
+    if [ "$version" == "$GT4PY_VERSION" ]; then
         cp -r /scratch/snx3000/olifu/jenkins/scratch/store_gt_caches/$EXPNAME/$BACKEND/.gt_cache_0000* .
         find . -name m_\*.py -exec sed -i "s|\/scratch\/snx3000\/olifu\/jenkins_submit\/workspace\/fv3core-cache-setup\/backend\/$BACKEND\/experiment\/$EXPNAME\/slave\/daint_submit|$(pwd)|g" {} +
     fi    
