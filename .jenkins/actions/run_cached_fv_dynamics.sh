cfca830f6fba9ccfe9717c08d62337aedf949bdf
Oliver Fuhrer <ofuhrer@users.noreply.github.com>
2021-03-24 16:31:32 -0700

    fix linting

--- .jenkins/actions/run_cached_fv_dynamics.sh
+++ .jenkins/actions/run_cached_fv_dynamics.sh
@@ -12,6 +12,6 @@ if [ ! -d $(pwd)/.gt_cache_000000 ]; then
     if [ "$version" = "$GT4PY_VERSION" ]; then
         cp -r /scratch/snx3000/olifu/jenkins/scratch/store_gt_caches/$EXPNAME/$BACKEND/.gt_cache_0000* .
         find . -name m_\*.py -exec sed -i "s|\/scratch\/snx3000\/olifu\/jenkins_submit\/workspace\/fv3core-cache-setup\/backend\/$BACKEND\/experiment\/$EXPNAME\/slave\/daint_submit|$(pwd)|g" {} +
-    fi    
+    fi
 fi
 make tests_venv_mpi
