7671555375c5d63a8a5710a677b96f99edd159db
Eddie Davis <eddied@vulcan.com>
2021-03-25 20:06:48 +0000

    Add syncs to FillCorners and SatAdjust3d

--- fv3core/stencils/saturation_adjustment.py
+++ fv3core/stencils/saturation_adjustment.py
@@ -5,6 +5,7 @@ from gt4py.gtscript import FORWARD, PARALLEL, computation, exp, floor, interval,
 
 import fv3core._config as spec
 import fv3core.utils.global_constants as constants
+import fv3core.utils.gt4py_utils as utils
 from fv3core.decorators import gtstencil
 from fv3core.stencils.basic_operations import dim
 from fv3core.stencils.moist_cv import compute_pkz_func
@@ -986,3 +987,4 @@ def compute(
         origin=origin,
         domain=domain,
     )
+    utils.device_sync()
