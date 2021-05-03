5d50afcff3c6dae2257265a19846e289c2dbe985
Jeremy McGibbon <jeremym@vulcan.com>
2021-05-03 09:42:54 -0700

    remove .func attribute access on function

--- fv3core/stencils/tracer_2d_1l.py
+++ fv3core/stencils/tracer_2d_1l.py
@@ -9,7 +9,7 @@ import fv3core.utils
 import fv3core.utils.global_config as global_config
 import fv3core.utils.gt4py_utils as utils
 import fv3gfs.util
-from fv3core.decorators import FrozenStencil, gtstencil
+from fv3core.decorators import FrozenStencil
 from fv3core.stencils.basic_operations import copy_stencil
 from fv3core.stencils.fvtp2d import FiniteVolumeTransport
 from fv3core.utils.typing import FloatField, FloatFieldIJ
@@ -183,7 +183,6 @@ class Tracer2D1L:
             domain=self.grid.domain_shape_full(add=(1, 1, 0)),
             externals=local_axis_offsets,
         )
-        self._copy_field = gtstencil(copy_stencil.func)
         self._loop_temporaries_copy = FrozenStencil(
             loop_temporaries_copy,
             origin=self.grid.full_origin(),
@@ -281,7 +280,7 @@ class Tracer2D1L:
         # duplicating storages/stencil calls, return to this, maybe you have more
         # options now, or maybe the one chosen here is the worse one.
 
-        self._copy_field(
+        copy_stencil(
             dp1,
             self._tmp_dp1_orig,
             origin=self.grid.full_origin(),
