c81a67aa60042cd7a6c7eb20163e78f11fb6eb78
Rhea George <rheag@vulcan.com>
2021-05-18 19:32:27 -0700

    lint

--- fv3core/stencils/a2b_ord4.py
+++ fv3core/stencils/a2b_ord4.py
@@ -3,6 +3,7 @@ from typing import Tuple
 import gt4py
 import gt4py.gtscript as gtscript
 from gt4py.gtscript import (
+    __INLINED,
     PARALLEL,
     asin,
     computation,
@@ -17,7 +18,6 @@ from gt4py.gtscript import (
 import fv3core._config as spec
 import fv3core.utils.gt4py_utils as utils
 from fv3core.decorators import FrozenStencil
-from fv3core.stencils.basic_operations import copy_defn
 from fv3core.utils import axis_offsets
 from fv3core.utils.typing import FloatField, FloatFieldI, FloatFieldIJ
 
@@ -80,7 +80,7 @@ def _south_corners(
     bgrid1: FloatFieldIJ,
     bgrid2: FloatFieldIJ,
 ):
-    from __externals__ import i_end, i_start, j_end, j_start
+    from __externals__ import i_end, i_start, j_start
 
     with computation(PARALLEL), interval(...):
         tmp = 0.0
@@ -158,7 +158,7 @@ def _north_corners(
     bgrid1: FloatFieldIJ,
     bgrid2: FloatFieldIJ,
 ):
-    from __externals__ import i_end, i_start, j_end, j_start
+    from __externals__ import i_end, i_start, j_end
 
     with computation(PARALLEL), interval(...):
         tmp = 0.0
@@ -253,19 +253,39 @@ def a2b_interpolation(
     edge_s: FloatFieldI,
     edge_n: FloatFieldI,
 ):
-    from __externals__ import i_end, i_start, j_end, j_start, REPLACE
+    from __externals__ import REPLACE, i_end, i_start, j_end, j_start
 
     with computation(PARALLEL), interval(...):
         # qout_edges_x
         with horizontal(region[i_start, j_start + 1 : j_end + 1]):
-            qout = edge_w * ((qin[-1, -1, 0] * dxa[0, -1] + qin[0, -1, 0] * dxa[-1, -1]) / (dxa[-1, -1] + dxa[0, -1])) + (1.0 - edge_w) * ((qin[-1, 0, 0] * dxa + qin * dxa[-1, 0]) / (dxa[-1, 0] + dxa))
+            qout = edge_w * (
+                (qin[-1, -1, 0] * dxa[0, -1] + qin[0, -1, 0] * dxa[-1, -1])
+                / (dxa[-1, -1] + dxa[0, -1])
+            ) + (1.0 - edge_w) * (
+                (qin[-1, 0, 0] * dxa + qin * dxa[-1, 0]) / (dxa[-1, 0] + dxa)
+            )
         with horizontal(region[i_end + 1, j_start + 1 : j_end + 1]):
-            qout = edge_e *  ((qin[-1, -1, 0] * dxa[0, -1] + qin[0, -1, 0] * dxa[-1, -1]) / (dxa[-1, -1] + dxa[0, -1])) + (1.0 - edge_e) * ((qin[-1, 0, 0] * dxa + qin * dxa[-1, 0]) / (dxa[-1, 0] + dxa))
-       
+            qout = edge_e * (
+                (qin[-1, -1, 0] * dxa[0, -1] + qin[0, -1, 0] * dxa[-1, -1])
+                / (dxa[-1, -1] + dxa[0, -1])
+            ) + (1.0 - edge_e) * (
+                (qin[-1, 0, 0] * dxa + qin * dxa[-1, 0]) / (dxa[-1, 0] + dxa)
+            )
+
         with horizontal(region[i_start + 1 : i_end + 1, j_start]):
-            qout = edge_s * ((qin[-1, -1, 0] * dya[-1, 0] + qin[-1, 0, 0] * dya[-1, -1]) / (dya[-1, -1] + dya[-1, 0])) + (1.0 - edge_s) * ((qin[0, -1, 0] * dya + qin * dya[0, -1]) / (dya[0, -1] + dya))
+            qout = edge_s * (
+                (qin[-1, -1, 0] * dya[-1, 0] + qin[-1, 0, 0] * dya[-1, -1])
+                / (dya[-1, -1] + dya[-1, 0])
+            ) + (1.0 - edge_s) * (
+                (qin[0, -1, 0] * dya + qin * dya[0, -1]) / (dya[0, -1] + dya)
+            )
         with horizontal(region[i_start + 1 : i_end + 1, j_end + 1]):
-            qout = edge_n * ((qin[-1, -1, 0] * dya[-1, 0] + qin[-1, 0, 0] * dya[-1, -1]) / (dya[-1, -1] + dya[-1, 0])) + (1.0 - edge_n) * ((qin[0, -1, 0] * dya + qin * dya[0, -1]) / (dya[0, -1] + dya))
+            qout = edge_n * (
+                (qin[-1, -1, 0] * dya[-1, 0] + qin[-1, 0, 0] * dya[-1, -1])
+                / (dya[-1, -1] + dya[-1, 0])
+            ) + (1.0 - edge_n) * (
+                (qin[0, -1, 0] * dya + qin * dya[0, -1]) / (dya[0, -1] + dya)
+            )
 
         # ppm_volume_mean_x
         qx = b2 * (qin[-2, 0, 0] + qin[1, 0, 0]) + b1 * (qin[-1, 0, 0] + qin)
@@ -374,14 +394,26 @@ def a2b_interpolation(
 
         qxx = a2 * (qx[0, -2, 0] + qx[0, 1, 0]) + a1 * (qx[0, -1, 0] + qx)
         with horizontal(region[:, j_start + 1]):
-            qxx = c1 * (qx[0, -1, 0] + qx) + c2 * (qout[0, -1, 0] + (a2 * (qx[0, -1, 0] + qx[0, 2, 0]) + a1 * (qx + qx[0, 1, 0])))
+            qxx = c1 * (qx[0, -1, 0] + qx) + c2 * (
+                qout[0, -1, 0]
+                + (a2 * (qx[0, -1, 0] + qx[0, 2, 0]) + a1 * (qx + qx[0, 1, 0]))
+            )
         with horizontal(region[:, j_end]):
-            qxx = c1 * (qx[0, -1, 0] + qx) + c2 * (qout[0, 1, 0] + (a2 * (qx[0, -3, 0] + qx) + a1 * (qx[0, -2, 0] + qx[0, -1, 0])))
+            qxx = c1 * (qx[0, -1, 0] + qx) + c2 * (
+                qout[0, 1, 0]
+                + (a2 * (qx[0, -3, 0] + qx) + a1 * (qx[0, -2, 0] + qx[0, -1, 0]))
+            )
         qyy = a2 * (qy[-2, 0, 0] + qy[1, 0, 0]) + a1 * (qy[-1, 0, 0] + qy)
         with horizontal(region[i_start + 1, :]):
-            qyy = c1 * (qy[-1, 0, 0] + qy) + c2 * (qout[-1, 0, 0] + (a2 * (qy[-1, 0, 0] + qy[2, 0, 0]) + a1 * (qy + qy[1, 0, 0])))
+            qyy = c1 * (qy[-1, 0, 0] + qy) + c2 * (
+                qout[-1, 0, 0]
+                + (a2 * (qy[-1, 0, 0] + qy[2, 0, 0]) + a1 * (qy + qy[1, 0, 0]))
+            )
         with horizontal(region[i_end, :]):
-            qyy = c1 * (qy[-1, 0, 0] + qy) + c2 * (qout[1, 0, 0] + ( a2 * (qy[-3, 0, 0] + qy) + a1 * (qy[-2, 0, 0] + qy[-1, 0, 0])))
+            qyy = c1 * (qy[-1, 0, 0] + qy) + c2 * (
+                qout[1, 0, 0]
+                + (a2 * (qy[-3, 0, 0] + qy) + a1 * (qy[-2, 0, 0] + qy[-1, 0, 0]))
+            )
         with horizontal(region[i_start + 1 : i_end + 1, j_start + 1 : j_end + 1]):
             qout = 0.5 * (qxx + qyy)
         if __INLINED(REPLACE):
@@ -402,7 +434,6 @@ def qout_y_edge(
     with computation(PARALLEL), interval(...):
         q1 = (qin[0, -1, 0] * dya + qin * dya[0, -1]) / (dya[0, -1] + dya)
         qout[0, 0, 0] = edge_s * q1[-1, 0, 0] + (1.0 - edge_s) * q1
-
 
 
 def qout_edges_x(
@@ -458,7 +489,7 @@ class AGrid2BGridFourthOrder:
         """
         assert grid_type < 3
         self.grid = spec.grid
-      
+
         shape = self.grid.domain_shape_full(add=(1, 1, 1))
         if nk is None:
             nk = self.grid.npz - kstart
@@ -468,13 +499,9 @@ class AGrid2BGridFourthOrder:
         self._tmp_qy = utils.make_storage_from_shape(shape)
         self._tmp_qxx = utils.make_storage_from_shape(shape)
         self._tmp_qyy = utils.make_storage_from_shape(shape)
-        origin=(self.grid.is_, self.grid.js, kstart)
-        domain=(self.grid.nic + 1, self.grid.njc + 1, nk)
-        ax_offsets = axis_offsets(
-            self.grid,
-            origin,
-            domain
-        )
+        origin = (self.grid.is_, self.grid.js, kstart)
+        domain = (self.grid.nic + 1, self.grid.njc + 1, nk)
+        ax_offsets = axis_offsets(self.grid, origin, domain)
         self._south_corners_stencil = FrozenStencil(
             _south_corners,
             externals=ax_offsets,
@@ -487,9 +514,12 @@ class AGrid2BGridFourthOrder:
             origin=origin,
             domain=domain,
         )
-    
+
         self._a2b_interpolation_stencil = FrozenStencil(
-            a2b_interpolation, externals={'REPLACE': replace, **ax_offsets}, origin=origin, domain=domain
+            a2b_interpolation,
+            externals={"REPLACE": replace, **ax_offsets},
+            origin=origin,
+            domain=domain,
         )
 
     # TODO
@@ -535,6 +565,9 @@ class AGrid2BGridFourthOrder:
             self._tmp_qxx,
             self._tmp_qyy,
             self.grid.dxa,
-            self.grid.dya, self._edge_w, self._edge_e,self.grid.edge_s, self.grid.edge_n, 
+            self.grid.dya,
+            self._edge_w,
+            self._edge_e,
+            self.grid.edge_s,
+            self.grid.edge_n,
         )
- 
