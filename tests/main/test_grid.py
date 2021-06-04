b4885f0395bf7929373088683703760d69c0a95a
Jeremy McGibbon <jeremym@vulcan.com>
2021-06-04 13:58:52 -0700

    add tests for get_origin_domain

--- tests/main/test_grid.py
+++ tests/main/test_grid.py
@@ -1,9 +1,19 @@
+from typing import Sequence
+
 import numpy as np
 import pytest
 from gt4py import gtscript
 
 import fv3core.utils.grid
 from fv3core.utils.typing import Index3D
+from fv3gfs.util import (
+    X_DIM,
+    X_INTERFACE_DIM,
+    Y_DIM,
+    Y_INTERFACE_DIM,
+    Z_DIM,
+    Z_INTERFACE_DIM,
+)
 
 
 @pytest.mark.parametrize(
@@ -299,3 +309,181 @@ def test_domain_compute(
     )
     result = grid.domain_compute(add=add)
     assert result == domain_compute
+
+
+@pytest.mark.parametrize(
+    "origin, domain, n_halo, dims, halos, origin_out, domain_out",
+    [
+        pytest.param(
+            (3, 3, 0),
+            (4, 4, 7),
+            [X_DIM, Y_DIM, Z_DIM],
+            (0, 0, 0),
+            (3, 3, 0),
+            (4, 4, 7),
+            id="compute_domain",
+        ),
+        pytest.param(
+            (3, 3, 0),
+            (4, 4, 7),
+            [X_DIM, Y_DIM, Z_DIM],
+            tuple(),
+            (3, 3, 0),
+            (4, 4, 7),
+            id="compute_domain_no_halo_arg",
+        ),
+        pytest.param(
+            (3, 3, 0),
+            (4, 4, 7),
+            [Z_DIM, Y_DIM, X_DIM],
+            (0, 0, 0),
+            (0, 3, 3),
+            (7, 4, 4),
+            id="reverse_compute_domain",
+        ),
+        pytest.param(
+            (3, 3, 0),
+            (4, 4, 7),
+            [X_DIM, Z_DIM],
+            (0, 0),
+            (3, 0),
+            (4, 7),
+            id="xz_compute_domain",
+        ),
+        pytest.param(
+            (3, 3, 0),
+            (4, 4, 7),
+            [X_DIM, Y_DIM, Z_DIM],
+            (1, 0, 0),
+            (2, 3, 0),
+            (6, 4, 7),
+            id="x_halo",
+        ),
+        pytest.param(
+            (3, 3, 0),
+            (4, 4, 7),
+            [X_DIM, Y_DIM, Z_DIM],
+            (0, 1, 0),
+            (3, 2, 0),
+            (4, 6, 7),
+            id="y_halo",
+        ),
+        # z_halo is an unrealistic case, but the API supports it
+        pytest.param(
+            (3, 3, 0),
+            (4, 4, 7),
+            [X_DIM, Y_DIM, Z_DIM],
+            (0, 0, 1),
+            (3, 3, -1),
+            (4, 6, 8),
+            id="z_halo",
+        ),
+        pytest.param(
+            (3, 3, 0),
+            (4, 4, 7),
+            [X_DIM, Y_DIM, Z_DIM],
+            (2, 2),
+            (1, 1, 0),
+            (8, 8, 7),
+            id="xy_2_halo",
+        ),
+        pytest.param(
+            (3, 3, 0),
+            (4, 4, 7),
+            [X_DIM, Y_DIM],
+            (2, 2),
+            (1, 1),
+            (8, 8),
+            id="xy_2_halo_no_zdim",
+        ),
+        pytest.param(
+            (3, 3, 0),
+            (4, 4, 7),
+            [X_INTERFACE_DIM, Y_DIM, Z_DIM],
+            (0, 0, 0),
+            (3, 3, 0),
+            (5, 4, 7),
+            id="x_interface",
+        ),
+        pytest.param(
+            (3, 3, 0),
+            (4, 4, 7),
+            [X_DIM, Y_INTERFACE_DIM, Z_DIM],
+            (0, 0, 0),
+            (3, 3, 0),
+            (4, 5, 7),
+            id="y_interface",
+        ),
+        pytest.param(
+            (3, 3, 0),
+            (4, 4, 7),
+            [X_DIM, Y_DIM, Z_INTERFACE_DIM],
+            (0, 0, 0),
+            (3, 3, 0),
+            (4, 4, 8),
+            id="z_interface",
+        ),
+        pytest.param(
+            (3, 3, 0),
+            (4, 4, 7),
+            [X_INTERFACE_DIM, Y_DIM, Z_DIM],
+            (0, 3),
+            (3, 1, 0),
+            (5, 8, 7),
+            id="x_interface_y_halo",
+        ),
+        pytest.param(
+            (1, 1, 0),
+            (4, 4, 7),
+            [X_DIM, Y_DIM, Z_DIM],
+            (0, 0, 0),
+            (1, 1, 0),
+            (4, 4, 7),
+            id="compute_domain_smaller_origin",
+        ),
+        pytest.param(
+            (3, 3, 0),
+            (2, 3, 6),
+            [X_DIM, Y_DIM, Z_DIM],
+            (0, 0, 0),
+            (3, 3, 0),
+            (2, 3, 6),
+            id="compute_domain_smaller_domain",
+        ),
+    ],
+)
+@pytest.mark.parametrize(
+    # edges shouldn't matter for this test, but let's make sure behaviors
+    # are all the same
+    "south_edge, north_edge, west_edge, east_edge",
+    [
+        pytest.param(True, True, True, True, id="all_edges"),
+        pytest.param(False, False, False, False, id="no_edges"),
+        pytest.param(True, False, False, True, id="southeast_corner"),
+    ],
+)
+def test_get_origin_domain(
+    origin: Index3D,
+    domain: Index3D,
+    n_halo: int,
+    south_edge: bool,
+    north_edge: bool,
+    west_edge: bool,
+    east_edge: bool,
+    dims: Sequence[str],
+    halos: Sequence[int],
+    origin_expected: Sequence[int],
+    domain_expected: Sequence[int],
+):
+    grid = fv3core.utils.grid.GridIndexing(
+        origin=origin,
+        domain=domain,
+        n_halo=n_halo,
+        south_edge=south_edge,
+        north_edge=north_edge,
+        west_edge=west_edge,
+        east_edge=east_edge,
+    )
+    origin_out, domain_out = grid.get_origin_domain(dims, halos)
+    assert origin_out == origin_expected
+    assert domain_out == domain_expected
