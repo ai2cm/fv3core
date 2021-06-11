760be7890390941bcd4c355e47c7ae6add4ea75c
tobias <twicki12@gmail.com>
2021-06-11 17:44:22 +0200

    stanalone acoustics

---
+++ examples/standalone/runfile/acoustics.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+from types import SimpleNamespace
+from typing import Any, Dict, List
+
+import click
+import numpy as np
+import serialbox
+
+import fv3core
+import fv3core._config as spec
+import fv3core.testing
+import fv3core.utils.global_config as global_config
+from fv3core.stencils.dyn_core import AcousticDynamics
+
+
+def set_up_namelist(data_directory: str) -> None:
+    spec.set_namelist(data_directory + "/input.nml")
+
+
+def initialize_serializer(data_directory: str, rank: int = 0) -> serialbox.Serializer:
+    return serialbox.Serializer(
+        serialbox.OpenModeKind.Read,
+        data_directory,
+        "Generator_rank" + str(rank),
+    )
+
+
+def read_grid(
+    serializer: serialbox.Serializer, rank: int = 0
+) -> fv3core.testing.TranslateGrid:
+    grid_savepoint = serializer.get_savepoint("Grid-Info")[0]
+    grid_data = {}
+    grid_fields = serializer.fields_at_savepoint(grid_savepoint)
+    for field in grid_fields:
+        grid_data[field] = serializer.read(field, grid_savepoint)
+        if len(grid_data[field].flatten()) == 1:
+            grid_data[field] = grid_data[field][0]
+    return fv3core.testing.TranslateGrid(grid_data, rank).python_grid()
+
+
+def initialize_fv3core(backend: str, do_halo_updates: bool) -> None:
+    fv3core.set_backend(backend)
+    fv3core.set_rebuild(False)
+    fv3core.set_validate_args(False)
+    global_config.set_do_halo_exchange(do_halo_updates)
+
+
+def read_input_data(grid, serializer):
+    driver_object = fv3core.testing.TranslateDynCore([grid])
+    savepoint_in = serializer.get_savepoint("DynCore-In")[0]
+    return driver_object.collect_input_data(serializer, savepoint_in)
+
+
+def get_state_from_input(grid, input_data):
+    driver_object = fv3core.testing.TranslateDynCore([grid])
+    driver_object._base.make_storage_data_input_vars(input_data)
+
+    inputs = driver_object.inputs
+    for name, properties in inputs.items():
+        grid.quantity_dict_update(
+            input_data, name, dims=properties["dims"], units=properties["units"]
+        )
+
+    statevars = SimpleNamespace(**input_data)
+    return {"state": statevars}
+
+
+@click.command()
+@click.argument("data_directory", required=True, nargs=1)
+@click.argument("time_steps", required=False, default="1")
+@click.argument("backend", required=False, default="gtc:gt:cpu_ifirst")
+@click.option("--halo_update/--no-halo_update", default=False)
+def driver(
+    data_directory: str,
+    time_steps: str,
+    backend: str,
+    halo_update: bool,
+):
+    set_up_namelist(data_directory)
+    serializer = initialize_serializer(data_directory)
+    initialize_fv3core(backend, halo_update)
+    grid = read_grid(serializer)
+    spec.set_grid(grid)
+
+    input_data = read_input_data(grid, serializer)
+
+    acoutstics_object = AcousticDynamics(
+        None,
+        spec.namelist,
+        input_data["ak"],
+        input_data["bk"],
+        input_data["pfull"],
+        input_data["phis"],
+    )
+
+    state = get_state_from_input(grid, input_data)
+    state.__dict__.update(acoutstics_object._temporaries)
+
+    # Testing dace infrastucture
+    output_field = acoutstics_object.dace_dummy(input_data["omga"])
+    output_field = acoutstics_object.dace_dummy(state["state"].omga)
+    print(output_field)
+
+    # @Linus: make this call a dace program
+    for _ in range(int(time_steps)):
+        acoutstics_object(state["state"], insert_temporaries=False)
+
+
+if __name__ == "__main__":
+    driver()
---
+++ examples/standalone/setup/setup.sh
@@ -0,0 +1,40 @@
+make update_submodules_base
+rm -rf test_venv
+python -m venv test_venv
+source ./test_venv/bin/activate
+pip install --upgrade pip
+pip install --upgrade wheel
+
+# installation of fv3 dependencies
+pip install cftime f90nml pandas pyparsing python-dateutil pytz pyyaml xarray zarr
+
+# installation of fv3config
+fv3config_sha1=1eb1f2898e9965ed7b32970bed83e64e074a7630
+pip install git+git://github.com/VulcanClimateModeling/fv3config.git@${fv3config_sha1}
+
+# installation of gt4py
+export GT4PY_VERSION=`cat GT4PY_VERSION.txt`
+rm -rf gt4py
+git clone git://github.com/VulcanClimateModeling/gt4py.git gt4py
+cd gt4py
+git checkout ${GT4PY_VERSION}
+cd ../
+pip install -e ./gt4py
+python -m gt4py.gt_src_manager install
+
+# installation of serialbox
+rm -rf serialbox
+git clone git@github.com:VulcanClimateModeling/serialbox.git serialbox
+cd serialbox
+mkdir build
+cd build
+cmake ../
+cmake --build . -j 8
+cmake --build . --target install
+cd ..
+export PYTHONPATH=`pwd`/install/python:$PYTHONPATH
+cd ..
+
+
+pip install ./external/fv3gfs-util/
+pip install -e .
