8dad0a611042cf0ba946312c9e4c691629abe638
Rhea George <rheag@vulcan.com>
2021-06-14 21:22:34 -0700

    CubedToLatLon does not use regions

--- fv3core/stencils/c2l_ord.py
+++ fv3core/stencils/c2l_ord.py
@@ -1,4 +1,4 @@
-from gt4py.gtscript import PARALLEL, computation, horizontal, interval, region
+from gt4py.gtscript import PARALLEL, computation, interval
 
 import fv3core.utils.global_config as global_config
 import fv3core.utils.gt4py_utils as utils
@@ -13,58 +13,55 @@ C1 = 1.125
 C2 = -0.125
 
 
-@utils.mark_untested("This namelist option is not tested")
-def c2l_ord2(
+def tmps_main(
     u: FloatField,
     v: FloatField,
-    dx: FloatFieldIJ,
-    dy: FloatFieldIJ,
-    a11: FloatFieldIJ,
-    a12: FloatFieldIJ,
-    a21: FloatFieldIJ,
-    a22: FloatFieldIJ,
-    ua: FloatField,
-    va: FloatField,
+    utmp: FloatField,
+    vtmp: FloatField,
 ):
     with computation(PARALLEL), interval(...):
-        wu = u * dx
-        wv = v * dy
-        # Co-variant vorticity-conserving interpolation
-        u1 = 2.0 * (wu + wu[0, 1, 0]) / (dx + dx[0, 1])
-        v1 = 2.0 * (wv + wv[1, 0, 0]) / (dy + dy[1, 0])
-        # Cubed (cell center co-variant winds) to lat-lon
-        ua = a11 * u1 + a12 * v1
-        va = a21 * u1 + a22 * v1
-
-
-def ord4_transform(
-    u: FloatField,
-    v: FloatField,
-    dx: FloatFieldIJ,
-    dy: FloatFieldIJ,
-    a11: FloatFieldIJ,
-    a12: FloatFieldIJ,
-    a21: FloatFieldIJ,
-    a22: FloatFieldIJ,
-    ua: FloatField,
-    va: FloatField,
-):
-    with computation(PARALLEL), interval(...):
-        from __externals__ import i_end, i_start, j_end, j_start
-
         utmp = C2 * (u[0, -1, 0] + u[0, 2, 0]) + C1 * (u + u[0, 1, 0])
         vtmp = C2 * (v[-1, 0, 0] + v[2, 0, 0]) + C1 * (v + v[1, 0, 0])
 
+def tmps_y_edge(
+    u: FloatField,
+    v: FloatField,
+    dx: FloatFieldIJ,
+    dy: FloatFieldIJ,
+    utmp: FloatField,
+    vtmp: FloatField,
+):
+    with computation(PARALLEL), interval(...):
         # south/north edge
-        with horizontal(region[:, j_start], region[:, j_end]):
-            vtmp = 2.0 * ((v * dy) + (v[1, 0, 0] * dy[1, 0])) / (dy + dy[1, 0])
-            utmp = 2.0 * (u * dx + u[0, 1, 0] * dx[0, 1]) / (dx + dx[0, 1])
+        #with horizontal(region[:, j_start], region[:, j_end]):
+        vtmp = 2.0 * ((v * dy) + (v[1, 0, 0] * dy[1, 0])) / (dy + dy[1, 0])
+        utmp = 2.0 * (u * dx + u[0, 1, 0] * dx[0, 1]) / (dx + dx[0, 1])
 
+def tmps_x_edge(
+    u: FloatField,
+    v: FloatField,
+    dx: FloatFieldIJ,
+    dy: FloatFieldIJ,
+    utmp: FloatField,
+    vtmp: FloatField,
+):
+    with computation(PARALLEL), interval(...):
         # west/east edge
-        with horizontal(region[i_start, :], region[i_end, :]):
-            utmp = 2.0 * ((u * dx) + (u[0, 1, 0] * dx[0, 1])) / (dx + dx[0, 1])
-            vtmp = 2.0 * ((v * dy) + (v[1, 0, 0] * dy[1, 0])) / (dy + dy[1, 0])
+        #with horizontal(region[i_start, :], region[i_end, :]):
+        utmp = 2.0 * ((u * dx) + (u[0, 1, 0] * dx[0, 1])) / (dx + dx[0, 1])
+        vtmp = 2.0 * ((v * dy) + (v[1, 0, 0] * dy[1, 0])) / (dy + dy[1, 0])
 
+def ord4_transform(
+    utmp: FloatField,
+    vtmp: FloatField,
+    a11: FloatFieldIJ,
+    a12: FloatFieldIJ,
+    a21: FloatFieldIJ,
+    a22: FloatFieldIJ,
+    ua: FloatField,
+    va: FloatField,
+):
+    with computation(PARALLEL), interval(...):
         # Transform local a-grid winds into latitude-longitude coordinates
         ua = a11 * utmp + a12 * vtmp
         va = a21 * utmp + a22 * vtmp
@@ -92,29 +89,47 @@ class CubedToLatLon:
             self._do_halo_update = global_config.get_do_halo_exchange()
         self._do_ord4 = True
         self.grid = grid
-        if namelist.c2l_ord == 2:
-            self._do_ord4 = False
-            self._compute_cubed_to_latlon = FrozenStencil(
-                func=c2l_ord2,
-                origin=self.grid.compute_origin(
-                    add=(-1, -1, 0) if self._do_halo_update else (0, 0, 0)
-                ),
-                domain=self.grid.domain_shape_compute(
-                    add=(2, 2, 0) if self._do_halo_update else (0, 0, 0)
-                ),
+        assert namelist.c2l_ord != 2, "not implemented c2l_rd == 2"
+            
+        origin = self.grid.compute_origin()
+        domain = self.grid.domain_shape_compute()
+        shape = self.grid.domain_shape_full(add=(1, 1, 1))
+        self._utmp = utils.make_storage_from_shape(shape)
+        self._vtmp = utils.make_storage_from_shape(shape)
+        self._tmps_main = FrozenStencil(
+            tmps_main,
+            origin=origin,
+            domain=domain,
+        )
+        if self.grid.south_edge:
+            self._tmps_south_edge = FrozenStencil(
+                tmps_y_edge,
+                origin=(origin[0], self.grid.js, origin[2]),
+                domain=(domain[0], 1, domain[2]),
             )
-        else:
-            origin = self.grid.compute_origin()
-            domain = self.grid.domain_shape_compute()
-            ax_offsets = axis_offsets(self.grid, origin, domain)
-            self._compute_cubed_to_latlon = FrozenStencil(
-                func=ord4_transform,
-                externals={
-                    **ax_offsets,
-                },
-                origin=origin,
-                domain=domain,
+        if self.grid.north_edge:
+            self._tmps_north_edge = FrozenStencil(
+                tmps_y_edge,
+                origin=(origin[0], self.grid.je, origin[2]),
+                domain=(domain[0], 1, domain[2]),
             )
+        if self.grid.west_edge:
+            self._tmps_west_edge = FrozenStencil(
+                tmps_x_edge,
+                origin=(self.grid.is_, origin[1],  origin[2]),
+                domain=(1, domain[1], domain[2]),
+            )
+        if self.grid.east_edge:
+            self._tmps_east_edge = FrozenStencil(
+                tmps_x_edge,
+                origin=(self.grid.ie, origin[1],  origin[2]),
+                domain=(1, domain[1], domain[2]),
+            )
+        self._compute_cubed_to_latlon = FrozenStencil(
+            ord4_transform,
+            origin=origin,
+            domain=domain,
+        )
 
     def __call__(
         self,
@@ -135,11 +150,51 @@ class CubedToLatLon:
         """
         if self._do_halo_update and self._do_ord4:
             comm.vector_halo_update(u, v, n_points=self.grid.halo)
-        self._compute_cubed_to_latlon(
+        self._tmps_main(
             u.storage,
             v.storage,
-            self.grid.dx,
-            self.grid.dy,
+            self._utmp,
+            self._vtmp,
+        )
+        if self.grid.south_edge:
+            self._tmps_south_edge(
+                u.storage,
+                v.storage,
+                self.grid.dx,
+                self.grid.dy,
+                self._utmp,
+                self._vtmp,
+            )
+        if self.grid.north_edge:
+            self._tmps_north_edge(
+                u.storage,
+                v.storage,
+                self.grid.dx,
+                self.grid.dy,
+                self._utmp,
+                self._vtmp,
+            )
+        if self.grid.west_edge:
+            self._tmps_west_edge(
+                u.storage,
+                v.storage,
+                self.grid.dx,
+                self.grid.dy,
+                self._utmp,
+                self._vtmp,
+            )
+        if self.grid.east_edge:
+            self._tmps_east_edge(
+                u.storage,
+                v.storage,
+                self.grid.dx,
+                self.grid.dy,
+                self._utmp,
+                self._vtmp,
+            )
+        self._compute_cubed_to_latlon(
+            self._utmp,
+            self._vtmp,
             self.grid.a11,
             self.grid.a12,
             self.grid.a21,
