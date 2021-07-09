577a7ad89467e190318a63a331036cf73efef409
Rhea George <rheag@vulcan.com>
2021-07-09 02:45:28 +0000

    os.environ.get rather than os.get

--- tests/test_config.py
+++
@@ -1,9 +0,0 @@
-import fv3core
-
-
-def test_set_backend():
-    start_backend = fv3core.get_backend()
-    new_backend = "new_backend"
-    assert new_backend != start_backend
-    fv3core.set_backend(new_backend)
-    assert fv3core.get_backend() == new_backend
