c1ca7408d2f91ec109e4f7e3a58ef2d107596488
Rhea George <rheag@vulcan.com>
2021-07-12 20:41:50 -0700

    install gridtools sources version 2 in the jenkins virtualenv, meant to be in daint_venv when we officially move over

--- fv3core/stencils/dyn_core.py
+++ fv3core/stencils/dyn_core.py
@@ -121,9 +121,8 @@ def p_grad_c_stencil(
         rdxc, rdyc
     """
     from __externals__ import hydrostatic
-    # TODO(rheag) change back to interval(...)
-    # https://github.com/GridTools/gt4py/issues/437
-    with computation(PARALLEL), interval(0, -1):
+
+    with computation(PARALLEL), interval(...):
         if __INLINED(hydrostatic):
             wk = pkc[0, 0, 1] - pkc
         else:
@@ -323,9 +322,7 @@ class AcousticDynamics:
         self._p_grad_c = FrozenStencil(
             p_grad_c_stencil,
             origin=self.grid.compute_origin(),
-            # TODO(rheag) change back to add=(1, 1,0)
-            # https://github.com/GridTools/gt4py/issues/437
-            domain=self.grid.domain_shape_compute(add=(1, 1, 1)),
+            domain=self.grid.domain_shape_compute(add=(1, 1, 0)),
             externals={"hydrostatic": self.namelist.hydrostatic},
         )
 
