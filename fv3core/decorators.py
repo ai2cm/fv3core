48b893dd9b0b7ef4b90ab57fb5f43b9247824151
Linus Groner <linus.groner@cscs.ch>
2021-07-13 10:11:01 +0200

    moved __sdfg__ method to gt4py repo, removed validation in __call__ of FrozenStencils

--- fv3core/decorators.py
+++ fv3core/decorators.py
@@ -94,102 +94,6 @@ class FrozenStencil:
     values of origin and domain.
     """
 
-    loaded_compiled_sdfgs: Dict[str, dace.SDFG] = dict()
-
-    def __sdfg__(self, *args, **kwargs):
-        if self._sdfg is not None:
-            return copy.deepcopy(self._sdfg)
-
-        if hasattr(self.stencil_object, "sdfg"):
-            stencil_object = self.stencil_object
-        else:
-            stencil_kwargs = {**self.stencil_config.stencil_kwargs, "backend": "gtc:dace"}
-            stencil_object = gtscript.stencil(definition=self.func, externals=self.externals, **stencil_kwargs)
-
-        basename= os.path.splitext(stencil_object._file_name)[0]
-        filename = basename +"_wrapper_" + str(shash(self.origin, self.domain))+".sdfg"
-        try:
-            self._sdfg = dace.SDFG.from_file(filename)
-            print('reused (__sdfg__):', filename)
-            return copy.deepcopy(self._sdfg)
-        except FileNotFoundError:
-            pass
-        except Exception:
-            raise
-        inner_sdfg = stencil_object.sdfg
-        self._sdfg = dace.SDFG('FrozenStencil_'+inner_sdfg.name)
-        state = self._sdfg.add_state('FrozenStencil_'+inner_sdfg.name+"_state")
-
-        inputs = set()
-        outputs = set()
-        for inner_state in inner_sdfg.nodes():
-            for node in inner_state.nodes():
-                if not isinstance(node, dace.nodes.AccessNode) or inner_sdfg.arrays[node.data].transient:
-                    continue
-                if node.access!=dace.dtypes.AccessType.WriteOnly:
-                    inputs.add(node.data)
-                if node.access != dace.dtypes.AccessType.ReadOnly:
-                    outputs.add(node.data)
-
-
-        nsdfg = state.add_nested_sdfg(inner_sdfg, None, inputs, outputs)
-        for name, array in inner_sdfg.arrays.items():
-            if isinstance(array, dace.data.Array) and not array.transient:
-                axes = self.stencil_object.field_info[name].axes
-
-                shape = [f"__{name}_{axis}_size" for axis in  axes] + [str(d) for d in self.stencil_object.field_info[name].data_dims]
-
-                self._sdfg.add_array(name, dtype=array.dtype, strides=array.strides, shape=shape)
-                if isinstance(self.origin, tuple):
-                    origin = [o for a, o in zip("IJK", self.origin) if a in axes]
-                else:
-                    origin = self.origin.get(name, self.origin.get('_all_', None))
-                    if len(origin)==3:
-                        origin = [o for a, o in zip("IJK", origin) if a in axes]
-
-                subset_strs = [f"{o-e}:{o-e+s}" for o, e ,s in zip(origin, self.stencil_object.field_info[name].boundary.lower_indices, inner_sdfg.arrays[name].shape)]
-                subset_strs += [f"0:{d}" for d in self.stencil_object.field_info[name].data_dims]
-
-                if name in inputs:
-                    state.add_edge(state.add_read(name), None, nsdfg, name, dace.Memlet.simple(name, ",".join(subset_strs)))
-                if name in outputs:
-                    state.add_edge(nsdfg, name, state.add_write(name), None, dace.Memlet.simple(name, ",".join(subset_strs)))
-
-        for symbol in nsdfg.sdfg.free_symbols:
-            if symbol not in self._sdfg.symbols:
-                self._sdfg.add_symbol(symbol, nsdfg.sdfg.symbols[symbol])
-        for sdfg in self._sdfg.all_sdfgs_recursive():
-            sdfg.replace("__I", str(self.domain[0]))
-            sdfg.replace("__J", str(self.domain[1]))
-            sdfg.replace("__K", str(self.domain[2]))
-            sdfg.specialize({"__I": self.domain[0], "__J": self.domain[1], "__K": self.domain[2]})
-        for _, name, array in self._sdfg.arrays_recursive():
-            if array.transient:
-                array.lifetime = dace.dtypes.AllocationLifetime.SDFG
-
-
-        self._sdfg.arg_names = [arg for arg in self.func.__annotations__.keys() if arg != 'return']
-        for arg in self._sdfg.arg_names:
-            if arg in self.stencil_object.field_info and self.stencil_object.field_info[arg] is None:
-                shape = tuple(dace.symbolic.symbol(f"__{arg}_{str(axis)}_size") for axis in self.func.__annotations__[arg].axes)
-                strides = tuple(dace.symbolic.symbol(f"__{arg}_{str(axis)}_stride") for axis in self.func.__annotations__[arg].axes)
-                self._sdfg.add_array(arg, shape=shape, strides=strides, dtype=dace.typeclass(str(self.func.__annotations__[arg].dtype)))
-            if arg in self.stencil_object.parameter_info and self.stencil_object.parameter_info[arg] is None:
-                self._sdfg.add_symbol(arg, stype=dace.typeclass(self.func.__annotations__[arg]))
-        true_args = [arg for arg in self._sdfg.signature_arglist(with_types=False) if not re.match(f"__.*_._stride", arg) and not re.match(f"__.*_._size", arg)]
-        assert len(self._sdfg.arg_names)==len(true_args)
-        self._sdfg.save(filename)
-        print('saved (__sdfg__):', filename)
-        return dace.SDFG.from_json(self._sdfg.to_json())
-
-    def __sdfg_signature__(self):
-        return ( [arg for arg in self.func.__annotations__.keys() if arg != 'return'], [])
-
-    def __sdfg_closure__(self, *args, **kwargs):
-        return {}
-
-
-
 
 
     def __init__(
@@ -209,6 +113,8 @@ class FrozenStencil:
             stencil_config: container for stencil configuration
             externals: compile-time external variables required by stencil
         """
+
+        from gt4py import gtscript # ??? why is this needed
         self.origin = origin
 
         self.domain = domain
@@ -257,107 +163,52 @@ class FrozenStencil:
 
         self._written_fields = get_written_fields(self.stencil_object.field_info) if not jit else None
 
+        if not jit:
+            self.sdfg_wrapper = gtscript.SDFGWrapper(
+                definition=func,
+                origin=origin,
+                domain=domain,
+                externals=externals,
+                name=f"{__name__}.{func.__name__}",
+                **ref_stencil_kwargs,
+            )
+        else:
+            self.sdfg_wrapper = None
+
     def __call__(
         self,
         *args,
         **kwargs,
     ) -> None:
-        assert all( not hasattr(a, 'shape') or all(s > 0 for s in a.shape) for a in args)
-        args_ref = copy.deepcopy(args)
-        kwargs_ref = copy.deepcopy(kwargs)
         if self.stencil_config.validate_args:
             if __debug__ and "origin" in kwargs:
                 raise TypeError("origin cannot be passed to FrozenStencil call")
             if __debug__ and "domain" in kwargs:
                 raise TypeError("domain cannot be passed to FrozenStencil call")
-
-            if self.stencil_object is None:
-                self.stencil_object=gtscript.stencil(
-                    definition=self.func,
-                    externals=self.externals,
-                    **{**self.stencil_config.stencil_kwargs, 'rebuild': True},
-                )
-                self._field_origins = compute_field_origins(
-                    self.stencil_object.field_info, self.origin
-                )
-                self._stencil_run_kwargs = {
-                    "_origin_": self._field_origins,
-                    "_domain_": self.domain,
-                }
-                self._written_fields = get_written_fields(self.stencil_object.field_info)
-            self.stencil_object(*args, **kwargs, origin=self._field_origins, domain=self.domain, validate_args=True)
-
+            self.stencil_object(
+                *args,
+                **kwargs,
+                origin=self._field_origins,
+                domain=self.domain,
+                validate_args=True,
+            )
         else:
             args_as_kwargs = dict(zip(self._argument_names, args))
-            ref_args_as_kwargs = dict(zip(self._argument_names, args_ref))
-            if False and self.stencil_config.backend=="gtc:dace":
-
-                sym_dict = {name: (field.shape, field.strides) for name, field in {**args_as_kwargs, **kwargs}.items() if hasattr(self.func.__annotations__[name], 'axes')}
-
-                try:
-                    name = f"FrozenStencil_{self.func.__name__}_offset_wrapper{shash(self.origin, self.domain, sym_dict)}"
-                    if name in self.loaded_compiled_sdfgs:
-                        csdfg = self.loaded_compiled_sdfgs[name]
-                    else:
-                        csdfg = dace.sdfg.utils.load_precompiled_sdfg(dace.Config.get('default_build_folder')+os.sep+name)
-                        self.loaded_compiled_sdfgs[name] = csdfg
-                    print('reused (__call__):', dace.Config.get('default_build_folder')+os.sep+name)
-                except (RuntimeError, FileNotFoundError):
-
-                    basename = os.path.splitext(self.stencil_object._file_name)[0]
-                    filename = basename + "_wrapper_" + str(shash(self.origin, self.domain, sym_dict)) + ".sdfg"
-                    sdfg = self.__sdfg__()
-                    sdfg.name += str(shash(self.origin, self.domain, sym_dict))
-                    for name, (shape, strides) in sym_dict.items():
-                        for i, axis in enumerate(self.func.__annotations__[name].axes):
-                            sdfg.specialize({f"__{name}_{str(axis)}_size": shape[i]})
-                            sdfg.specialize({f"__{name}_{str(axis)}_stride": strides[i] // self.func.__annotations__[name].dtype.itemsize})
-                    csdfg = sdfg.compile()
-                except Exception:
-                    raise
-                csdfg(**args_as_kwargs, **kwargs)
-            else:
-                if self.stencil_object is None:
-                    self.stencil_object=gtscript.stencil(
-                        definition=self.func,
-                        externals=self.externals,
-                        **{**self.stencil_config.stencil_kwargs, 'rebuild': self.func.__name__ == 'update_vorticity'},
-                    )
-                    self._field_origins = compute_field_origins(
-                        self.stencil_object.field_info, self.origin
-                    )
-                    self._stencil_run_kwargs = {
-                        "_origin_": self._field_origins,
-                        "_domain_": self.domain,
-                    }
-                    self._written_fields = get_written_fields(self.stencil_object.field_info)
-                self.stencil_object.run(**args_as_kwargs, **kwargs, **self._stencil_run_kwargs, exec_info=None)
-
-            self.ref_stencil_object.run(
-                **ref_args_as_kwargs, **kwargs_ref, **self._stencil_run_kwargs, exec_info=None
+            self.stencil_object.run(
+                **args_as_kwargs, **kwargs, **self._stencil_run_kwargs, exec_info=None
             )
             self._mark_cuda_fields_written({**args_as_kwargs, **kwargs})
 
-            fail = False
-            for name, arg in args_as_kwargs.items():
-                ref = ref_args_as_kwargs[name]
-                if hasattr(arg, 'data'):
-                    try:
-                        np.testing.assert_allclose(np.asarray(arg.data), np.asarray(ref.data),
-                                                   err_msg=f'mismatch on arg {name}')
-                    except AssertionError:
-                        print(f'mismatch on arg {name}')
-                        fail = True
-            for k, v in kwargs.items():
-                if hasattr(v, 'data'):
-                    try:
-                        np.testing.assert_allclose(np.asarray(v.data), np.asarray(kwargs_ref[k].data),
-                                                   err_msg=f'mismatch on kwarg {k}')
-                    except AssertionError:
-                        print(f'mismatch on kwarg {k}')
-                        fail = True
-            assert not fail, str(self.func.__name__)
-            # assert not self.func.__name__ == 'update_vorticity'
+    def __sdfg__(self, *args, **kwargs):
+        return self.sdfg_wrapper.__sdfg__(*args, **kwargs)
+
+    def __sdfg_signature__(self):
+        return self.sdfg_wrapper.__sdfg_signature__()
+
+    def __sdfg_closure__(self, *args, **kwargs):
+        return self.sdfg_wrapper.__sdfg_closure__(*args, **kwargs)
+
+
 
 
     def _mark_cuda_fields_written(self, fields: Mapping[str, Storage]):
--- tmp.py
+++ tmp.py
@@ -1,3 +1,49 @@
 import dace
 import numpy as np
+from dace.frontend.python.common import SDFGConvertible
+from gt4py.gtscript import SDFGWrapper, Field, stencil
+from types import SimpleNamespace
 
+origin=(2,0,0)
+domain= (2,2,2)
+
+class TillCallable(SDFGConvertible):
+
+    def __init__(self, definition):
+        self.definition = definition
+        self.stencil_object = stencil(backend="gtc:dace")(definition)
+
+    def __call__(self, grid, *args, **kwargs):
+        self.stencil_object(*args, **kwargs, origin=grid.origin, domain=grid.domain)
+
+    def __sdfg__(self, grid, *args, **kwargs):
+        self.sdfg_wrapper = SDFGWrapper(definition=self.definition, origin=grid.origin, domain=grid.domain)
+        return self.sdfg_wrapper.__sdfg__(self, *args, **kwargs)
+
+    def __sdfg_signature__(self):
+        return ( ['grid']+[arg for arg in self.definition.__annotations__.keys() if arg != 'return'], ['grid'])
+
+
+    def __sdfg_closure__(self, *args, **kwargs):
+        return {}
+
+def tilldecorator(func):
+
+    return TillCallable(definition=func)
+
+@tilldecorator
+def a_stencil(field: Field[np.float64]):
+    with computation(PARALLEL), interval(...):
+        field = 7.0
+
+A = np.ones((10,10,10))
+grid = SimpleNamespace(origin=origin, domain=domain)
+
+
+@dace.program # the real test if this works both commented and not.
+def forward_call(A):
+    a_stencil(grid, A)
+
+forward_call(A)
+
+assert np.allclose(7.0, A[origin[0]:origin[0]+domain[0], origin[1]:origin[1]+domain[1], origin[2]:origin[2]+domain[2]])
