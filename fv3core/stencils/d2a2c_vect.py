6a6cab056920fea51a2ede5692c61a8e1b0d2dc1
Rhea George <rheag@vulcan.com>
2021-08-02 16:35:18 -0700

    no edge code in d2a2c

--- fv3core/stencils/d2a2c_vect.py
+++ fv3core/stencils/d2a2c_vect.py
@@ -24,84 +24,6 @@ def set_tmps(utmp: FloatField, vtmp: FloatField, big_number: float):
         vtmp = big_number
 
 
-def u_east_west_edges1(
-    uc: FloatField,
-    utc: FloatField,
-    utmp: FloatField,
-    v: FloatField,
-    cosa_u: FloatFieldIJ,
-    rsin_u: FloatFieldIJ,
-):
-    with computation(PARALLEL), interval(...):
-        uc = vol_conserv_cubic_interp_func_x(utmp)
-        utc = contravariant(uc, v, cosa_u, rsin_u)
-
-
-def u_east_west_edges2(
-    ua: FloatField,
-    uc: FloatField,
-    utc: FloatField,
-    sin_sg1: FloatFieldIJ,
-    sin_sg3: FloatFieldIJ,
-    dxa: FloatFieldIJ,
-):
-    with computation(PARALLEL), interval(...):
-        utc = edge_interpolate4_x(ua, dxa)
-        uc = utc * sin_sg3[-1, 0] if utc > 0 else utc * sin_sg1
-
-
-def u_east_west_edges3(
-    uc: FloatField,
-    utc: FloatField,
-    utmp: FloatField,
-    v: FloatField,
-    cosa_u: FloatFieldIJ,
-    rsin_u: FloatFieldIJ,
-):
-    with computation(PARALLEL), interval(...):
-        uc = vol_conserv_cubic_interp_func_x_rev(utmp)
-        utc = contravariant(uc, v, cosa_u, rsin_u)
-
-
-def v_north_south_edges1(
-    vc: FloatField,
-    vtc: FloatField,
-    vtmp: FloatField,
-    u: FloatField,
-    cosa_v: FloatFieldIJ,
-    rsin_v: FloatFieldIJ,
-):
-    with computation(PARALLEL), interval(...):
-        vc = vol_conserv_cubic_interp_func_y(vtmp)
-        vtc = contravariant(vc, u, cosa_v, rsin_v)
-
-
-def v_north_south_edges2(
-    va: FloatField,
-    vc: FloatField,
-    vtc: FloatField,
-    sin_sg2: FloatFieldIJ,
-    sin_sg4: FloatFieldIJ,
-    dya: FloatFieldIJ,
-):
-    with computation(PARALLEL), interval(...):
-        vtc = edge_interpolate4_y(va, dya)
-        vc = vtc * sin_sg4[0, -1] if vtc > 0 else vtc * sin_sg2
-
-
-def v_north_south_edges3(
-    vc: FloatField,
-    vtc: FloatField,
-    vtmp: FloatField,
-    u: FloatField,
-    cosa_v: FloatFieldIJ,
-    rsin_v: FloatFieldIJ,
-):
-    with computation(PARALLEL), interval(...):
-        vc = vol_conserv_cubic_interp_func_y_rev(vtmp)
-        vtc = contravariant(vc, u, cosa_v, rsin_v)
-
-
 # almost the same as a2b_ord4's version
 @gtscript.function
 def lagrange_y_func_p1(qx):
@@ -121,12 +43,6 @@ def lagrange_x_func_p1(qy):
 def lagrange_interpolation_x_p1(qy: FloatField, qout: FloatField):
     with computation(PARALLEL), interval(...):
         qout = lagrange_x_func_p1(qy)
-
-
-def avg_box(u: FloatField, v: FloatField, utmp: FloatField, vtmp: FloatField):
-    with computation(PARALLEL), interval(...):
-        utmp = 0.5 * (u + u[0, 1, 0])
-        vtmp = 0.5 * (v + v[1, 0, 0])
 
 
 @gtscript.function
@@ -269,56 +185,6 @@ def vol_conserv_cubic_interp_y(vtmp: FloatField, vc: FloatField):
         vc = vol_conserv_cubic_interp_func_y(vtmp)
 
 
-def vt_edge(
-    vtmp: FloatField,
-    vc: FloatField,
-    u: FloatField,
-    cosa_v: FloatFieldIJ,
-    rsin_v: FloatFieldIJ,
-    vt: FloatField,
-    rev: int,
-):
-    with computation(PARALLEL), interval(...):
-        vc = (
-            vol_conserv_cubic_interp_func_y(vtmp)
-            if rev == 0
-            else vol_conserv_cubic_interp_func_y_rev(vtmp)
-        )
-        vt = contravariant(vc, u, cosa_v, rsin_v)
-
-
-def uc_x_edge1(
-    ut: FloatField, sin_sg3: FloatFieldIJ, sin_sg1: FloatFieldIJ, uc: FloatField
-):
-    with computation(PARALLEL), interval(...):
-        uc = ut * sin_sg3[-1, 0] if ut > 0 else ut * sin_sg1
-
-
-def vc_y_edge1(
-    vt: FloatField, sin_sg4: FloatFieldIJ, sin_sg2: FloatFieldIJ, vc: FloatField
-):
-    with computation(PARALLEL), interval(...):
-        vc = vt * sin_sg4[0, -1] if vt > 0 else vt * sin_sg2
-
-
-@gtscript.function
-def edge_interpolate4_x(ua, dxa):
-    t1 = dxa[-2, 0] + dxa[-1, 0]
-    t2 = dxa[0, 0] + dxa[1, 0]
-    n1 = (t1 + dxa[-1, 0]) * ua[-1, 0, 0] - dxa[-1, 0] * ua[-2, 0, 0]
-    n2 = (t1 + dxa[0, 0]) * ua[0, 0, 0] - dxa[0, 0] * ua[1, 0, 0]
-    return 0.5 * (n1 / t1 + n2 / t2)
-
-
-@gtscript.function
-def edge_interpolate4_y(va, dya):
-    t1 = dya[0, -2] + dya[0, -1]
-    t2 = dya[0, 0] + dya[0, 1]
-    n1 = (t1 + dya[0, -1]) * va[0, -1, 0] - dya[0, -1] * va[0, -2, 0]
-    n2 = (t1 + dya[0, 0]) * va[0, 0, 0] - dya[0, 0] * va[0, 1, 0]
-    return 0.5 * (n1 / t1 + n2 / t2)
-
-
 class DGrid2AGrid2CGridVectors:
     """
     Fortran name d2a2c_vect
@@ -393,121 +259,19 @@ class DGrid2AGrid2CGridVectors:
             d2a2c_avg_offset = -1
         else:
             d2a2c_avg_offset = 3
-
-        if self.grid.south_edge:
-            self._avg_box_south = FrozenStencil(
-                avg_box,
-                origin=(self.grid.isd, self.grid.jsd, 0),
-                domain=(self.grid.nid, self.grid.js + d2a2c_avg_offset, self.grid.npz),
-            )
-        if self.grid.north_edge:
-            self._avg_box_north = FrozenStencil(
-                avg_box,
-                origin=(self.grid.isd, self.grid.je - d2a2c_avg_offset + 1, 0),
-                domain=(
-                    self.grid.nid,
-                    self.grid.jed - self.grid.je + d2a2c_avg_offset,
-                    self.grid.npz,
-                ),
-            )
-        if self.grid.west_edge:
-            self._avg_box_west = FrozenStencil(
-                avg_box,
-                origin=(self.grid.isd, self.grid.jsd, 0),
-                domain=(self.grid.is_ + d2a2c_avg_offset, self.grid.njd, self.grid.npz),
-            )
-        if self.grid.east_edge:
-            self._avg_box_east = FrozenStencil(
-                avg_box,
-                origin=(self.grid.ie - d2a2c_avg_offset + 1, self.grid.jsd, 0),
-                domain=(
-                    self.grid.ied - self.grid.ie + d2a2c_avg_offset,
-                    self.grid.njd,
-                    self.grid.npz,
-                ),
-            )
         self._contravariant_components = FrozenStencil(
             func=contravariant_components,
             origin=(self.grid.is_ - 1 - id_, self.grid.js - 1 - id_, 0),
             domain=(self.grid.nic + pad, self.grid.njc + pad, self.grid.npz),
         )
-        origin_edges = self.grid.compute_origin(add=(-3, -3, 0))
-        domain_edges = self.grid.domain_shape_compute(add=(6, 6, 0))
 
         self._ut_main = FrozenStencil(
             func=ut_main,
             origin=(ifirst, self._j1, 0),
             domain=(idiff, self.grid.njc + 2, self.grid.npz),
         )
-        edge_domain_x = (1, self.grid.njc + 2, self.grid.npz)
-        if self.grid.west_edge:
-            self._u_west_edge1 = FrozenStencil(
-                func=u_east_west_edges1,
-                origin=self.grid.compute_origin(add=(-1, -1, 0)),
-                domain=edge_domain_x,
-            )
-            self._u_west_edge2 = FrozenStencil(
-                func=u_east_west_edges2,
-                origin=self.grid.compute_origin(add=(0, -1, 0)),
-                domain=edge_domain_x,
-            )
-            self._u_west_edge3 = FrozenStencil(
-                func=u_east_west_edges3,
-                origin=self.grid.compute_origin(add=(1, -1, 0)),
-                domain=edge_domain_x,
-            )
-        if self.grid.east_edge:
-            self._u_east_edge1 = FrozenStencil(
-                func=u_east_west_edges1,
-                origin=(self.grid.ie, self.grid.js - 1, 0),
-                domain=edge_domain_x,
-            )
-            self._u_east_edge2 = FrozenStencil(
-                func=u_east_west_edges2,
-                origin=(self.grid.ie + 1, self.grid.js - 1, 0),
-                domain=edge_domain_x,
-            )
-            self._u_east_edge3 = FrozenStencil(
-                func=u_east_west_edges3,
-                origin=(self.grid.ie + 2, self.grid.js - 1, 0),
-                domain=edge_domain_x,
-            )
 
         # Ydir:
-        edge_domain_y = (self.grid.nic + 2, 1, self.grid.npz)
-        if self.grid.south_edge:
-            self._v_south_edge1 = FrozenStencil(
-                func=v_north_south_edges1,
-                origin=self.grid.compute_origin(add=(-1, -1, 0)),
-                domain=edge_domain_y,
-            )
-            self._v_south_edge2 = FrozenStencil(
-                func=v_north_south_edges2,
-                origin=self.grid.compute_origin(add=(-1, 0, 0)),
-                domain=edge_domain_y,
-            )
-            self._v_south_edge3 = FrozenStencil(
-                func=v_north_south_edges3,
-                origin=self.grid.compute_origin(add=(-1, 1, 0)),
-                domain=edge_domain_y,
-            )
-        if self.grid.north_edge:
-            self._v_north_edge1 = FrozenStencil(
-                func=v_north_south_edges1,
-                origin=(self.grid.is_ - 1, self.grid.je, 0),
-                domain=edge_domain_y,
-            )
-            self._v_north_edge2 = FrozenStencil(
-                func=v_north_south_edges2,
-                origin=(self.grid.is_ - 1, self.grid.je + 1, 0),
-                domain=edge_domain_y,
-            )
-            self._v_north_edge3 = FrozenStencil(
-                func=v_north_south_edges3,
-                origin=(self.grid.is_ - 1, self.grid.je + 2, 0),
-                domain=edge_domain_y,
-            )
-
         self._vt_main = FrozenStencil(
             func=vt_main,
             origin=(self._i1, jfirst, 0),
@@ -554,9 +318,6 @@ class DGrid2AGrid2CGridVectors:
             va,
         )
 
-        # Fix the x-edges
-        self._fix_x_edges(ua, va)
-
         self._ut_main(
             self._utmp,
             uc,
@@ -566,8 +327,6 @@ class DGrid2AGrid2CGridVectors:
             utc,
         )
 
-        self._fix_y_edges(ua, va)
-
         self._vt_main(
             self._vtmp,
             vc,
@@ -576,90 +335,3 @@ class DGrid2AGrid2CGridVectors:
             self.grid.rsin_v,
             vtc,
         )
-    def _fix_x_edges(self, ua: FloatField, va: FloatField):
-        utils.device_sync()
-        # Convert gt4py storages to numpy/cupy arrays...
-        ua_arr = utils.asarray(ua)
-        va_arr = utils.asarray(va)
-        utmp_arr = utils.asarray(self._utmp)
-        vtmp_arr = utils.asarray(self._vtmp)
-
-        # Xdir:
-        if self.grid.sw_corner:
-            for i in range(-2, 1):
-                utmp_arr[i + 2, self.grid.js - 1, :] = vtmp_arr[
-                    self.grid.is_ - 1, self.grid.js - i, :
-                ] * -1.0
-            ua_arr[self._i1 - 1, self._j1, :] = -va_arr[self._i1, self._j1 + 2, :]
-            ua_arr[self._i1, self._j1, :] = -va_arr[self._i1, self._j1 + 1, :]
-        if self.grid.se_corner:
-            for i in range(0, 3):
-                utmp_arr[self._nx + i, self.grid.js - 1, :] = vtmp_arr[
-                    self._nx, i + self.grid.js, :
-                ]
-            ua_arr[self._nx, self._j1, :] = va_arr[self._nx, self._j1 + 1, :]
-            ua_arr[self._nx + 1, self._j1, :] = va_arr[self._nx, self._j1 + 2, :]
-        if self.grid.ne_corner:
-            for i in range(0, 3):
-                utmp_arr[self._nx + i, self._ny, :] = -vtmp_arr[
-                    self._nx, self.grid.je - i, :
-                ]
-            ua_arr[self._nx, self._ny, :] = -va_arr[self._nx, self._ny - 1, :]
-            ua_arr[self._nx + 1, self._ny, :] = -va_arr[self._nx, self._ny - 2, :]
-        if self.grid.nw_corner:
-            for i in range(-2, 1):
-                utmp_arr[i + 2, self._ny, :] = vtmp_arr[
-                    self.grid.is_ - 1, self.grid.je + i, :
-                ]
-            ua_arr[self._i1 - 1, self._ny, :] = va_arr[self._i1, self._ny - 2, :]
-            ua_arr[self._i1, self._ny, :] = va_arr[self._i1, self._ny - 1, :]
-
-        # Assign results back to gt4py storages...
-        ua[:] = ua_arr
-        va[:] = va_arr
-        self._utmp[:] = utmp_arr
-        self._vtmp[:] = vtmp_arr
-    
-    def _fix_y_edges(self, ua: FloatField, va: FloatField):
-        utils.device_sync()
-        # Convert gt4py storages to numpy/cupy arrays...
-        ua_arr = utils.asarray(ua)
-        va_arr = utils.asarray(va)
-        utmp_arr = utils.asarray(self._utmp)
-        vtmp_arr = utils.asarray(self._vtmp)
-
-        # Ydir:
-        if self.grid.sw_corner:
-            for j in range(-2, 1):
-                vtmp_arr[self.grid.is_ - 1, j + 2, :] = -utmp_arr[
-                    self.grid.is_ - j, self.grid.js - 1, :
-                ]
-            va_arr[self._i1, self._j1 - 1, :] = -ua_arr[self._i1 + 2, self._j1, :]
-            va_arr[self._i1, self._j1, :] = -ua_arr[self._i1 + 1, self._j1, :]
-        if self.grid.nw_corner:
-            for j in range(0, 3):
-                vtmp_arr[self.grid.is_ - 1, self._ny + j, :] = utmp_arr[
-                    j + self.grid.is_, self._ny, :
-                ]
-            va_arr[self._i1, self._ny, :] = ua_arr[self._i1 + 1, self._ny, :]
-            va_arr[self._i1, self._ny + 1, :] = ua_arr[self._i1 + 2, self._ny, :]
-        if self.grid.se_corner:
-            for j in range(-2, 1):
-                vtmp_arr[self._nx, j + 2, :] = utmp_arr[
-                    self.grid.ie + j, self.grid.js - 1, :
-                ]
-            va_arr[self._nx, self._j1, :] = ua_arr[self._nx - 1, self._j1, :]
-            va_arr[self._nx, self._j1 - 1, :] = ua_arr[self._nx - 2, self._j1, :]
-        if self.grid.ne_corner:
-            for j in range(0, 3):
-                vtmp_arr[self._nx, self._ny + j, :] = -utmp_arr[
-                    self.grid.ie - j, self._ny, :
-                ]
-            va_arr[self._nx, self._ny, :] = -ua_arr[self._nx - 1, self._ny, :]
-            va_arr[self._nx, self._ny + 1, :] = -ua_arr[self._nx - 2, self._ny, :]
-
-        # Assign results back to gt4py storages...
-        ua[:] = ua_arr
-        va[:] = va_arr
-        self._utmp[:] = utmp_arr
-        self._vtmp[:] = vtmp_arr
