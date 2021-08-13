ed0cfd747dcef21cab7923ee4cf6f95a2e618858
tobias <twicki12@gmail.com>
2021-08-13 17:15:18 +0200

    cleanup acoustics

--- examples/standalone/runfile/acoustics.py
+++ examples/standalone/runfile/acoustics.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 from types import SimpleNamespace
-from typing import Any, Dict
+from typing import Any, Collection, Dict, List, Optional, Tuple
 
 import click
 import serialbox
+import yaml
+from timing import collect_data_and_write_to_file
 
 import fv3core
 import fv3core._config as spec
 import fv3core.testing
 import fv3core.utils.global_config as global_config
+import fv3gfs.util as util
 from fv3core.stencils.dyn_core import AcousticDynamics
 from fv3core.utils.grid import Grid
+from fv3core.utils.null_comm import NullComm
+
+
+try:
+    from mpi4py import MPI
+except ImportError:
+    MPI = None
 
 
 def set_up_namelist(data_directory: str) -> None:
@@ -42,7 +52,7 @@ def read_grid(serializer: serialbox.Serializer, rank: int = 0) -> Grid:
     return fv3core.testing.TranslateGrid(grid_data, rank).python_grid()
 
 
-def initialize_fv3core(backend: str, do_halo_updates: bool) -> None:
+def initialize_fv3core(backend: str, disable_halo_exchange: bool) -> None:
     """
     Initializes globalfv3core config to the arguments for single runs
     with the given backend and choice of halo updates
@@ -50,7 +60,6 @@ def initialize_fv3core(backend: str, do_halo_updates: bool) -> None:
     fv3core.set_backend(backend)
     fv3core.set_rebuild(False)
     fv3core.set_validate_args(False)
-    global_config.set_do_halo_exchange(do_halo_updates)
 
 
 def read_input_data(grid: Grid, serializer: serialbox.Serializer) -> Dict[str, Any]:
@@ -60,9 +69,7 @@ def read_input_data(grid: Grid, serializer: serialbox.Serializer) -> Dict[str, A
     return driver_object.collect_input_data(serializer, savepoint_in)
 
 
-def get_state_from_input(
-    grid: Grid, input_data: Dict[str, Any]
-) -> Dict[str, SimpleNamespace]:
+def get_state_from_input(grid: Grid, input_data: Dict[str, Any]) -> Dict[str, SimpleNamespace]:
     """
     Transforms the input data from the dictionary of strings
     to arrays into a state  we can pass in
@@ -86,43 +93,111 @@ def get_state_from_input(
     return {"state": statevars}
 
 
+def set_up_communicator(
+    disable_halo_exchange: bool,
+) -> Tuple[Optional[MPI.Comm], Optional[util.CubedSphereCommunicator]]:
+    layout = spec.namelist.layout
+    partitioner = util.CubedSpherePartitioner(util.TilePartitioner(layout))
+    if not disable_halo_exchange:
+        comm = MPI.COMM_WORLD
+        cube_comm = util.CubedSphereCommunicator(comm, partitioner)
+    else:
+        comm = None
+        cube_comm = util.CubedSphereCommunicator(NullComm(0, 0), partitioner)
+    return comm, cube_comm
+
+
+def get_experiment_name(
+    data_directory: str,
+) -> str:
+    return yaml.safe_load(
+        open(
+            data_directory + "/input.yml",
+            "r",
+        )
+    )["experiment_name"]
+
+
+def initialize_timers() -> Tuple[util.Timer, util.Timer, List, List]:
+    total_timer = util.Timer()
+    total_timer.start("total")
+    timestep_timer = util.Timer()
+    return total_timer, timestep_timer, [], []
+
+
+def read_and_reset_timer(timestep_timer, times_per_step, hits_per_step):
+    times_per_step.append(timestep_timer.times)
+    hits_per_step.append(timestep_timer.hits)
+    timestep_timer.reset()
+    return times_per_step, hits_per_step
+
+
 @click.command()
 @click.argument("data_directory", required=True, nargs=1)
 @click.argument("time_steps", required=False, default="1")
 @click.argument("backend", required=False, default="gtc:gt:cpu_ifirst")
-@click.option("--halo_update/--no-halo_update", default=False)
+@click.option("--disable_halo_exchange/--no-disable_halo_exchange", default=True)
+@click.option("--print_timings/--no-print_timings", default=True)
 def driver(
     data_directory: str,
     time_steps: str,
     backend: str,
-    halo_update: bool,
+    disable_halo_exchange: bool,
+    print_timings: bool,
 ):
-    set_up_namelist(data_directory)
-    serializer = initialize_serializer(data_directory)
-    initialize_fv3core(backend, halo_update)
-    grid = read_grid(serializer)
-    spec.set_grid(grid)
+    total_timer, timestep_timer, times_per_step, hits_per_step = initialize_timers()
+    with total_timer.clock("initialization"):
+        set_up_namelist(data_directory)
+        serializer = initialize_serializer(data_directory)
+        initialize_fv3core(backend, disable_halo_exchange)
+        mpi_comm, communicator = set_up_communicator(disable_halo_exchange)
+        grid = read_grid(serializer)
+        spec.set_grid(grid)
 
-    input_data = read_input_data(grid, serializer)
-    acoutstics_object = AcousticDynamics(
-        None,
-        grid.grid_indexing,
-        grid.grid_data,
-        grid.damping_coefficients,
-        grid.grid_type,
-        grid.nested,
-        grid.stretched_grid,
-        spec.namelist.acoustic_dynamics,
-        input_data["ak"],
-        input_data["bk"],
-        input_data["pfull"],
-        input_data["phis"],
+        input_data = read_input_data(grid, serializer)
+        experiment_name = get_experiment_name(data_directory)
+        acoustics_object = AcousticDynamics(
+            communicator,
+            grid.grid_indexing,
+            grid.grid_data,
+            grid.damping_coefficients,
+            grid.grid_type,
+            grid.nested,
+            grid.stretched_grid,
+            spec.namelist.acoustic_dynamics,
+            input_data["ak"],
+            input_data["bk"],
+            input_data["pfull"],
+            input_data["phis"],
+        )
+
+        state = get_state_from_input(grid, input_data)
+
+        # warm-up timestep.
+        # We're intentionally not passing the timer here to exclude
+        # warmup/compilation from the internal timers
+        acoustics_object(**state)
+
+    # we set up a specific timer for each timestep
+    # that is cleared after so we get individual statistics
+    for _ in range(int(time_steps) - 1):
+        # this loop is not required, but make performance numbers comparable with FVDynamics
+        for _ in range(spec.namelist.k_split):
+            with timestep_timer.clock("DynCore"):
+                acoustics_object(**state)
+        times_per_step, hits_per_step = read_and_reset_timer(
+            timestep_timer, times_per_step, hits_per_step
+        )
+    total_timer.stop("total")
+    times_per_step, hits_per_step = read_and_reset_timer(
+        total_timer, times_per_step, hits_per_step
     )
 
-    state = get_state_from_input(grid, input_data)
-
-    for _ in range(int(time_steps)):
-        acoutstics_object(**state)
+    if print_timings:
+        # Collect times and output statistics in json
+        collect_data_and_write_to_file(
+            mpi_comm, hits_per_step, times_per_step, experiment_name, time_steps, backend
+        )
 
 
 if __name__ == "__main__":
