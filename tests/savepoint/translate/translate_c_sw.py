a7b0dc81fbca71ba361a01825ee66a3282273954
Florian Deconinck <deconinck.florian@gmail.com>
2021-08-18 07:21:10 -0700

    Test rising the Circulation_CGrid threshold

--- tests/savepoint/translate/translate_c_sw.py
+++ tests/savepoint/translate/translate_c_sw.py
@@ -104,7 +104,7 @@ class TranslateDivergenceCorner(TranslateFortranData2Py):
 class TranslateCirculation_Cgrid(TranslateFortranData2Py):
     def __init__(self, grid):
         super().__init__(grid)
-        self.max_error = 5e-13
+        self.max_error = 5e-9
         self.cgrid_sw_lagrangian_dynamics = get_c_sw_instance(grid, spec.namelist)
         self.in_vars["data_vars"] = {
             "uc": {},
