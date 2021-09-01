a71b767957ff94dbacd54a01756a98dd4d281c91
Eddie Davis <eddied@vulcan.com>
2021-09-01 23:18:24 +0000

    Add TODO comment to Del2Cubed

--- fv3core/stencils/del2cubed.py
+++ fv3core/stencils/del2cubed.py
@@ -41,24 +41,28 @@ def corner_fill(q_in: FloatField, q_out: FloatField):
     from __externals__ import i_end, i_start, j_end, j_start
 
     # Fills the same scalar value into three locations in q for each corner
+    # TODO(eddied): Alternating computations prevents improper fusion in the toolchain
     with computation(PARALLEL), interval(...), horizontal(region[i_start, j_start]):
         q_out = (q_in[0, 0, 0] + q_in[-1, 0, 0] + q_in[0, -1, 0]) * (1.0 / 3.0)
     with computation(FORWARD), interval(...), horizontal(region[i_start - 1, j_start]):
         q_out = q_in[1, 0, 0]
     with computation(PARALLEL), interval(...), horizontal(region[i_start, j_start - 1]):
         q_out = q_in[0, 1, 0]
+
     with computation(FORWARD), interval(...), horizontal(region[i_end, j_start]):
         q_out = (q_in[0, 0, 0] + q_in[1, 0, 0] + q_in[0, -1, 0]) * (1.0 / 3.0)
     with computation(PARALLEL), interval(...), horizontal(region[i_end + 1, j_start]):
         q_out = q_in[-1, 0, 0]
     with computation(FORWARD), interval(...), horizontal(region[i_end, j_start - 1]):
         q_out = q_in[0, 1, 0]
+
     with computation(PARALLEL), interval(...), horizontal(region[i_end, j_end]):
         q_out = (q_in[0, 0, 0] + q_in[1, 0, 0] + q_in[0, 1, 0]) * (1.0 / 3.0)
     with computation(FORWARD), interval(...), horizontal(region[i_end + 1, j_end]):
         q_out = q_in[-1, 0, 0]
     with computation(PARALLEL), interval(...), horizontal(region[i_end, j_end + 1]):
         q_out = q_in[0, -1, 0]
+
     with computation(FORWARD), interval(...), horizontal(region[i_start, j_end]):
         q_out = (q_in[0, 0, 0] + q_in[-1, 0, 0] + q_in[0, 1, 0]) * (1.0 / 3.0)
     with computation(PARALLEL), interval(...), horizontal(region[i_start - 1, j_end]):
