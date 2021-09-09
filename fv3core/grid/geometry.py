b5515ffe212a0c9c20358e78404e8ce686c37f05
oelbert <olivere@allenai.org>
2021-09-08 20:12:41 -0400

    more work on utils

--- fv3core/grid/geometry.py
+++ fv3core/grid/geometry.py
@@ -1,3 +1,4 @@
+from math import sin
 import typing
 from fv3core.utils.global_constants import PI
 from .gnomonic import lon_lat_to_xyz, xyz_midpoint, normalize_xyz, spherical_cos
@@ -92,3 +93,51 @@ def calc_es(xyz_dgrid, xyz_agrid, nhalo, tile_partitioner, rank, np):
     es[-nhalo:, -nhalo:, :, :] = 0.
     
     return es
+
+def calculate_cos_sin_sg(xyz_dgrid, xyz_agrid, ec1, ec2, tile_partitioner, rank np):
+    """
+    Calculates the cosine and sine of the corner and side angles at each of the following points:
+    8---3---7
+    |       |
+    0   4   2
+    |       |
+    5---1---6
+    """
+    shape_a = xyz_agrid.shape
+    cos_sg = np.zeros((shape_a[0], shape_a[1], 9))
+    sin_sg = np.zeros((shape_a[0], shape_a[1], 9))
+
+    cos_sg[:, :, 5] = spherical_cos(xyz_dgrid[:-1, :-1, 0], xyz_dgrid[1:, :-1, 0], xyz_dgrid[:-1, 1:, 0])
+    cos_sg[:, :, 6] = -1 * spherical_cos(xyz_dgrid[1:, :-1, 0], xyz_dgrid[:-1, :-1, 0], xyz_dgrid[1:, 1:, 0])
+    cos_sg[:, :, 7] = spherical_cos(xyz_dgrid[1:, 1:, 0], xyz_dgrid[1:, :-1, 0], xyz_dgrid[:-1, 1:, 0])
+    cos_sg[:, :, 8] = -1 * spherical_cos(xyz_dgrid[:-1, 1:, 0], xyz_dgrid[:-1, :-1, 0], xyz_dgrid[1:, 1:, 0])
+
+    midpoint = xyz_midpoint(xyz_dgrid[:-1, :-1, 0], xyz_dgrid[:-1, 1:, 0])
+    cos_sg[:, :, 0] = spherical_cos(midpoint, xyz_agrid[:, :, 0], xyz_dgrid[:-1, 1:, 0])
+    midpoint = xyz_midpoint(xyz_dgrid[:-1, :-1, 0], xyz_dgrid[1:, :-1, 0])
+    cos_sg[:, :, 1] = spherical_cos(midpoint, xyz_dgrid[1:, :-1, 0], xyz_agrid[:, :, 0])
+    midpoint = xyz_midpoint(xyz_dgrid[1:, :-1, 0], xyz_dgrid[1:, 1:, 0])
+    cos_sg[:, :, 2] = spherical_cos(midpoint, xyz_agrid[:, :, 0], xyz_dgrid[1:, :-1, 0])
+    midpoint = xyz_midpoint(xyz_dgrid[:-1, 1:, 0], xyz_dgrid[1:, 1:, 0])
+    cos_sg[:, :, 3] = spherical_cos(midpoint, xyz_dgrid[:-1, 1:, 0], xyz_agrid[:, :, 0])
+
+    cos_sg[:, :, 4] = np.sum(ec1*ec2, axis=-1)
+
+    sin_sg_tmp = 1.-cos_sg**2
+    sin_sg_tmp[sin_sg_tmp < 0.] = 0.
+    sin_sg = np.sqrt(sin_sg_tmp)
+    sin_sg[sin_sg > 1.] = 1.
+
+    #Adjust for corners:
+    if tile_partitioner.on_tile_left(rank):
+        if tile_partitioner.on_tile_bottom(rank):
+            pass #southwest corner
+        elif tile_partitioner.on_tile_top(rank):
+            pass #northwest corner
+    elif tile_partitioner.on_tile_right(rank):
+        if tile_partitioner.on_tile_bottom(rank):
+            pass #southeast corner
+        elif tile_partitioner.on_tile_top(rank):
+            pass #northeast corner
+
+    return cos_sg, sin_sg
