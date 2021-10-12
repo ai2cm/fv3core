cb34ec76b6d5fb56080dd9ba66c3af8db6c4f167
tobias <twicki12@gmail.com>
2021-10-13 00:24:01 +0200

    check before copy

--- .jenkins/actions/fetch_caches.sh
+++ .jenkins/actions/fetch_caches.sh
@@ -3,19 +3,24 @@ set -e -x
 BACKEND=$1
 EXPNAME=$2
 SANITIZED_BACKEND=`echo $BACKEND | sed 's/:/_/g'` #sanitize the backend from any ':'
+CACHE_DIR="/scratch/snx3000/tobwi/sbox/test_pr/"
 
 if [ ! -d $(pwd)/.gt_cache ]; then
-    # version_file=/scratch/snx3000/olifu/jenkins/scratch/store_gt_caches/$EXPNAME/$SANITIZED_BACKEND/GT4PY_VERSION.txt
-    version_file=/scratch/snx3000/tobwi/sbox/test_pr/$EXPNAME/$SANITIZED_BACKEND/GT4PY_VERSION.txt
-    if [ -f ${version_file} ]; then
-	version=`cat ${version_file}`
-    else
-	version=""
-    fi
-    if [ "$version" == "$GT4PY_VERSION" ]; then
-        # cp -r /scratch/snx3000/olifu/jenkins/scratch/store_gt_caches/$EXPNAME/$SANITIZED_BACKEND/.gt_cache* .
-        cp -r /scratch/snx3000/tobwi/sbox/test_pr/$EXPNAME/$SANITIZED_BACKEND/.gt_cache* .
-        # find . -name m_\*.py -exec sed -i "s|\/scratch\/snx3000\/olifu\/jenkins_submit\/workspace\/fv3core-cache-setup\/backend\/$SANITIZED_BACKEND\/experiment\/$EXPNAME\/slave\/daint_submit|$(pwd)|g" {} +
-        find . -name m_\*.py -exec sed -i "s|\/scratch\/snx3000\/olifu\/jenkins_submit\/workspace\/gtc_cache_setup\/backend\/$SANITIZED_BACKEND\/experiment\/$EXPNAME\/slave\/daint_submit|$(pwd)|g" {} +
+    if [ -d ${CACHE_DIR}/$EXPNAME/$SANITIZED_BACKEND ]; then
+        # version_file=/scratch/snx3000/olifu/jenkins/scratch/store_gt_caches/$EXPNAME/$SANITIZED_BACKEND/GT4PY_VERSION.txt
+        version_file=${CACHE_DIR}/$EXPNAME/$SANITIZED_BACKEND/GT4PY_VERSION.txt
+        if [ -f ${version_file} ]; then
+        version=`cat ${version_file}`
+        else
+        version=""
+        fi
+        if [ "$version" == "$GT4PY_VERSION" ]; then
+            if [ -d ${CACHE_DIR}/$EXPNAME/$SANITIZED_BACKEND/.gt_cache  ]; then
+                # cp -r /scratch/snx3000/olifu/jenkins/scratch/store_gt_caches/$EXPNAME/$SANITIZED_BACKEND/.gt_cache* .
+                cp -r /scratch/snx3000/tobwi/sbox/test_pr/$EXPNAME/$SANITIZED_BACKEND/.gt_cache .
+                # find . -name m_\*.py -exec sed -i "s|\/scratch\/snx3000\/olifu\/jenkins_submit\/workspace\/fv3core-cache-setup\/backend\/$SANITIZED_BACKEND\/experiment\/$EXPNAME\/slave\/daint_submit|$(pwd)|g" {} +
+                find . -name m_\*.py -exec sed -i "s|\/scratch\/snx3000\/olifu\/jenkins_submit\/workspace\/gtc_cache_setup\/backend\/$SANITIZED_BACKEND\/experiment\/$EXPNAME\/slave\/daint_submit|$(pwd)|g" {} +
+            fi
+        fi
     fi
 fi
