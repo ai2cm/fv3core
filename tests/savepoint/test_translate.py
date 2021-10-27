16326ee0bafe39512455ff3b2e89f5dd62f64524
Eddie Davis <eddied@allenai.org>
2021-10-27 23:45:16 +0000

    Initialize validation variables

--- tests/savepoint/test_translate.py
+++ tests/savepoint/test_translate.py
@@ -108,6 +108,9 @@ def sample_wherefail(
     # Determine worst result
     worst_metric_err = 0.0
     worst_full_idx = 0
+    computed_worst = 0.0
+    reference_worst = 0.0
+    worst_abs_err = 0.0
     for b in range(bad_indices_count):
         full_index = [f[b] for f in found_indices]
         metric_err = compare_scalar(computed_failures[b], reference_failures[b])
