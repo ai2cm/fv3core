fd4efae054770ca82d34cc610b309b696e3624be
Eddie Davis <eddied@allenai.org>
2021-12-15 15:38:17 -0800

    Fix computepath_method import in fv_subgridz

--- fv3core/stencils/fv_subgridz.py
+++ fv3core/stencils/fv_subgridz.py
@@ -5,7 +5,7 @@ from gt4py.gtscript import __INLINED, BACKWARD, PARALLEL, computation, interval
 
 import fv3core.utils.gt4py_utils as utils
 import fv3gfs.util
-from fv3core.decorators import ArgSpec, computepath_method
+from fv3core.decorators import ArgSpec
 from fv3core.stencils.basic_operations import dim
 from fv3core.utils.global_constants import (
     C_ICE,
@@ -18,7 +18,7 @@ from fv3core.utils.global_constants import (
     RDGAS,
     ZVIR,
 )
-from fv3core.utils.stencil import StencilFactory
+from fv3core.utils.stencil import StencilFactory, computepath_method
 from fv3core.utils.typing import FloatField
 
 
