ba6e6f727badd6ab5bfc86b8c5b366b1be264320
Florian <deconinck.florian@gmail.com>
2022-03-18 14:55:30 -0700

    Fine tune for splittable regions

--- fv3core/utils/dace/computepath.py
+++ fv3core/utils/dace/computepath.py
@@ -12,6 +12,7 @@ from dace.transformation.transformation import simplification_transformations
 from fv3core.utils.dace.utils import BuildProgress
 
 from fv3core.utils import global_config
+from fv3core.utils.dace.sdfg_opt_passes import splittable_region_expansion
 
 import gt4py.storage
 
@@ -106,6 +107,9 @@ def call_sdfg(daceprog: DaceProgram, sdfg: dace.SDFG, args, kwargs, sdfg_final=F
                 )
         else:
             sdfg.simplify(validate=False)
+
+        # Perform pre-expansion fine tuning
+        splittable_region_expansion(sdfg)
 
         # Expand the stencil computation Library Nodes with the right expansion
         with BuildProgress("Expand"):
